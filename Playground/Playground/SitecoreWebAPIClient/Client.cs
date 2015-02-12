991b3a96470eadca34ef8d33d1cf1afa2d36b7d6
Mateusz Kopij <sagasu@gmail.com>
2015-02-12 14:29:33 +0000

    not typical stack implementation

---
+++ Playground/Playground/SitecoreWebAPIClient/Client.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using NUnit.Framework;
+
+namespace Playground.SitecoreWebAPIClient
+{
+    [TestFixture]
+    public class Client
+    {
+        [Test]
+        public void ReceiveResponse_ValidSecurity_Success()
+        {
+            var client = new WebClient();
+
+            var n = new NameValueCollection();
+            n["X-Scitemwebapi-Username"] = @"extranet\webapi";
+            n["X-Scitemwebapi-Password"] = "standardMas";
+
+            client.Headers.Add(n);
+
+            var result = client.DownloadString(
+                      "http://my.trafalgar.com/-/item/v1/?sc_itemid={AB881E56-C187-47DF-BCF6-395DD545D95E}");
+
+        }
+    }
+}
---
+++ Playground/Playground/Stack/VerbatimStack.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Playground.Stack
+{
+    [TestFixture]
+    public class VerbatimStackTests
+    {
+        [Test]
+        public void VerbatimStack_PushOneElement_ShouldReturnInOnTop()
+        {
+            var foo = VerbatimStack<bool>.Empty.Push(true);
+
+            Assert.AreEqual(true, foo.Top);
+        }
+
+        [Test]
+        public void VerbatimStack_PopAndTop_ShouldReturnFirstPushedElement()
+        {
+            var foo = VerbatimStack<bool>.Empty.Push(false).Push(true);
+
+            Assert.AreEqual(false, foo.Pop().Top);
+        }
+        
+        [Test]
+        public void VerbatimStack_Iterator_ShouldReturnAllElementsInOrder()
+        {
+            var foo = VerbatimStack<bool>.Empty.Push(true).Push(false).Push(false);
+
+            using (var e1 = foo.GetEnumerator())
+            using (var e2 = new List<bool> { true, false, false }.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    Assert.AreEqual(e1.Current, e2.Current);
+                }
+            }
+        }
+    }
+
+    public abstract class VerbatimStack<T> : IEnumerable<T>
+    {
+        public static readonly VerbatimStack<T> Empty = new EmptyStack();
+        public abstract VerbatimStack<T> Pop();
+        public abstract T Top { get; }
+        public abstract bool IsEmpty { get; }
+
+        public VerbatimStack<T> Push(T t)
+        {
+            return new NonEmptyStack(t, this);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (this.IsEmpty)
+            {
+                yield return this.Top;
+                this.Pop();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class EmptyStack : VerbatimStack<T>
+        {
+            public override VerbatimStack<T> Pop()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public override T Top
+            {
+                get { throw new System.NotImplementedException(); }
+            }
+
+            public override bool IsEmpty
+            {
+                get { return true; }
+            }
+        }
+
+        private class NonEmptyStack : VerbatimStack<T>
+        {
+            private readonly T _head;
+            private readonly VerbatimStack<T> _tail;
+
+            public NonEmptyStack(T t, VerbatimStack<T> verbatimStack)
+            {
+                _head = t;
+                _tail = verbatimStack;
+            }
+
+            public override VerbatimStack<T> Pop()
+            {
+                return _tail;
+            }
+
+            public override T Top
+            {
+                get { return _head; }
+            }
+
+            public override bool IsEmpty
+            {
+                get { return false; }
+            }
+        }
+    }
+}
