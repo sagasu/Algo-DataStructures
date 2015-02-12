291c3ad342f12736f8617087f05f11b72ea7c6e8
Mateusz Kopij <sagasu@gmail.com>
2015-02-12 17:03:53 +0000

    Initial commit of array based stack. Work in progress.

---
+++ Playground/Playground/Stack/ArrayStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Playground.Stack
+{
+    public class ArrayStack<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private const int DefaultSize = 4;
+        private int _nrOfElements = 0;
+        
+        public ArrayStack()
+        {
+            _items = new T[DefaultSize];
+        }
+
+        public bool IsEmpty()
+        {
+            return _nrOfElements == 0;
+        }
+
+        public T Top()
+        {
+            return _items[_nrOfElements - 1];
+        }
+
+        public T Pop()
+        {
+            _nrOfElements -= 1;
+            return _items[_nrOfElements];
+        }
+
+        public void Push(T item)
+        {
+            if (_items.Length == _nrOfElements)
+            {
+                var destinationArray = new T[_items.Length*2];
+                Array.Copy(_items, destinationArray, _items.Length);
+                _items = destinationArray;
+            }
+            _items[_nrOfElements] = item;
+            _nrOfElements += 1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (!this.IsEmpty())
+            {
+                yield return this.Pop();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Playground/Playground/Stack/ArrayStackTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Playground.Stack
+{
+    [TestFixture]
+    public class ArrayStackTests
+    {
+        [Test]
+        public void Push_OneElement_ShouldBeReturnedWhenPop()
+        {
+            var arrayStack = new ArrayStack<bool>();
+            arrayStack.Push(true);
+
+            Assert.AreEqual(true, arrayStack.Pop());
+        }
+        
+        [Test]
+        public void Push_MoreThanFiveElements_ShouldBeReturnedWhenPop()
+        {
+            var arrayStack = new ArrayStack<bool>();
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+
+            Assert.AreEqual(true, arrayStack.Pop());
+            Assert.AreEqual(true, arrayStack.Pop());
+            Assert.AreEqual(true, arrayStack.Pop());
+            Assert.AreEqual(true, arrayStack.Pop());
+            Assert.AreEqual(true, arrayStack.Pop());
+        }
+        
+        [Test]
+        public void Push_MoreThanFiveElements_ShouldEnumeratorWork()
+        {
+            var arrayStack = new ArrayStack<bool>();
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+            arrayStack.Push(true);
+
+            using (var actualEnumerator = arrayStack.GetEnumerator())
+            using (var expectedEnumerator = new List<bool> { true, true, true, true, true }.GetEnumerator())
+            {
+                while (actualEnumerator.MoveNext() && expectedEnumerator.MoveNext())
+                {
+                    Assert.AreEqual(actualEnumerator.Current, expectedEnumerator.Current);
+                }
+            }
+        }
+    }
+}
