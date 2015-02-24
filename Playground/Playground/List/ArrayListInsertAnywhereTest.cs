3120d8182d52fbdda6227eee74b2a8c4fb0cd4e2
Mateusz Kopij <sagasu@gmail.com>
2015-02-24 15:04:58 +0000

    ArrayListInsertAnywhere initial commit, just add method and enumerator work.

---
+++ Playground/Playground/List/ArrayListInsertAnywhereTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Playground.List
+{
+    [TestFixture]
+    public class ArrayListInsertAnywhereTest
+    {
+        [Test]
+        public void Add_SingleElement_AbleToRetriveIt()
+        {
+            var al = new ArrayListInsertAnywhere<int>(){{3,0}};
+            
+            Assert.AreEqual(1,al.Count());
+            Assert.AreEqual(3,al.First());
+        }
+    }
+}
