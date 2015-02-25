e4688434cf0fd17ad6638f90aed48c87343aa6cf
Mateusz Kopij <sagasu@gmail.com>
2015-02-25 11:49:52 +0000

    Initial merge sort implementation.

---
+++ Playground/Playground/Sort/MergeSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Playground.Sort
+{
+    public class MergeSort<T> where T : IComparable<T>
+    {
+        public IEnumerable<T> Sort(IEnumerable<T> elements)
+        {
+            var enumerable = elements as IList<T> ?? elements.ToList();
+            var firstElement = enumerable.First();
+            return Sort(enumerable.Skip(1), firstElement);
+        }
+
+        private static IList<T> Sort(IEnumerable<T> elements, T mediumElement)
+        {
+            var sortNode = Split(elements, mediumElement);
+            IList<T> sortedSmallerValues;
+            IList<T> sortedGreaterValues;
+            if (sortNode.SmallerValues.Count > 1)
+            {
+                var firstSmallerValue = sortNode.SmallerValues.First();
+                sortedSmallerValues = Sort(sortNode.SmallerValues.Skip(1), firstSmallerValue);
+            }
+            else
+            {
+                sortedSmallerValues = sortNode.SmallerValues;
+            }
+
+            if (sortNode.SmallerValues.Count > 1)
+            {
+                var firstGreaterValue = sortNode.GreaterValues.First();
+                sortedGreaterValues = Sort(sortNode.GreaterValues.Skip(1), firstGreaterValue);
+            }
+            else
+            {
+                sortedGreaterValues = sortNode.GreaterValues;
+            }
+
+            var sorted = new List<T>();
+            sorted.AddRange(sortedSmallerValues);
+            sorted.Add(mediumElement);
+            sorted.AddRange(sortedGreaterValues);
+            return sorted;
+        }
+
+        private static SortNode Split(IEnumerable<T> elements, T mediumElement)
+        {
+            var sortNode = new SortNode();
+            foreach (var element in elements)
+            {
+                if (element.CompareTo(mediumElement) < 0)
+                {
+                    sortNode.SmallerValues.Add(element);
+                }
+                else
+                {
+                    sortNode.GreaterValues.Add(element);
+                }
+            }
+            return sortNode;
+        }
+
+        private class SortNode
+        {
+            public IList<T> SmallerValues { get; set; }
+            public IList<T> GreaterValues { get; set; }
+
+            public SortNode()
+            {
+                SmallerValues = new List<T>();
+                GreaterValues = new List<T>();
+            }
+        }
+    }
+}
---
+++ Playground/Playground/Sort/MergeSortTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Playground.Sort
+{
+    [TestFixture]
+    public class MergeSortTest
+    {
+        [Test]
+        public void Sort_FewElements_ShouldSortElements()
+        {
+            var elements = new List<int> {5, 6, 7, 435, 2, 23, 456, 63, 89, 2};
+            var sorted = new MergeSort<int>().Sort(elements);
+            CollectionAssert.AreEqual(new List<int>{2,2,5,6,7,23,63, 89,435,456}, sorted);
+        }
+    }
+}
