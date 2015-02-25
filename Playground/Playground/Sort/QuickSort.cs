0c609e26355634627e870ec3890cad9c42f8e039
Mateusz Kopij <sagasu@gmail.com>
2015-02-25 14:55:45 +0000

    Initial quicksort implementation.

---
+++ Playground/Playground/Sort/QuickSort.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Playground.Sort
+{
+    public class QuickSort<T>
+    {
+        public IEnumerable<T> Sort(List<T> v, IComparer<T> comparer)
+        {
+            if (v.Count < 2)
+                return v;
+
+            var pivot = v[v.Count / 2];
+
+            return Sort(v.Where(x => comparer.Compare(x, pivot) < 0).ToList(), comparer)
+                .Concat(new T[] { pivot })
+                .Concat(Sort(v.Where(x => comparer.Compare(x, pivot) > 0).ToList(), comparer));
+        }
+    }
+}
---
+++ Playground/Playground/Sort/QuickSortTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.Sort
+{
+    [TestFixture]
+    public class QuickSortTest
+    {
+        [Test]
+        public void Sort_SimpleList_ShouldSort()
+        {
+            var elements = new List<int> {42, 534, 63, 3, 2, 23, 1};
+            var sorted = new QuickSort<int>().Sort(elements, Comparer<int>.Default);
+
+            CollectionAssert.AreEqual(new List<int>{1,2,3,23,42,63,534}, sorted);
+        }
+
+        [Test]
+        public void Sort_EmptyList_ShouldReturnEmptyList()
+        {
+            var elements = new List<int>();
+            var sorted = new QuickSort<int>().Sort(elements, Comparer<int>.Default);
+
+            CollectionAssert.AreEqual(new List<int>(), sorted);
+        }
+
+        [Test]
+        public void Sort_SingleElement_ShouldReturnSingleElement()
+        {
+            var elements = new List<int> { 42 };
+            var sorted = new QuickSort<int>().Sort(elements, Comparer<int>.Default);
+
+            CollectionAssert.AreEqual(new List<int> { 42 }, sorted);
+        }
+
+        [Test]
+        public void Sort_TwoElements_ShouldSort()
+        {
+            var elements = new List<int> { 534, 63};
+            var sorted = new QuickSort<int>().Sort(elements, Comparer<int>.Default);
+
+            CollectionAssert.AreEqual(new List<int> { 63, 534 }, sorted);
+        }
+
+        [Test]
+        public void Sort_ThreeElements_ShouldSort()
+        {
+            var elements = new List<int> { 42, 534, 63};
+            var sorted = new QuickSort<int>().Sort(elements, Comparer<int>.Default);
+
+            CollectionAssert.AreEqual(new List<int> {  42, 63, 534 }, sorted);
+        }
+    }
+}
