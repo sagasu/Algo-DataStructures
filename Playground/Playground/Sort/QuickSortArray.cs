d1c0e70d20b60cd13e26b89b84448b23cc5c7d50
Mateusz Kopij <sagasu@gmail.com>
2015-03-03 11:23:46 +0000

    working on array quick sort impl.

---
+++ Playground/Playground/Sort/QuickSortArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Sort
+{
+    public class QuickSortArray<T> where T : IComparable<T>
+    {
+        public IEnumerable<T> Sort(List<T> elements)
+        {
+            return !elements.Any() ? elements : Sort(elements, elements.Count - 1, 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <param name="hi">Index of highest element in array</param>
+        /// <param name="lo">Index of lowest element in array</param>
+        /// <returns></returns>
+        public IEnumerable<T> Sort(List<T> elements, int lo, int hi)
+        {
+            var pivotIndex = Partition(elements, hi, lo);
+            if (pivotIndex > 0 && lo < hi)
+            {
+                Sort(elements, lo, pivotIndex - 1);
+            }
+            if (pivotIndex < hi && lo < hi)
+            {
+                Sort(elements, pivotIndex + 1, hi);
+            }
+            return elements;
+        }
+
+        private static int Partition(List<T> elements, int hi, int lo)
+        {
+            var pivotIndex = hi;
+            while (hi > lo)
+            {
+                if (Comparer<T>.Default.Compare(elements[lo],elements[pivotIndex]) > 0)
+                {
+                    Swap(elements, lo, pivotIndex);
+                    var tempIndex = lo;
+                    lo = pivotIndex;
+                    pivotIndex = tempIndex;
+                }
+
+                if (Comparer<T>.Default.Compare(elements[hi], elements[pivotIndex]) < 0)
+                {
+                    Swap(elements, hi, pivotIndex);
+                    var tempIndex = hi;
+                    hi = pivotIndex;
+                    pivotIndex = tempIndex;
+                }
+
+                --hi;
+                ++lo;
+            }
+            return pivotIndex;
+        }
+
+        private static void Swap(IList<T> elements, int a, int b)
+        {
+            var temp = elements[a];
+            elements[a] = elements[b];
+            elements[b] = temp;
+        }
+    }
+}
---
+++ Playground/Playground/Sort/QuickSortArrayTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.Sort
+{
+    [TestFixture]
+    class QuickSortArrayTests
+    {
+        [Test]
+        public void Sort_EmptyList_ShouldReturnEmptyList()
+        {
+            var qs = new QuickSortArray<int>();
+            Assert.AreEqual(Enumerable.Empty<int>(),qs.Sort(new List<int>()));
+        }
+
+        [Test]
+        public void Sort_OneElement_ShouldReturnOneElement()
+        {
+            var qs = new QuickSortArray<int>();
+            var elements = new List<int> {3};
+            Assert.AreEqual(new List<int>{3}, qs.Sort(elements));
+        }
+
+        [Test]
+        public void Sort_TwoElements_ShouldSort()
+        {
+            var qs = new QuickSortArray<int>();
+
+            var elements = new List<int> { 3,4 };
+            Assert.AreEqual(new List<int> { 3,4 }, qs.Sort(elements));
+
+            elements = new List<int> { 4,3 };
+            Assert.AreEqual(new List<int> { 3, 4 }, qs.Sort(elements));
+        }
+
+        [Test]
+        public void Sort_MultipleElements_ShouldSort()
+        {
+            var qs = new QuickSortArray<int>();
+            var elements = new List<int> { 3,2,55,23,23,1,534,3,444 };
+            Assert.AreEqual(new List<int> { 1,2,3,3,23,23,55,444,534 }, qs.Sort(elements));
+        }
+    }
+}
