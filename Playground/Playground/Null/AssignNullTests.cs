82e908dced9da520991d3ebb44f14b150f99d6dd
Mateusz Kopij <sagasu@gmail.com>
2015-03-03 14:31:25 +0000

    checking null assignments.

---
+++ Playground/Playground/Null/AssignNullTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Playground.Null
+{
+    [TestFixture]
+    class AssignNullTests
+    {
+        [Test]
+        public void TestNullAsignementInMethod()
+        {
+            var c = new Cut();
+            Assert.NotNull(c);
+
+            c = new Cut().AssignNullAndReturn(c);
+            Assert.Null(c);
+
+            c = new Cut();
+            c.AssignNull(c);
+            Assert.NotNull(c);
+
+            var _ = new Cut();
+            c = new Cut();
+
+            c.AssignNull(_);
+            Assert.NotNull(c);
+            Assert.NotNull(_);
+        }
+    }
+}
---
+++ Playground/Playground/Null/Cut.cs
@@ -0,0 +1,23 @@
+﻿namespace Playground.Null
+{
+    public class Cut
+    {
+        public Cut AssignNullAndReturn(Cut c)
+        {
+            c = null;
+            return c;
+        }
+
+        public void AssignNull(Cut c)
+        {
+            c = null;
+        }
+
+        public void SetNull()
+        {
+            // Cannot do this operation
+            //this = null;
+        }
+
+    }
+}
