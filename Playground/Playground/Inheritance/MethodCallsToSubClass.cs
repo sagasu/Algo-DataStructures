1a85f25d659d66cc0e4985f46dde52de81c7206c
Mateusz Kopij <sagasu@gmail.com>
2015-08-20 14:52:12 +0100

    Testing method calls for super/sub classes

---
+++ Playground/Playground/Inheritance/MethodCallsToSubClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.Inheritance
+{
+    [TestFixture]
+    public class MethodCallsToSubClass
+    {
+        [Test]
+        public void SubClass_CallMethodOnSubClass_ExpectSubclassReturnValue()
+        {
+            var subClass = new SubClass();
+            Assert.AreEqual(2, subClass.Method());
+        }
+
+        [Test]
+        public void SubClassWithNew_CallMethodOnSubClassWithNew_ExpectSubClassWithNewReturnValue()
+        {
+            var subClass = new SubClassWithNew();
+            Assert.AreEqual(3, subClass.Method());
+        }
+    }
+
+    public class SuperClass
+    {
+        public int Method()
+        {
+            return 1;
+        }
+    }
+
+    public class SubClass : SuperClass
+    {
+        public int Method()
+        {
+            return 2;
+        }
+    }
+
+    public class SubClassWithNew : SuperClass
+    {
+        public new int Method()
+        {
+            return 3;
+        }
+    }
+}
