09ddbe302a4498cdbd4f76c3e2d1346d34d3a255
Mateusz Kopij <sagasu@gmail.com>
2016-05-23 15:07:39 +0100

    Checking how to cast URI, so it works with File.Exists

---
+++ Playground/Playground/FileManipulation/FileExistPlayground.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.FileManipulation
+{
+    [TestFixture]
+    public class FileExistPlayground
+    {
+        [Test]
+        public void FileExist() {
+            Assert.True(File.Exists(@"F:\worek2\github\playground\README.md"));
+        }
+
+        [Test]
+        public void FileExist2()
+        {
+            var uri = new UriBuilder(@"file:///F:\worek2\github\playground\README.md");
+            var path = uri.Path;
+            Assert.True(File.Exists(path));
+        }
+    }
+}
