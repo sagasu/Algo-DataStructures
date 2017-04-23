19e6da56b997ba4c9b7c52f0c4d4826a45e1cc4d
sagasu <sagasu@gmail.com>
2017-04-23 20:54:58 +0200

    adding dependency to mvc, and creating first controller with routing

---
+++ playCore/playCore/playCore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace playCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
--- playCore/playCore/playCore/Startup.cs
+++ playCore/playCore/playCore/Startup.cs
@@ -17,6 +17,8 @@ namespace playCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
+
             services.AddSingleton<ISensorDataService, SensorDataService>();
         }
 
@@ -30,16 +32,22 @@ namespace playCore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-                await next();
-            });
+           
+            app.UseMvcWithDefaultRoute();
+            //app.UseStaticFiles();
+            app.UseStatusCodePages();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            // default route
+            /*
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index"}
+                    );
             });
+            */
+
         }
     }
 }
