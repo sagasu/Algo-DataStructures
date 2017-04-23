1bd66d7bf32ee60d768f95913ad77fcbf4778018
sagasu <sagasu@gmail.com>
2017-04-23 17:13:17 +0200

    introducing budleconfig and task for gulp, setting copy after build

--- playCore/playCore/playCore/gulpfile.js
+++ playCore/playCore/playCore/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/*
+﻿/// <binding AfterBuild='copy' />
+/*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
@@ -28,6 +29,37 @@ paths.jsDest = paths.webroot + "js";
 paths.cssDest = paths.webroot + "css";
 paths.fontDest = paths.webroot + "css/fonts";
 
+gulp.task("min:js", function () {
+    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+        .pipe(concat(paths.jsDest + "/min/site.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("copy:js", function () {
+    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+        .pipe(gulp.dest(paths.jsDest));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+        .pipe(concat(paths.cssDest + "/min/site.min.css"))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("copy:css", function () {
+    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+        .pipe(gulp.dest(paths.cssDest));
+});
+
+gulp.task("copy:fonts", function () {
+    return gulp.src([paths.fonts])
+        .pipe(gulp.dest(paths.fontDest));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+gulp.task("copy", ["copy:js", "copy:css", "copy:fonts"]);
 
 gulp.task('default', function () {
     // place code for your default task here
