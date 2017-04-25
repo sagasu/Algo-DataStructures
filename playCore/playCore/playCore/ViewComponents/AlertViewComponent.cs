d128e21ff1b3f55c6bc5cfbc5c82b314ee0b472e
sagasu <sagasu@gmail.com>
2017-04-25 21:15:42 +0200

    adding view components

---
+++ playCore/playCore/playCore/ViewComponents/AlertViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using playCore.Options;
+using playCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace playCore.ViewComponents
+{
+    public class AlertViewComponent: ViewComponent
+    {
+        private readonly ISensorDataService sensorDataService;
+        private readonly ThresholdOptions thresholdConfig;
+
+        public AlertViewComponent(ISensorDataService sensorDataService, IOptions<ThresholdOptions> thresholdConfig )
+        {
+            this.sensorDataService = sensorDataService;
+            this.thresholdConfig = thresholdConfig.Value;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new List<string>();
+
+            if (sensorDataService.GetFishMotionPercentage().Value > thresholdConfig.FishMotionMax)
+                viewModel.Add("Too much fish activity");
+            if (sensorDataService.GetFishMotionPercentage().Value < thresholdConfig.FishMotionMin)
+                viewModel.Add("Looks like we have some dead fish");
+
+            if (sensorDataService.GetLightIntensityLumens().Value > thresholdConfig.LightIntensityMax)
+                viewModel.Add("Bright light, bright light!");
+            if (sensorDataService.GetLightIntensityLumens().Value < thresholdConfig.LightIntensityMin)
+                viewModel.Add("It's dark out here");
+
+            if (sensorDataService.GetWaterOpacityPercentage().Value > thresholdConfig.WaterOpacityMax)
+                viewModel.Add("The fish can't see you");
+            if (sensorDataService.GetWaterOpacityPercentage().Value < thresholdConfig.WaterOpacityMin)
+                viewModel.Add("Water too clean");
+
+            if (sensorDataService.GetWaterTemperatureFahrenheit().Value > thresholdConfig.WaterTemperatureMax)
+                viewModel.Add("Water too hot!");
+            if (sensorDataService.GetWaterTemperatureFahrenheit().Value < thresholdConfig.WaterTemperatureMin)
+                viewModel.Add("Water too cold!");
+
+            return View(viewModel);
+        }
+    }
+}
