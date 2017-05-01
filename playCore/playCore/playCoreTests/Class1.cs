29ae077f88636702dd1ca0f2a75d640a25165570
sagasu <sagasu@gmail.com>
2017-05-01 13:56:25 +0200

    checking various features in C# 7

---
+++ playCore/playCore/playCoreTests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace playCoreTests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void DigitSeparator()
+        {
+
+            Assert.AreEqual(1_000_000, 1000000);
+
+        }
+
+        [TestMethod]
+        public void DigitSeparator()
+        {
+
+            Assert.AreEqual(1_000_000, 1000000);
+
+        }
+    }
+}
