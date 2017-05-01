da0621932f8710af258d9e4d7099ecab57a1e992
sagasu <sagasu@gmail.com>
2017-05-01 09:29:03 +0200

    adding test to check nullability for events

---
+++ playCore/playCore/playCoreTests/EventHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace playCoreTests
+{
+    [TestClass]
+    class EventHandlerTest
+    {
+        public event EventHandler Clicked;
+
+        [TestMethod]
+        public void TestNullability()
+        {
+            // nice way to check if event is not null
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
