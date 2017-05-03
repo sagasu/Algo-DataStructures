26f44a413f552338c7011e3e047f5952096b9c63
sagasu <sagasu@gmail.com>
2017-05-04 00:02:37 +0200

    unifying name of TuplesTest

--- playCore/playCore/playCoreTests/TuplesTest.cs
+++ playCore/playCore/playCoreTests/TuplesTest.cs
@@ -6,15 +6,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace playCoreTests
 {
     [TestClass]
-    public class Tuples
+    public class TuplesTest
     {
 
         [TestMethod]
-        public void TestTuples()
+        public void TestTuples_ReturnedAsTwoVariables_ExpectAutoCastingToWork()
         {
             var (a, b) = returnSample();
             Assert.AreEqual(2, a);
             Assert.AreEqual("foo", b);
+        }
+
+        [TestMethod]
+        public void TestTuples_ReturnedAsSingleVariable_ExpectedToSplitItNicely()
+        {
+            var tuple = returnSample();
+            Assert.AreEqual(2, tuple.a);
+            Assert.AreEqual("foo", tuple.b);
         }
 
         // need to install System.ValueType to make it work
