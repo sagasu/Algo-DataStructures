b3a7085e514dfdb9bccfbee3dad19a69c5a2fcff
sagasu <sagasu@gmail.com>
2017-05-04 00:10:25 +0200

    testing nested tuples

--- playCore/playCore/playCoreTests/TuplesTest.cs
+++ playCore/playCore/playCoreTests/TuplesTest.cs
@@ -25,6 +25,21 @@ namespace playCoreTests
             Assert.AreEqual("foo", tuple.b);
         }
 
+        [DataTestMethod]
+        [DataRow(10, 10, "Foo")]
+        public void TestTuples_NestingTuple_ExpectReturningNestedTuple(int a, double b, string c)
+        {
+            var (returnedInt, (returnedDouble, returnedString)) = GetNestedTuple(a, b, c);
+            Assert.AreEqual(a, returnedInt);
+            Assert.AreEqual(b, returnedDouble);
+            Assert.AreEqual(c, returnedString);
+        }
+
+        private (int a, (double b, string c)) GetNestedTuple(int a, double b, string c)
+        {
+            return (a, (b, c));
+        }
+
         // need to install System.ValueType to make it work
         (int a, string b) returnSample()
         {
