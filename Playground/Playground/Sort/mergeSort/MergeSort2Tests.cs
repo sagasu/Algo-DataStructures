9833898ef3ab6edc6111310aa591434072824cbc
Mateusz Kopij <sagasu@gmail.com>
2017-10-18 14:25:13 +0100

    working on new mergeSort approach

---
+++ Playground/Playground/Sort/mergeSort/MergeSort2Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.Sort.mergeSort
+{
+    [TestFixture]
+    class MergeSort2Tests
+    {
+        [TestCase(new []{4,2,3,1}, new[] { 1, 2, 3, 4 })]
+        public void Sort_UnsortedArray_ExpectSorted(int[] original, int[] expected)
+        {
+            //Sort sorts in place, so original array is going to be sorted.
+            new MergeSort2().Sort(original);
+            Assert.AreEqual(expected, original);
+        }
+    }
+}
