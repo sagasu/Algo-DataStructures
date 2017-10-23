f43a03a04ddfd91313d35dd46b8e77de065b67f7
Mateusz Kopij <sagasu@gmail.com>
2017-10-23 14:24:38 +0100

    print subsets of set

---
+++ Playground/Playground/PrintAllSubsets/PrintAllSubsets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.PrintAllSubsets
+{
+    class PrintAllSubsets
+    {
+        public void PrintAllKSizeSubsets(int[] set, int k)
+        {
+            PrintAllKSizeSubsets(set, new bool[set.Length], 0, k, 0);
+        }
+
+        private void PrintAllKSizeSubsets(int[] set, bool[] used, int index, int k, int currentSize)
+        {
+            if (currentSize == k)
+            {
+                for (int i = 0; i < set.Length; i++)
+                {
+                    if (used[i])
+                    {
+                        Console.Out.Write(set[i] + " ");
+                    }
+                }
+                Console.Out.WriteLine();
+                return;
+            }
+
+            if (index >= set.Length)
+                return;
+
+            used[index] = true;
+            PrintAllKSizeSubsets(set, used, index + 1, k, currentSize + 1);
+            used[index] = false;
+            PrintAllKSizeSubsets(set, used, index + 1, k, currentSize);
+        }
+    }
+}
---
+++ Playground/Playground/PrintAllSubsets/PrintAllSubsetsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Playground.PrintAllSubsets
+{
+    [TestFixture]
+    class PrintAllSubsetsTests
+    {
+        [TestCase(new int[]{1,2,3,4}, 3)]
+        public void PrintAllSubsets_TestCase_ExpectedResult(int[] set, int k)
+        {
+            new PrintAllSubsets().PrintAllKSizeSubsets(set, k);
+
+            //cheating
+            Assert.True(true);
+        }
+    }
+}
