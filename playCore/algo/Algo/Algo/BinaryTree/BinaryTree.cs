7bdbd0fd74e09c47a6d29f121fc2941048506374
Mateusz Kopij <sagasu@gmail.com>
2020-05-13 19:21:22 +0100

    adding simple algo project

---
+++ playCore/algo/Algo/Algo/BinaryTree/BinaryTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algo.BinaryTree
+{
+    class Node
+    {
+        Node Left;
+        Node Right;
+        int value;
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+
+        public bool Contains(int value) {
+            if (value == this.value)
+            {
+                return true;
+            } else if (value < this.value) {
+                if (Left == null)
+                    return false;
+
+                return Left.Contains(value);
+            }
+
+            if (Right == null)
+                return false;
+            return Right.Contains(value);
+        }
+
+        public void PrintInOrder() {
+            if (Left != null)
+                Left.PrintInOrder();
+
+            Console.Out.WriteLine(value);
+
+            if (Right != null)
+                Right.PrintInOrder();
+        }
+
+        // If there are two nodes with this same value they will be kept in Left node
+        public void Insert(int value) {
+            if (value <= this.value)
+            {
+                if (Left == null)
+                {
+                    Left = new Node(value);
+                    return;
+                }
+                Left.Insert(value);
+            } else {
+                if (Right == null) {
+                    Right = new Node(value);
+                    return;
+                }
+                Right.Insert(value);
+            }
+        }
+
+    }
+}
---
+++ playCore/algo/Algo/Algo/Graph/Graph.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algo.Graph
+{
+    public class Graph
+    {
+        Dictionary<int,Node> Nodes = new Dictionary<int, Node>();
+
+        class Node {
+            public int id;
+            public List<Node> Neighbours { get; set; }
+
+            public Node(int id)
+            {
+                Neighbours = new List<Node>();
+                this.id = id;
+            }
+        }
+
+        Node GetNode(int id) {
+            return Nodes[id];
+        }
+
+        public void AddEdge(int source, int destination) {
+            var s = GetNode(source);
+            var d = GetNode(destination);
+            s.Neighbours.Add(d);
+            d.Neighbours.Add(s);
+        }
+
+        public bool HasPathDFS(int source, int destination) {
+            var s = GetNode(source);
+            var d = GetNode(destination);
+            var visitedNodesIds = new HashSet<int>();
+
+            return HasPathDFS(s, d, visitedNodesIds);
+        }
+
+        private bool HasPathDFS(Node s, Node d, HashSet<int> visitedNodesIds)
+        {
+            if (s.id == d.id)
+                return true;
+
+            if (visitedNodesIds.Contains(s.id))
+                return false;
+
+            visitedNodesIds.Add(s.id);
+
+            foreach (var n in s.Neighbours) {
+                var found = HasPathDFS(n, d, visitedNodesIds);
+                if (found)
+                    return true;
+            }
+            return false;
+
+        }
+    }
+}
