b4f4405253828de2f49eb1086b842d024cf139ab
Mateusz Kopij <sagasu@gmail.com>
2020-05-18 17:10:54 +0100

    adding fibonacci.

---
+++ playCore/algo/Algo/Algo/Fibonacci/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Algo.Fibonacci
+{
+    public class Fibonacci
+    {
+        private Dictionary<int, int> Mem = new Dictionary<int, int>();
+        public int Calculate(int n) {
+            if (Mem.ContainsKey(n))
+                return Mem[n];
+
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+
+            var fib = Calculate(n - 1) + Calculate(n - 2);
+            Mem.Add(n, fib);
+
+            return fib;
+        }
+    }
+}
