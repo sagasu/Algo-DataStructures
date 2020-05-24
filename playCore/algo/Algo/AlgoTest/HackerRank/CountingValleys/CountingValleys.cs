a01c6be43c3836d90e9b197a2182acc5f83c1ef7
Mateusz Kopij <sagasu@gmail.com>
2020-05-24 13:33:29 +0100

    adding counting valleys solution

---
+++ playCore/algo/Algo/AlgoTest/HackerRank/CountingValleys/CountingValleys.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.HackerRank.CountingValleys
+{
+    [TestClass]
+    public class CountingValleys
+    {
+        [TestMethod]
+        public void CountingValleysTest()
+        {
+            var n = 8;
+            var s = "UDDDUDUU";
+            Assert.AreEqual(1, GetNumberOfValleys(n, s));
+        }
+
+        public int GetNumberOfValleys(int n, string s)
+        {
+            var valleyCount = 0;
+            var level = 0;
+            foreach (var direction in s)
+            {
+                if (direction == 'U')
+                {
+                    level = level + 1;
+                }
+                else
+                {
+                    if (level == 0)
+                        valleyCount += 1;
+                    level = level - 1;
+                }
+
+
+            }
+            return valleyCount;
+        }
+    }
+}
