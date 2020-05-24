9d126f506e9aab537981edfe3ca6f9c06d7e08ea
Mateusz Kopij <sagasu@gmail.com>
2020-05-24 14:09:56 +0100

    adding in order tree traversal

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/TreeInOrder/InOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.TreeInOrder
+{
+    class InOrder
+    {
+        List<int> orderPrint = new List<int>();
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return orderPrint;
+
+            if (root.left != null)
+                InorderTraversal(root.left);
+
+            orderPrint.Add(root.val);
+
+            if (root.right != null)
+                InorderTraversal(root.right);
+
+            return orderPrint;
+        }
+    }
+}
---
+++ playCore/algo/Algo/AlgoTest/LeetCode/TreePreOrder/PreOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.TreePreOrder
+{
+
+    class PreOrder
+    {
+        List<int> orderPrint = new List<int>();
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return orderPrint;
+
+            orderPrint.Add(root.val);
+            if (root.left != null)
+            {
+                PreorderTraversal(root.left);
+            }
+            if (root.right != null)
+            {
+                PreorderTraversal(root.right);
+            }
+            return orderPrint;
+        }
+    }
+}
