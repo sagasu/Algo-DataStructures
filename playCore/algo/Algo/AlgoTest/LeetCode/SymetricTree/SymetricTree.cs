7d084086251e769bab283750c0ad2d879f3b34ac
Mateusz Kopij <sagasu@gmail.com>
2020-05-24 19:30:08 +0100

    symetric trees with expressions

--- playCore/algo/Algo/AlgoTest/LeetCode/SymetricTree/SymetricTree.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/SymetricTree/SymetricTree.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+//using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace AlgoTest.LeetCode.SymetricTree
@@ -14,8 +16,11 @@ namespace AlgoTest.LeetCode.SymetricTree
             if (root == null)
                 return true;
 
+            //Traverse(root, inOrder, (treeNode) =>treeNode.left, treeNode => treeNode.right );
+            //Traverse(root, symetricInOrder, (treeNode) => treeNode.left, treeNode => treeNode.right);
             TraverseInOrder(root);
             TraverseInSymOrder(root);
+
             for (var i = 0; i < inOrder.Count; i++)
             {
                 if (inOrder[i] != symetricInOrder[i])
@@ -25,15 +30,53 @@ namespace AlgoTest.LeetCode.SymetricTree
             return true;
         }
 
+
+        private void Traverse(TreeNode root, List<int> order, System.Linq.Expressions.Expression<Func<TreeNode,TreeNode>> firstExpression, System.Linq.Expressions.Expression<Func<TreeNode, TreeNode>> secondExpression)
+        {
+            order.Add(root.val);
+
+            var firstChild = firstExpression.Compile().Invoke(root);
+            if (firstChild != null)
+            {
+                Traverse(firstChild, order, firstExpression, secondExpression);
+            }
+            else
+            {
+                order.Add(-1);
+            }
+
+            var secondChild = secondExpression.Compile().Invoke(root);
+            if (secondChild != null)
+            {
+                Traverse(secondChild, order, firstExpression, secondExpression);
+            }
+            else
+            {
+                order.Add(-1);
+            }
+        }
+
         private void TraverseInOrder(TreeNode root)
         {
             inOrder.Add(root.val);
 
-            if(root.left != null)
+            if (root.left != null)
+            {
                 TraverseInOrder(root.left);
+            }
+            else
+            {
+                inOrder.Add(-1);
+            }
 
             if (root.right != null)
+            {
                 TraverseInOrder(root.right);
+            }
+            else
+            {
+                inOrder.Add(-1);
+            }
         }
 
         private void TraverseInSymOrder(TreeNode root)
@@ -41,10 +84,22 @@ namespace AlgoTest.LeetCode.SymetricTree
             symetricInOrder.Add(root.val);
 
             if (root.right != null)
+            {
                 TraverseInSymOrder(root.right);
+            }
+            else
+            {
+                symetricInOrder.Add(-1);
+            }
 
             if (root.left != null)
+            {
                 TraverseInSymOrder(root.left);
+            }
+            else
+            {
+                symetricInOrder.Add(-1);
+            }
         }
     }
 }
