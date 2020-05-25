0ab0202a7ab9ec82933d87cb6aea550c6c0cb62c
Mateusz Kopij <sagasu@gmail.com>
2020-05-25 19:42:53 +0100

    linked list DS

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/LinkedListDataStructure/LinkedListDS.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.LinkedListDataStructure
+{
+    class LinkedListDS
+    {
+        private Node Head;
+
+        class Node
+        {
+            public int Data;
+            public Node Next;
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (Head == null)
+            {
+                Head = new Node(value);
+                return;
+            }
+
+            var current = Head;
+            while (true)
+            {
+                if (current.Next == null)
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+
+            current.Next = new Node(value);
+        }
+
+        public void Prepend(int value)
+        {
+            if (Head == null)
+            {
+                Head = new Node(value);
+            }
+
+            var oldHead = Head;
+            Head = new Node(value) {Next = oldHead};
+
+        }
+
+        public void DeleteByValue(int value)
+        {
+            if(Head == null)
+                throw new ArgumentException("Linked list is empty");
+
+            if (Head.Data == value)
+            {
+                Head = null;
+                return;
+            }
+
+
+            var current = Head;
+            while (true)
+            {
+                if (current.Next == null)
+                {
+                    throw new ArgumentException("no such value in a list");
+                }
+
+                if (current.Next.Data == value)
+                {
+                    if (current.Next.Next != null)
+                    {
+                        current.Next = current.Next.Next;
+                    }
+                    else
+                    {
+                        current.Next = null;
+                    }
+                }
+
+                current = current.Next;
+            }
+        }
+    }
+}
