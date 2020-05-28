2f95d14ca5293d7a88ecba773bd01bfdc3a83076
Mateusz Kopij <sagasu@gmail.com>
2020-05-28 19:44:53 +0100

    adding reversed int

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReverseInt/ReverseInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ReverseInt
+{
+    class ReverseInt
+    {
+        public int Reverse(int x)
+        {
+            var isPositive = true;
+            var lastElement = 0;
+            if (x < 0)
+            {
+                isPositive = false;
+                lastElement = 1;
+            }
+
+            var reversedInt = new StringBuilder();
+
+            var xs = x.ToString();
+            for (var i = xs.Length - 1; i >= lastElement; i--)
+            {
+                reversedInt.Append(xs[i]);
+            }
+
+            var revInt = reversedInt.ToString();
+            var rev = 0;
+            return int.TryParse(revInt, out rev) ? isPositive ? rev : -1 * rev : 0;
+        }
+    }
+}
