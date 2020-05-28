7867b557d735925a6bb9707d2427eae8dba3cd96
Mateusz Kopij <sagasu@gmail.com>
2020-05-28 20:36:19 +0100

    validate anagram

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/AnagramCheck/AnagramCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.AnagramCheck
+{
+    [TestClass]
+    public class AnagramCheck
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(IsAnagram("anagram", "nagaram"));
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            var hs = new Dictionary<char, int>();
+            var sa = s.ToCharArray();
+            var ta = t.ToCharArray();
+            foreach (var c in sa)
+            {
+                hs[c] = hs.ContainsKey(c) ? hs[c] + 1 : 1;
+            }
+
+            foreach (var i in ta)
+            {
+                if (!hs.ContainsKey(i))
+                    return false;
+
+                hs[i] -= 1;
+            }
+
+            foreach (var hsKey in hs.Keys)
+            {
+                if (hs[hsKey] != 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
