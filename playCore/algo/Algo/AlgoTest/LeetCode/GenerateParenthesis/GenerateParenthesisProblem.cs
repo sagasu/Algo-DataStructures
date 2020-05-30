fd5894d93386f372316b2a6385e8b3095d0c3da5
Mateusz Kopij <sagasu@gmail.com>
2020-05-30 19:17:49 +0100

    Generate parenthesis problem

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/GenerateParenthesis/GenerateParenthesisProblem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.GenerateParenthesis
+{
+    [TestClass]
+    public class GenerateParenthesisProblem
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var result = GenerateParenthesis(3);
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+            GenerateParenthesis(n, "", result, 0);
+            return result;
+        }
+
+        public void GenerateParenthesis(int n, string current, IList<string> result, int close)
+        {
+            if (n == 0)
+            {
+                while (close != 0)
+                {
+                    current += ")";
+                    close -= 1;
+                }
+                result.Add(current);
+
+                return;
+            }
+
+            GenerateParenthesis(n - 1, current + "(", result, close+1);
+            if(close > 0)
+                GenerateParenthesis(n, current + ")", result, close-1);
+        }
+    }
+}
