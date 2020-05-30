ec018c0e66911a68ba3508d2ba598dab378e6d74
Mateusz Kopij <sagasu@gmail.com>
2020-05-30 15:20:09 +0100

    working 3sum in accepted time

--- playCore/algo/Algo/AlgoTest/LeetCode/3Sum/3Sum.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/3Sum/3Sum.cs
@@ -12,44 +12,54 @@ namespace AlgoTest.LeetCode._3Sum
         [TestMethod]
         public void Test()
         {
-            var nums = new int[] {-1, 0, 1, 2, -1, -4};
-            var response1 = new int[] {-1, 0, 1};
-            var response2 = new int[] {-1, -1, 2};
+            //var nums = new int[] {-1, 0, 1, 2, -1, -4};
+            //var response1 = new int[] {-1, 0, 1};
+            //var response2 = new int[] {-1, -1, 2};
+
+            var nums = new[] {0, 0, 0, 0};
             var resp = ThreeSum(nums);
-            Assert.AreEqual(response1, resp[0]);
         }
 
         public IList<IList<int>> ThreeSum(int[] nums)
         {
             if (nums.Length < 3)
                 return new List<IList<int>>();
-
-            var result = new HashSet<ValueTuple<int, int, int>>();
+            ValueTuple<int, int> lastAdded = (int.MaxValue, int.MaxValue);
+            var result = new List<IList<int>>();
             Array.Sort(nums);
-            for (var i = 0; i < nums.Length-2; i++)
+            for (var i = 0; i < nums.Length - 2; i++)
             {
-                for (var j = i+1; j < nums.Length-1; j++)
-                {
-                    if (nums[i] + nums[j] > 0)
-                        break;
-                    for (var x = nums.Length - 1; x > j; x--)
-                    {
-                        var sum = nums[i] + nums[j] + nums[x];
-                        if (j >= x || sum < 0)
-                            break;
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
 
-                        if (sum == 0)
+                var pointerA = i + 1;
+                var pointerB = nums.Length - 1;
+                var searchedSum = nums[i] * -1;
+                while (pointerA < pointerB)
+                {
+                    var sum = nums[pointerA] + nums[pointerB];
+                    if (sum == searchedSum)
+                    {
+                        if (!(lastAdded.Item1 == nums[pointerA] && lastAdded.Item2 == nums[pointerB]))
                         {
-                            if (result.Contains((nums[i], nums[j], nums[x])))
-                                continue;
-                            result.Add((nums[i], nums[j], nums[x]));
+                            lastAdded = (nums[pointerA], nums[pointerB]);
+                            result.Add(new List<int> { nums[i], nums[pointerA], nums[pointerB] });
                         }
+                    }
+
+                    if (sum < searchedSum)
+                    {
+                        pointerA += 1;
+                    }
+                    else
+                    {
+                        pointerB -= 1;
                     }
                 }
             }
 
-            return result.Select(valueTuple => new List<int> {valueTuple.Item1, valueTuple.Item2, valueTuple.Item3}).Cast<IList<int>>().ToList();
-            
+            return result;
+
         }
     }
 }
