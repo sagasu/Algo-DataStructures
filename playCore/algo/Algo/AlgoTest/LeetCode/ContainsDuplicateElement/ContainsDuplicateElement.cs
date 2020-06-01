f9872f3f0f763357263b0fd8d25c9e3e8cd1981c
Mateusz Kopij <sagasu@gmail.com>
2020-06-01 09:50:12 +0100

    Contains Duplicates

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ContainsDuplicateElement/ContainsDuplicateElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ContainsDuplicateElement
+{
+    class ContainsDuplicateElement
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            IDictionary<int,int> dupsCount = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dupsCount.Keys.Contains(num))
+                    return true;
+                dupsCount.Add(num, 1);
+            }
+
+            return false;
+        }
+    }
+}
