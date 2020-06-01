2eda9fd9d6da56d91708a0d73196e03f02a1c205
Mateusz Kopij <sagasu@gmail.com>
2020-06-01 08:19:04 +0100

    Equalize Array

---
+++ playCore/algo/Algo/AlgoTest/HackerRank/EqualizeArray/EqualizeArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.HackerRank.EqualizeArray
+{
+    public class EqualizeArray
+    {
+        static int equalizeArray(int[] arr)
+        {
+            if (arr.Length == 0)
+                return 0;
+            IDictionary<int,int> countElements = new Dictionary<int, int>();
+            var max = 1;
+            foreach (var element in arr)
+            {
+                if (countElements.ContainsKey(element))
+                {
+                    countElements[element] += 1;
+                    if (countElements[element] > max)
+                        max = countElements[element];
+                }
+                else
+                {
+                    countElements.Add(element, 1);
+                }
+            }
+
+            return arr.Length - max;
+        }
+    }
+}
