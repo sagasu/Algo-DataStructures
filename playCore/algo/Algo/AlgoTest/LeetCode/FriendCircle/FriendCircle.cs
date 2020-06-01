9df9a50ff37892eda4b3b2c9ff5d68ec01748970
Mateusz Kopij <sagasu@gmail.com>
2020-06-01 16:25:51 +0100

    Friend Circle

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FriendCircle/FriendCircle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.FriendCircle
+{
+    class FriendCircle
+    {
+        HashSet<int> seen = new HashSet<int>();
+        private int count = 0;
+        public int FindCircleNum(int[][] M)
+        {
+            for (var i = 0; i < M.Length; i++)
+            {
+                if (!seen.Contains(i))
+                {
+                    count += 1;
+                    SearchDFS(i, M);
+                }
+            }
+
+            return count;
+        }
+
+        private void SearchDFS(int i, int[][] M)
+        {
+            seen.Add(i);
+            
+            for(var j =0;j < M[i].Length;j++)
+            {
+                if(M[i][j] == 1 && !seen.Contains(j))
+                    SearchDFS(j, M);
+            }
+        }
+    }
+}
