09a84f503106354ad892990a3bf7c750c0cd013c
Mateusz Kopij <sagasu@gmail.com>
2020-06-01 09:35:34 +0100

    Rotate Array Rigth

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/RotateArray/RotateArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.RotateArray
+{
+    class RotateArray
+    {
+        //Right
+        public void Rotate(int[] nums, int k)
+        {
+            var rotatedArray = new int[nums.Length];
+            Array.Copy(nums, rotatedArray, nums.Length);
+
+            var index = 0;
+            while (index < nums.Length)
+            {
+                var nextPosition = (index + k) % nums.Length;
+                nums[nextPosition] = rotatedArray[index];
+                index += 1;
+            }
+        }
+    }
+}
