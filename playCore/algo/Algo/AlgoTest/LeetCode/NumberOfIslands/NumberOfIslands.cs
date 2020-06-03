1b79d966011f6ab2d38616bda4f13bedde8cd902
Mateusz Kopij <sagasu@gmail.com>
2020-06-03 15:01:26 +0100

    Number of Islands

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/NumberOfIslands/NumberOfIslands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.NumberOfIslands
+{
+    class NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var count = 0;
+            for(var i = 0; i < grid.Length; i++)
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    count += 1;
+                    RemoveIsland(grid, i, j);
+                }
+            }
+        }
+
+        private void RemoveIsland(char[][] grid, int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] == 0)
+                return;
+
+            RemoveIsland(grid, i+1, j);
+            RemoveIsland(grid, i-1, j);
+            RemoveIsland(grid, i, j+1);
+            RemoveIsland(grid, i, j-1);
+        }
+    }
+}
