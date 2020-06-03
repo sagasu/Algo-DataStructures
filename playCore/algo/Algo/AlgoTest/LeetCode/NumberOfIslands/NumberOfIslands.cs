f4b3d48168ea1cecdca9ef1f9e8c90f0f9850f39
Mateusz Kopij <sagasu@gmail.com>
2020-06-03 17:11:27 +0100

    fixing number of islands

--- playCore/algo/Algo/AlgoTest/LeetCode/NumberOfIslands/NumberOfIslands.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/NumberOfIslands/NumberOfIslands.cs
@@ -12,19 +12,21 @@ namespace AlgoTest.LeetCode.NumberOfIslands
             for(var i = 0; i < grid.Length; i++)
             for (var j = 0; j < grid[i].Length; j++)
             {
-                if (grid[i][j] == 1)
+                if (grid[i][j] == '1')
                 {
                     count += 1;
                     RemoveIsland(grid, i, j);
                 }
             }
+
+            return count;
         }
 
         private void RemoveIsland(char[][] grid, int i, int j)
         {
-            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] == 0)
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] == '0')
                 return;
-
+            grid[i][j] = '0';
             RemoveIsland(grid, i+1, j);
             RemoveIsland(grid, i-1, j);
             RemoveIsland(grid, i, j+1);
