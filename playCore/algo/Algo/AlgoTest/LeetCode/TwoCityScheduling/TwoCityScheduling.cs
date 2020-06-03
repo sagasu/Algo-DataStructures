b80529c51585ee3f94bf72538347ba51221fbd1a
Mateusz Kopij <sagasu@gmail.com>
2020-06-03 12:49:59 +0100

    naive implementation of two city cost problem

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/TwoCityScheduling/TwoCityScheduling.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.TwoCityScheduling
+{
+    class TwoCityScheduling
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            var sum = 0;
+            foreach (var cost in costs)
+            {
+                sum += Math.Min(cost[0], cost[1]);
+            }
+
+            return sum;
+        }
+    }
+}
