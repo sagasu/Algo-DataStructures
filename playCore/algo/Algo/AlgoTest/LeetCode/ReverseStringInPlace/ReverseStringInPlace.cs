e7be7a717dcc44746eec482c3633d2f0c8aa5458
Mateusz Kopij <sagasu@gmail.com>
2020-06-04 08:25:11 +0100

    Reverse string in place

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReverseStringInPlace/ReverseStringInPlace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ReverseStringInPlace
+{
+    class ReverseStringInPlace
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s.Length == 1)
+                return;
+            var startIndex = 0;
+            var endIndex = s.Length-1;
+
+            while (startIndex < endIndex)
+            {
+                var replacement = s[startIndex];
+                s[startIndex] = s[endIndex];
+                s[endIndex] = replacement;
+
+                startIndex += 1;
+                endIndex -= 1;
+            }
+
+        }
+    }
+}
