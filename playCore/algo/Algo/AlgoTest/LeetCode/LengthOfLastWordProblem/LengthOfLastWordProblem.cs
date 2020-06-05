ad3d7d53eadf58db48d3c8b77169c7c47c2061a7
Mateusz Kopij <sagasu@gmail.com>
2020-06-05 23:53:38 +0100

    length of last word problem

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/LengthOfLastWordProblem/LengthOfLastWordProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.LengthOfLastWordProblem
+{
+    class LengthOfLastWordProblem
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+            s = s.Trim();
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            var elements = s.Split(' ');
+            var lastElement = elements[elements.Length - 1];
+            return lastElement.Length;
+        }
+    }
+}
