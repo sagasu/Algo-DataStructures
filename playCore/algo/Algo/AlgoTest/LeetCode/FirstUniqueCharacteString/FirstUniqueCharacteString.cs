704998635ad765c05a14134859b93318a6c2dbf5
Mateusz Kopij <sagasu@gmail.com>
2020-06-06 08:54:15 +0100

    First Unique Character in String index

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FirstUniqueCharacteString/FirstUniqueCharacteString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.FirstUniqueCharacteString
+{
+    [TestClass]
+    public class FirstUniqueCharacteString
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(3, FirstUniqChar("dddccdbba"));
+        }
+
+        IDictionary<char,ValueTuple<int, int>> dic = new Dictionary<char, ValueTuple<int, int>>();
+        public int FirstUniqChar(string s)
+        {
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    var d = dic[s[i]];
+                    dic[s[i]] = new ValueTuple<int,int>(d.Item1+1, d.Item2);
+                }
+                else
+                {
+                    dic[s[i]] = new ValueTuple<int,int>(1,i);
+                }
+            }
+
+            int position = 0;
+            foreach (var dicKey in dic.Keys)
+            {
+                if (dic[dicKey] == 1)
+                    return position;
+
+                position += 1;
+            }
+
+            return -1;
+        }
+    }
+}
