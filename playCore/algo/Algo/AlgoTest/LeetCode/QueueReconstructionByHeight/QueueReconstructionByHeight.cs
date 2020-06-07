4484e0fc6826edd909f610ee1b4bb1933e34cf2a
Mateusz Kopij <sagasu@gmail.com>
2020-06-07 18:50:01 +0100

    Queue Reconstruction by height

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/QueueReconstructionByHeight/QueueReconstructionByHeight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.QueueReconstructionByHeight
+{
+    [TestClass]
+    public class QueueReconstructionByHeight
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var t = new int[][] {new[] {7, 0}, new[] {4, 4}, new[] {7, 1}, new[] {5, 0}, new[] {6, 1}, new[] {5, 2}};
+            ReconstructQueue(t);
+        }
+
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            var sorted = people.OrderByDescending(x => x[0]).ThenBy(x => x[1]);
+            var ret = new List<int[]>(people.Length);
+            foreach (var elements in sorted)
+            {
+                ret.Insert(elements[1], elements);
+            }
+
+            return ret.ToArray();
+        }
+    }
+}
