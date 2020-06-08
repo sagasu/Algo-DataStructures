7b1d6c91c9c1f73b7775a2da5f976f876987470a
Mateusz Kopij <sagasu@gmail.com>
2020-06-08 08:47:23 +0100

    Fixing two city scheduling

--- playCore/algo/Algo/AlgoTest/LeetCode/TwoCityScheduling/TwoCityScheduling.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/TwoCityScheduling/TwoCityScheduling.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgoTest.LeetCode.TwoCityScheduling
 {
-    class TwoCityScheduling
+    [TestClass]
+    public class TwoCityScheduling
     {
+        [TestMethod]
+        public void Test()
+        {
+            //var t = new int[][] {new[]{10, 20}, new[] { 30, 200}, new[] { 400, 50}, new[] { 30, 20}};
+            var t = new int[][] {new[]{10, 20}, new[] { 30, 200}, new[] { 400, 50}, new[] { 30, 20}, new[] { 10, 20 }, new[] { 10, 20 } };
+            TwoCitySchedCost(t);
+        }
+
         public int TwoCitySchedCost(int[][] costs)
         {
+            var sorted = costs.OrderByDescending(x => Math.Abs(x[0] - x[1]));
+
             var sum = 0;
-            foreach (var cost in costs)
+            var fligthsToA = 0;
+            var flightsToB = 0;
+            var sumOfFlights = costs.Length / 2;
+            foreach (var cost in sorted)
             {
-                sum += Math.Min(cost[0], cost[1]);
+                if (cost[0] < cost[1] && (fligthsToA < sumOfFlights) || flightsToB >= sumOfFlights)
+                {
+                    fligthsToA += 1;
+                    sum += cost[0];
+                }
+                else
+                {
+                    flightsToB += 1;
+                    sum += cost[1];
+                }
+                
             }
 
             return sum;
