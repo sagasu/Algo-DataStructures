f79d4655a1b04088a80f12f29560969b6b8a18b7
Mateusz Kopij <sagasu@gmail.com>
2020-06-09 14:29:26 +0100

    Is subsequence problem

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/IsSubsequenceProblem/IsSubsequenceProblem.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.IsSubsequenceProblem
+{
+    [TestClass]
+    public class IsSubsequenceProblem
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s))
+                return true;
+
+            var index = 0;
+            for (var i = 0; i < t.Length; i++)
+            {
+
+                if (s.Length == index)
+                    return true;
+
+                if (t[i] == s[index])
+                {
+                    index += 1;
+                }
+            }
+
+            return s.Length == index;
+        }
+    }
+}
