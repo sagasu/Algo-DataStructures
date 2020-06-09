a2d12128cd2d5cbc42ff3e9688fb88d00dbd682b
Mateusz Kopij <sagasu@gmail.com>
2020-06-09 14:45:33 +0100

    naive power of two implementation

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/PowerOfTwo/PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.PowerOfTwo
+{
+    class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 1)
+                return true;
+
+            var pow = 1;
+            while (pow < n)
+            {
+                pow = pow * 2;
+            }
+            return n == pow;
+        }
+    }
+}
