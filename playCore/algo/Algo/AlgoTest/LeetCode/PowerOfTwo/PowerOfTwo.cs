aef3216d706aa9d5848a9f2bb5bc56d1e2a25600
Mateusz Kopij <sagasu@gmail.com>
2020-06-09 15:11:16 +0100

    power of two bit manipulation

--- playCore/algo/Algo/AlgoTest/LeetCode/PowerOfTwo/PowerOfTwo.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/PowerOfTwo/PowerOfTwo.cs
@@ -1,22 +1,38 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AlgoTest.LeetCode.PowerOfTwo
 {
-    class PowerOfTwo
+    [TestClass]
+    public class PowerOfTwo
     {
+        [TestMethod]
+        public void Test() {
+            Assert.AreEqual(true, IsPowerOfTwo(16));
+            Assert.AreEqual(true, IsPowerOfTwo(512));
+        }
         public bool IsPowerOfTwo(int n)
         {
             if (n == 1)
                 return true;
 
-            var pow = 1;
-            while (pow < n)
-            {
-                pow = pow * 2;
+            if (n <= 0)
+                return false;
+
+            var bits = Convert.ToString(n, 2);//new BitArray(new int[]{n});
+
+            if (bits[0] != '1')
+                return false;
+
+            for (var i = 1; i < bits.Length; i++) {
+                if (bits[i] != '0')
+                    return false;
             }
-            return n == pow;
+
+            return true;
         }
     }
 }
