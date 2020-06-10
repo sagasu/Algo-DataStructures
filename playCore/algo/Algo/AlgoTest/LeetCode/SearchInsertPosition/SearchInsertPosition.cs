f1db4d7c0a98038bbd2582b04844fe9ca25aad0d
Mateusz Kopij <sagasu@gmail.com>
2020-06-10 09:30:08 +0100

    search insert position

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/SearchInsertPosition/SearchInsertPosition.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SearchInsertPosition
+{
+    [TestClass]
+    public class SearchInsertPosition
+    {
+        [TestMethod]
+        public void Test() {
+            var t = new int[] { 1, 3, 5, 6 };
+            Assert.AreEqual(2, SearchInsert(t, 5));
+
+            //var t = new int[]{ 1, 3, 5, 6 };
+            //Assert.AreEqual(1, SearchInsert(t, 2));
+        }
+        public int SearchInsert(int[] nums, int target)
+        {
+            var low = 0;
+            var hight = nums.Length - 1;
+            while (low < hight) {
+                var mid = (hight + low) / 2;
+
+                if (target > nums[mid])
+                {
+                    low = mid + 1;
+                }
+                else {
+                    hight = mid;
+                }
+            }
+            return low;
+        }
+    }
+}
