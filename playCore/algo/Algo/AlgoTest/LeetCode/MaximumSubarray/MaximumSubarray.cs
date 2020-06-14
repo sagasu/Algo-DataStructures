e97ef396aeaf7bca64906327d76be792ff99aa86
Mateusz Kopij <sagasu@gmail.com>
2020-06-14 17:52:03 +0100

    maximum subarray value

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/MaximumSubarray/MaximumSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.MaximumSubarray
+{
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+
+            if (nums.Length == 1)
+                return nums[0];
+
+            var previous = nums[0];
+            var max = previous;
+
+            for (var i = 1; i < nums.Length; i++) {
+                var current = Math.Max(nums[i], previous + nums[i]);
+                if (current > max)
+                    max = current;
+                previous = current;
+            }
+
+            return max;
+        }
+    }
+}
