6ba3c56933e84cad2e0d837fb623139d0d1dce02
Mateusz Kopij <sagasu@gmail.com>
2020-06-14 18:49:45 +0100

    better solution for reversed linked list

--- playCore/algo/Algo/AlgoTest/LeetCode/ReverseLinkedList/ReverseLinkedList.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReverseLinkedList/ReverseLinkedList.cs
@@ -9,14 +9,14 @@ namespace AlgoTest.LeetCode.ReverseLinkedList
     {
         public class ListNode
         {
-              public int val;
-              public ListNode next;
-              public ListNode(int val = 0, ListNode next = null)
-               {
-                        this.val = val;
-                        this.next = next;
-              }
-          }
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
         public ListNode ReverseList(ListNode head)
         {
             if (head == null)
@@ -27,18 +27,42 @@ namespace AlgoTest.LeetCode.ReverseLinkedList
             var curent = head;
             var stack = new Stack<int>();
 
-            while (curent != null) {
+            while (curent != null)
+            {
                 stack.Push(curent.val);
                 curent = curent.next;
             }
 
             ListNode ret = new ListNode();
             ListNode next = ret;
-            while(stack.Count > 0) {
+            while (stack.Count > 0)
+            {
                 next.next = new ListNode(stack.Pop());
                 next = next.next;
             }
             return ret.next;
+        }
+
+        // Different solution, by repointing next to previous
+        public ListNode ReverseList2(ListNode head)
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return head;
+
+            var current = head;
+            ListNode previous = null;
+            while (true)
+            {
+                var next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+                if (current == null)
+                    break;
+            }
+            return previous;
         }
     }
 }
