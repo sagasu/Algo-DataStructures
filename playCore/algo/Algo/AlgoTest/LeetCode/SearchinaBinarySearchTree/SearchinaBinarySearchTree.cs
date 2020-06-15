cca043dd720b780161e6a7e7ba9fab1d7e65322b
Mateusz Kopij <sagasu@gmail.com>
2020-06-15 21:18:41 +0100

    Search in a Binary Search Tree

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/SearchinaBinarySearchTree/SearchinaBinarySearchTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SearchinaBinarySearchTree
+{
+    class SearchinaBinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+
+            if (root.val == val)
+                return root;
+
+            if (root.val < val)
+                return SearchBST(root.right, val);
+
+            if (root.val > val)
+                return SearchBST(root.left, val);
+
+            return null;
+        }
+    }
+}
