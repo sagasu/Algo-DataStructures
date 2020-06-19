a57e3e9e1c3f09363b4a996b2f692a28d4003e4e
Mateusz Kopij <sagasu@gmail.com>
2020-06-19 12:42:35 +0100

    trying to implementa longest substring in a naive way (not finished), but I am switching to a dynamic programming because I can see that it will be a better choice.

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/LongestDuplicateSubstring/LongestDuplicateSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.LongestDuplicateSubstring
+{
+    class LongestDuplicateSubstringProblem
+    {
+        public string LongestDupSubstring(string S)
+        {
+            if (string.IsNullOrEmpty(S))
+                return "";
+
+            var longestSub = 0;
+            var index = 0;
+            var currentLength = 0;
+            var isSub = false;
+            for (var i = 0; i < S.Length; i++) {
+                for (var j = index; j <= i; j++) {
+                    if (S[i] == S[j])
+                    {
+                        currentLength += 1;
+                        isSub = true;
+                        index = i;
+                    }
+                    else {
+                        if (isSub) {
+                            if (currentLength > longestSub)
+                                longestSub = currentLength;
+                        }
+                        currentLength = 0;
+                        isSub = false;
+                    }
+                }
+            }
+        }
+    }
+}
