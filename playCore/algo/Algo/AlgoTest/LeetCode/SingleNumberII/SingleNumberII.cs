8cea5c9a47a023d14bca2ab41a3a58b3ff75987a
Mateusz Kopij <sagasu@gmail.com>
2020-06-30 15:06:35 +0100

    single number 2

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/SingleNumberII/SingleNumberII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SingleNumberII
+{
+    public class SingleNumberII
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    dic[num] += 1;
+                    if (dic[num] == 3)
+                        dic.Remove(num);
+                }
+                else
+                {
+                    dic.Add(num,1);
+                }
+            }
+
+            return dic.First().Value;
+        }
+    }
+}
