5999561084278e93e14a86b1d3e525ef4e7074f0
Mateusz Kopij <sagasu@gmail.com>
2020-07-06 20:04:57 +0100

    introducing HashSet to detect cycles in next day computation.

--- playCore/algo/Algo/AlgoTest/LeetCode/PrisonCellsAfterNDays/PrisonCellsAfterNDays.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/PrisonCellsAfterNDays/PrisonCellsAfterNDays.cs
@@ -21,23 +21,56 @@ namespace AlgoTest.LeetCode.PrisonCellsAfterNDays
 
         public int[] PrisonAfterNDays(int[] cells, int N)
         {
+            var hashSet = new HashSet<string>();
+            var isCycle = false;
             var second = new int[cells.Length];
+            var cnt = 0;
             while (N > 0)
             {
-                second[0] = 0;
-                for (var i = 1; i < cells.Length - 1; i++)
+                GetSecond(cells, second);
+                var key = string.Join("", second);
+                if (hashSet.Contains(key))
                 {
-                    second[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+                    isCycle = true;
+                    break;
+                }
+                else
+                {
+                    cnt += 1;
+                    hashSet.Add(key);
                 }
 
-                second[cells.Length - 1] = 0;
                 N -= 1;
                 var temp = second;
                 second = cells;
                 cells = temp;
             }
 
+            if (isCycle)
+            {
+                N = N % cnt;
+                while (N > 0)
+                {
+                    GetSecond(cells, second);
+                    N -= 1;
+                    var temp = second;
+                    second = cells;
+                    cells = temp;
+                }
+            }
+
             return cells;
+        }
+
+        private static void GetSecond(int[] cells, int[] second)
+        {
+            second[0] = 0;
+            for (var i = 1; i < cells.Length - 1; i++)
+            {
+                second[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            }
+
+            second[cells.Length - 1] = 0;
         }
     }
 }
