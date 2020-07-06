bae23e2ca89fca9aa0fb8e840210bc383114ed55
Mateusz Kopij <sagasu@gmail.com>
2020-07-06 08:59:15 +0100

    Catalan number approach for unique binary search trees

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/UniqueBinarySearchTrees/UniqueBinarySearchTrees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.UniqueBinarySearchTrees
+{
+    public class UniqueBinarySearchTrees
+    {
+        public int NumTrees(int n)
+        {
+            long c = 1;
+            for (var i = 0; i < n; ++i)
+            {
+                //Catalan number
+                c = c * 2 * (2 * i + 1) / (i + 2);
+            }
+            return (int)c;
+        }
+    }
+}
