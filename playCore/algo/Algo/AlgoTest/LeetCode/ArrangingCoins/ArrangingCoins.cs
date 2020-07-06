dc56a971d499a15e575ea22af2cbc77f77485d86
Mateusz Kopij <sagasu@gmail.com>
2020-07-06 10:12:43 +0100

    Arranging coins solution that exceeds time limit.

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ArrangingCoins/ArrangingCoins.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ArrangingCoins
+{
+    class ArrangingCoinsProblem
+    {
+        public int ArrangeCoins(int n)
+        {
+            if (n == 1 || n == 0)
+                return n;
+
+            var current = 1;
+            var taken = 1;
+            var count = 1;
+            while (true)
+            {
+                var newLimit = current + 1;
+                taken += newLimit;
+                if (taken <= n)
+                {
+                    count += 1;
+                    current = newLimit;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
