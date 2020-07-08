ec781b613048173c4aab63c458bd2e0e90daae15
Mateusz Kopij <sagasu@gmail.com>
2020-07-08 19:42:46 +0100

    Validate Parentheses

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ValidParentheses
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            var dic = new Dictionary<char,char>
+            {
+                {'{', '}'},
+                {'[', ']'},
+                {'(', ')'}
+            };
+
+            foreach (var character in s)
+            {
+                if (dic.ContainsKey(character))
+                {
+                    stack.Push(dic[character]);
+                }else if (dic.ContainsValue(character))
+                {
+                    if (stack.Count == 0 || stack.Pop() != character)
+                        return false;
+                }
+            }
+
+            return stack.Count == 0 ? true : false;
+        }
+    }
+}
