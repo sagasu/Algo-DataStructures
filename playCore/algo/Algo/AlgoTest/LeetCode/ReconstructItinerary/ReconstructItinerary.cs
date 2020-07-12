c29f3e205365c9af3018266b017beb01a290655e
Mateusz Kopij <sagasu@gmail.com>
2020-07-12 20:03:16 +0100

    Fixing Reconstruct Itinerary

--- playCore/algo/Algo/AlgoTest/LeetCode/ReconstructItinerary/ReconstructItinerary.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReconstructItinerary/ReconstructItinerary.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgoTest.LeetCode.ReconstructItinerary
 {
-    class ReconstructItinerary
+    [TestClass]
+    public class ReconstructItinerary
     {
+        [TestMethod]
+        public void Test()
+        {
+            //var t = new List<IList<string>>{new List<string> {"JFK", "SFO"}, new List<string> {"JFK", "ATL"},new List<string>
+            //    {"SFO", "ATL"},new List<string> {"ATL", "JFK"},new List<string> {"ATL", "SFO"}
+            //};
+            //var exp = new List<string> {"JFK", "ATL", "JFK", "SFO", "ATL", "SFO"};
+            //
+            var t = new List<IList<string>>
+            {
+                new List<string> {"EZE", "AXA"}, new List<string> {"TIA", "ANU"}, new List<string> {"ANU", "JFK"},
+                new List<string> {"JFK", "ANU"}, new List<string> {"ANU", "EZE"}, new List<string> {"TIA", "ANU"},
+                new List<string> {"AXA", "TIA"}, new List<string> {"TIA", "JFK"}, new List<string> {"ANU", "TIA"},
+                new List<string> {"JFK", "TIA"}
+            };
+            Assert.AreEqual(new List<string>(), FindItinerary(t));
+        }
+
         private IDictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
         public IList<string> FindItinerary(IList<IList<string>> tickets)
         {
@@ -22,28 +42,40 @@ namespace AlgoTest.LeetCode.ReconstructItinerary
                 }
             }
 
-            return FindPath("JFK", new List<string>{"JFK"});
+            foreach (var key in dic.Keys)
+            {
+                dic[key].Sort();
+            }
+
+            return FindPath("JFK", new List<string>{"JFK"}, new List<string>());
 
         }
 
         private int airportsNum;
-        private IList<string> FindPath(string key, IList<string> path)
+        private IList<string> FindPath(string key, IList<string> path, IList<string> visited)
         {
-            if (!dic.ContainsKey(key))
+            if (path.Count == airportsNum)
             {
-                return path.Count == airportsNum ? path : null;
+                return path;
             }
 
-            dic[key].Sort();
+            if (!dic.ContainsKey(key))
+                return null;
 
             for (var i = 0; i < dic[key].Count; i++)
             {
                 var airport = dic[key][i];
+                var hash = $"{key},{i}";
+                if (visited.Contains(hash))
+                    continue;
+                visited.Add(hash);
+                
                 path.Add(airport);
-                var foundPath = FindPath(airport, path);
+                var foundPath = FindPath(airport, path,visited);
                 if (foundPath != null)
                     return foundPath;
                 path.RemoveAt(path.Count -1);
+                visited.RemoveAt(visited.Count-1);
             }
 
             return null;
