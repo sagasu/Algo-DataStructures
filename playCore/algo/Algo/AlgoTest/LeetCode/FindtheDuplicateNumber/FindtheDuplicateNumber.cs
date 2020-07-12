656487dfd7df558deb413a2bc029ea1a0d5dd210
Mateusz Kopij <sagasu@gmail.com>
2020-07-12 15:53:08 +0100

    Find The Duplicate Number

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FindtheDuplicateNumber/FindtheDuplicateNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.FindtheDuplicateNumber
+{
+    class FindtheDuplicateNumber
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (hashSet.Contains(num))
+                    return num;
+
+                hashSet.Add(num);
+            }
+
+            return 0;
+        }
+    }
+}
