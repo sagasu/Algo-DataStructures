481662d0cd6bf21e4a10773af6db281c55bb3206
Mateusz Kopij <sagasu@gmail.com>
2020-07-12 16:30:34 +0100

    Naive implementation for Perfect Squares

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/PerfectSquares/PerfectSquares.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.PerfectSquares
+{
+    [TestClass]
+    public class PerfectSquares
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //Assert.AreEqual(3,NumSquares(12));
+            Assert.AreEqual(4,NumSquares(47));
+        }
+
+        public int NumSquares(int n)
+        {
+            var i = 1;
+            while (true)
+            {
+                var square = i * i;
+                if (square > n)
+                    break;
+                perfectSquares.Add(square);
+                i += 1;
+            }
+            FindNumSquares(n,0,0);
+            return min;
+        }
+
+        private int min = int.MaxValue;
+        public void FindNumSquares(int n, int count, int sum)
+        {
+            if (sum > n)
+                return;
+
+            if (n == sum)
+            {
+                if (count < min)
+                    min = count;
+                return;
+            }
+
+            for (var i = 0; i < perfectSquares.Count; i++)
+            {
+                if (sum > n)
+                    return;
+
+                FindNumSquares(n, count + 1, sum + perfectSquares[i]);
+            }
+        }
+
+        List<int> perfectSquares = new List<int>();
+    }
+}
