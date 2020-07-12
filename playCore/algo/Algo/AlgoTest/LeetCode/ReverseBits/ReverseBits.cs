d6315c70e66557a75824cdcff165e92b47e88405
Mateusz Kopij <sagasu@gmail.com>
2020-07-12 14:33:30 +0100

    Reverse Bits

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReverseBits/ReverseBits.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.ReverseBits
+{
+    [TestClass]
+    public class ReverseBitsProblem
+    {
+        [TestMethod]
+        public void Test()
+        {
+            uint t = 43261596;
+            uint expected = 964176192;
+            Assert.AreEqual(expected ,reverseBits(t));
+        }
+
+        public uint reverseBits(uint n)
+        {
+            uint ret = 0;
+            for (var i = 0; i < 32; i++)
+            {
+                var nextBit = n & 1;
+                n >>= 1;
+                ret = ret << 1;
+                ret = ret | nextBit;
+            }
+
+            return ret;
+        }
+    }
+}
