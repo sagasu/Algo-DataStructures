222e8e5d364816d199f9aa59d761981f56c2ad85
Mateusz Kopij <sagasu@gmail.com>
2020-07-14 08:45:48 +0100

    Container with most water

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ContainerWithMostWater/ContainerWithMostWater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ContainerWithMostWater
+{
+    class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            var i = 0;
+            var j = height.Length -1;
+            var max = int.MinValue;
+            while (i < j)
+            {
+                var min = Math.Min(height[i], height[j]);
+                max = Math.Max(max, min * (j-i));
+                if (min == height[i])
+                    i++;
+                else
+                    j--;
+            }
+
+            return max;
+        }
+    }
+}
