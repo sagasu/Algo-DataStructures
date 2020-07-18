be5fcacf43f1379233ab484274158f23323df30a
Mateusz Kopij <sagasu@gmail.com>
2020-07-18 15:05:34 +0100

    FindMedianOfTwoSortedArrays

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/MedianofTwoSortedArrays/MedianofTwoSortedArrays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.MedianofTwoSortedArrays
+{
+    public class MedianofTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            return 2;
+        }
+    }
+}
