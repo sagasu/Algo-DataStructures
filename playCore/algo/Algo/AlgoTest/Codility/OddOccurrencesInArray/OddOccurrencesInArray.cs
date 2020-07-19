5c6342785d3af13d91f83c8f831112f947b9734e
Mateusz Kopij <sagasu@gmail.com>
2020-07-19 17:01:14 +0100

    This solution according to Codility is invalid, it fails 3 test cases, showing that throw new ArgumentException was thrown, that means to me that these test cases were invalid.

---
+++ playCore/algo/Algo/AlgoTest/Codility/OddOccurrencesInArray/OddOccurrencesInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.Codility.OddOccurrencesInArray
+{
+    public class OddOccurrencesInArray
+    {
+        public int solution(int[] A)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var element in A)
+            {
+                if (dic.ContainsKey(element))
+                {
+                    dic[element] += 1;
+                }
+                else
+                {
+                    dic.Add(element,1);
+                }
+            }
+
+            foreach (var dicKey in dic.Keys)
+            {
+                if (dic[dicKey] == 1) return dicKey;
+            }
+
+            throw new ArgumentException("Incorrect collection");
+        }
+    }
+}
