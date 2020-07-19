4a4382fdd6a948bf9285107482fd5ef001f823db
Mateusz Kopij <sagasu@gmail.com>
2020-07-19 17:16:50 +0100

    Frog jumps

---
+++ playCore/algo/Algo/AlgoTest/Codility/FrogJumps/FrogJumps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.Codility.FrogJumps
+{
+    class FrogJumps
+    {
+        public int solution(int X, int Y, int D)
+        {
+            var distanceDiff = (Y - X);
+            var nrOfJumps = distanceDiff / D;
+            return distanceDiff % D == 0 ? nrOfJumps : nrOfJumps + 1;
+        }
+    }
+}
