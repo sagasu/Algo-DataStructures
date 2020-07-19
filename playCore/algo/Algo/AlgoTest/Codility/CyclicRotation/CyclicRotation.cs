8b8c800ce508cd596e5a9615ac2cfc5f5168c0a9
Mateusz Kopij <sagasu@gmail.com>
2020-07-19 16:23:38 +0100

    Cyclic Rotation

---
+++ playCore/algo/Algo/AlgoTest/Codility/CyclicRotation/CyclicRotation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.Codility.CyclicRotation
+{
+    [TestClass]
+    public class CyclicRotation
+    {
+
+        [TestMethod]
+        public void Test()
+        {
+            var t = new int[] {1, 2};
+            var k = 3;
+            solution(t,k);
+        }
+
+        public int[] solution(int[] A, int K)
+        {
+            if (A == null || A.Length <= 1)
+                return A;
+
+            var shifted = new int[A.Length];
+            for (var i = 0; i < A.Length; i++)
+            {
+                var newPosition = (i + K) % (A.Length);
+                shifted[newPosition] = A[i];
+            }
+
+            return shifted;
+        }
+    }
+}
