d3f0d313b6af0c8081d9881dfeb7b4d29400a62e
Mateusz Kopij <sagasu@gmail.com>
2020-07-20 15:43:03 +0100

    fix for smallest positive intiger

---
+++ playCore/algo/Algo/AlgoTest/Codility/PermMissingElem/PermMissingElem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.Codility.PermMissingElem
+{
+    [TestClass]
+    public class PermMissingElem
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var t = new int[]{2,3,1,5};
+            solution(t);
+
+        }
+
+        public int solution(int[] A)
+        {
+            if (A == null)
+                return 0;
+
+            if (A.Length == 1)
+                return A[0];
+
+            var max = 0;
+            var min = int.MaxValue;
+            foreach (var i in A)
+            {
+                if (i > max)
+                    max = i;
+                if (i < min)
+                    min = i;
+            }
+
+            var hashSet = new HashSet<int>(Enumerable.Range(min, max));
+
+            foreach (var i in A)
+            {
+                hashSet.Remove(i);
+            }
+
+            
+            return hashSet.Count == 0 ? min - 1 : hashSet.First();
+        }
+    }
+}
--- playCore/algo/Algo/AlgoTest/Codility/SmallestPositiveIntiger/SmallestPositiveIntiger.cs
+++ playCore/algo/Algo/AlgoTest/Codility/SmallestPositiveIntiger/SmallestPositiveIntiger.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Text;
-using AlgoTest.LeetCode.RotateList;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AlgoTest.LeetCode.SwapNodesInPairs
+namespace AlgoTest.Codility.SmallestPositiveIntiger
 {
     [TestClass]
     public class SmallestPositiveIntiger
@@ -13,36 +9,41 @@ namespace AlgoTest.LeetCode.SwapNodesInPairs
         [TestMethod]
         public void Test()
         {
-            //var t= new int[] {1, 3, 6, 4, 1, 2};
             var t= new int[] {1, 2,3};
             Assert.AreEqual(4, solution(t));
+
+            t = new int[] {2};
+            Assert.AreEqual(1, solution(t));
+
+            t = new int[] { 2,4,5 };
+            Assert.AreEqual(1, solution(t));
+
+
+            t = new int[] { -2, -4, -5 };
+            Assert.AreEqual(1, solution(t));
+
+            t = new int[] { -2, -4, 5 };
+            Assert.AreEqual(1, solution(t));
         }
 
         public int solution(int[] A)
         {
+            if (A.Length == 0)
+                return 1;
+
             Array.Sort(A);
 
-
-            var min = A[A.Length - 1] > 0 ? A[A.Length - 1] + 1 : 1;
-            for (var i = 0; i < A.Length-1; i++)
+            var min = 1;
+            for (var i = 0; i < A.Length; i++)
             {
-                if (A[i] >= 0)
+                if (A[i] > 0)
                 {
-                    if (A[i] + 1 < A[i+1])
-                    {
-                        min = A[i] + 1;
-                        return min;
-                    } 
-
+                    if (A[i] == min)
+                        min += 1;
                 }
             }
 
             return min;
         }
-
-        //public ListNode SwapPairs(ListNode head)
-        //{
-
-        //}
     }
 }
