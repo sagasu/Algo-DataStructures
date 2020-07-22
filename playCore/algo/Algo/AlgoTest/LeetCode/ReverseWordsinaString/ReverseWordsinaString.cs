10aaaf34cd563eb59ab5d0c739d86d044c66ac02
Mateusz Kopij <sagasu@gmail.com>
2020-07-22 08:59:29 +0100

    Reverse words in a string

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ReverseWordsinaString/ReverseWordsinaString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.ReverseWordsinaString
+{
+    public class ReverseWordsinaString
+    {
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+            var reversed = "";
+            var isFirst = true;
+            var words = s.Split(" ");
+            foreach (var fullWord in words)
+            {
+                var word = fullWord.Trim();
+                if(string.IsNullOrEmpty(word))
+                    continue;
+                if (isFirst)
+                {
+                    reversed += $"{word.Reverse()}";
+                }
+                else {
+                    reversed += $" {word.Reverse()}";
+                }
+
+                isFirst = false;
+            }
+
+            return reversed;
+        }
+    }
+}
