e4014b6dd701e95a781f304c7a6232412d31fa18
Mateusz Kopij <sagasu@gmail.com>
2020-08-03 18:26:25 +0100

    optimizing pow for specific minus integer, that needs to be long, and computation speed.

--- playCore/algo/Algo/AlgoTest/LeetCode/PowXN/PowXN2.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/PowXN/PowXN2.cs
@@ -11,10 +11,16 @@ namespace AlgoTest.LeetCode.PowXN
         [TestMethod]
         public void Test()
         {
-            MyPow(0.00001, 2147483647);
+            //MyPow(0.00001, 2147483647);
+            MyPow(1, -2147483648); // I don't like this test case, because it is specific for long
         }
 
         public double MyPow(double x, int n)
+        {
+            return MyPowPrecision(x, n);
+        }
+
+        public double MyPowPrecision(double x, long n)
         {
             if (n == 1)
                 return x;
@@ -23,10 +29,16 @@ namespace AlgoTest.LeetCode.PowXN
                 return 1;
 
             if (n < 0)
-                return 1/MyPow(x, -1 * n);
+                return 1 / MyPowPrecision(x, -1 * ((long)n));
 
             if (n > 0)
-                return x * MyPow(x, n - 1);
+            {
+                if (n % 2 == 0)
+                    return MyPowPrecision(x * x, n / 2);
+
+                return x * MyPowPrecision(x, n - 1);
+            }
+
 
             throw new ArgumentException("this shouldn't happen");
         }
