7f0905ab53a221ebabd19e6ba2e8069a110116ce
Mateusz Kopij <sagasu@gmail.com>
2020-08-03 11:54:11 +0100

    Is Palindrome

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ValidPalindrome/ValidPalindrome.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.ValidPalindrome
+{
+    [TestClass]
+    public class ValidPalindrome
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(true, IsPalindrome("A man, a plan, a canal: Panama"));
+        }
+
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return true;
+
+            var i = 0;
+            var j = s.Length-1;
+
+            var alphanumeric = new Regex("^[a-zA-Z0-9]*$");
+
+            while (i<j)
+            {
+                while (i < j)
+                {
+                    if (char.IsLetterOrDigit(s[i]))
+                    {
+                        break;
+                    }
+
+                    i += 1;
+                }
+
+                while (i<j)
+                {
+                    if (char.IsLetterOrDigit(s[j]))
+                    {
+                        break;
+                    }
+
+                    j -= 1;
+                }
+
+                var equal = char.ToUpperInvariant(s[i]) == char.ToUpperInvariant(s[j]);
+                if (!equal)
+                {
+                    return false;
+                }
+
+                i += 1;
+                j -= 1;
+            }
+
+            return true;
+
+        }
+    }
+}
