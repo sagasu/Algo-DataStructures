e85f0afa2746b5fe27a63a94ef2dd64807b3e11d
Mateusz Kopij <sagasu@gmail.com>
2020-08-03 19:08:37 +0100

    single number 3

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/SingleNumberIII/SingleNumberIII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SingleNumberIII
+{
+    class SingleNumberIII
+    {
+        private HashSet<int> visitedNumbers = new HashSet<int>();
+
+        public int[] SingleNumber(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (visitedNumbers.Contains(num))
+                {
+                    visitedNumbers.Remove(num);
+                }
+                else
+                {
+                    visitedNumbers.Add(num);
+                }
+            }
+
+            return visitedNumbers.ToArray();
+        }
+    }
+}
