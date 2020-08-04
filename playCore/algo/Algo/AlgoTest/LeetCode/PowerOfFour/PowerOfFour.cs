afca2889e392edfef5de77fdea406e845a84aad4
Mateusz Kopij <sagasu@gmail.com>
2020-08-04 16:39:27 +0100

    power of four that throws stack overflow when large numbers are being used.

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/PowerOfFour/PowerOfFour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.PowerOfFour
+{
+    public class PowerOfFour
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            if (num == 0)
+                return false;
+
+            if (num % 4 != 0)
+                return false;
+
+            if (num < 0)
+                return false;
+
+            if (num == 1)
+                return true;
+
+            return IsPowerOfFour(num, 4);
+        }
+
+        public bool IsPowerOfFour(int num, int acc)
+        {
+            if (acc > num)
+                return false;
+
+            if (acc == num)
+                return true;
+
+            return IsPowerOfFour(num, acc * 4);
+        }
+    }
+}
