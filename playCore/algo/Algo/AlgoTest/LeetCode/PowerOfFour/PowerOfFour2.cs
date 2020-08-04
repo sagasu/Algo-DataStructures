3ee5b855345e741c244095fad9464a3465618788
Mateusz Kopij <sagasu@gmail.com>
2020-08-04 17:53:18 +0100

    different approach for power of four

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/PowerOfFour/PowerOfFour2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.PowerOfFour
+{
+    class PowerOfFour2
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            return num > 0 && (Math.Log10(num) / Math.Log10(4)) % 1 == 0;
+        }
+    }
+}
