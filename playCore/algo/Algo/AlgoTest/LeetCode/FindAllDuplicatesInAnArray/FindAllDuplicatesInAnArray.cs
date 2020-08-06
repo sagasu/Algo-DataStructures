83517b73425fbdf99f5c2e832830b02e93827010
Mateusz Kopij <sagasu@gmail.com>
2020-08-06 09:31:07 +0100

    Find All Duplicates in an Array

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FindAllDuplicatesInAnArray/FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.FindAllDuplicatesInAnArray
+{
+    //Could you do it without extra space and in O(n) runtime?
+    // notice that 1 ≤ a[i] ≤ n (n = size of array) int this problem
+    class FindAllDuplicatesInAnArray
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var duplicates = new List<int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                    duplicates.Add(Math.Abs(nums[i]));
+                nums[index] = -nums[index];
+            }
+
+            return duplicates;
+        }
+    }
+}
