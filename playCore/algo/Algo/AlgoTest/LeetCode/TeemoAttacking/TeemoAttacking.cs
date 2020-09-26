de7ac867a99541070b660fc1390e0a25e60fabf3
Mateusz Kopij <sagasu@gmail.com>
2020-09-26 17:57:48 +0100

    Teemo Attacking, approach with a fixed array. Out of time.

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/TeemoAttacking/TeemoAttacking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.TeemoAttacking
+{
+    [TestClass]
+    public class TeemoAttacking
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(3, FindPoisonedDuration(new int[]{1,2},2 ));
+            Assert.AreEqual(4, FindPoisonedDuration(new int[]{1,4},2 ));
+        }
+
+
+        public int FindPoisonedDuration(int[] timeSeries, int duration)
+        {
+            if (timeSeries.Length == 0)
+                return 0;
+
+            var timeline = new int[timeSeries[timeSeries.Length - 1] + duration + 1];
+            foreach (var series in timeSeries)
+            {
+                Array.Fill(timeline, 1, series, duration);
+            }
+
+            return timeline.Sum(x => x);
+
+        }
+    }
+}
