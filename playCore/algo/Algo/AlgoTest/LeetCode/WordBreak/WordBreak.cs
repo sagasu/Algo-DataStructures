287e01553019d58778a11805bf330c264b310ebc
Mateusz Kopij <sagasu@gmail.com>
2020-09-27 09:46:12 +0100

    Timeout solution for Word Break problem.

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/WordBreak/WordBreak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.WordBreak
+{
+    [TestClass]
+    public class WordBreakSolution
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var s = "leetcode";
+            var wordDict = new string[] {"leet", "code"};
+            Assert.IsTrue(WordBreak(s, wordDict));
+        }
+
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            if (s.Length == 0)
+                return true;
+            for (var i = 1; i <= s.Length; i++)
+            {
+                var word = s.Substring(0, i);
+                if (wordDict.Contains(word) && WordBreak(s.Substring(i), wordDict)) return true;
+            }
+
+            return false;
+        }
+    }
+}
