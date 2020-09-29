3d4f4787510e90fb1a754a517028ade7f8c32b90
Mateusz Kopij <sagasu@gmail.com>
2020-09-29 17:28:47 +0100

    Throne Interitance Test

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ThroneInheritance/ThroneInheritance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.ThroneInheritance
+{
+    [TestClass]
+    public class ThroneInteritanceTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+        
+        }
+    }
+
+    class Family
+    {
+        public Family(string name, string parent)
+        {
+            
+        }
+    }
+
+    public class ThroneInheritance
+    {
+        IDictionary<string, List<string>> inheritance = new Dictionary<string,List<string>>();
+        public ThroneInheritance(string kingName)
+        {
+
+        }
+
+        public void Birth(string parentName, string childName)
+        {
+
+        }
+
+        public void Death(string name)
+        {
+            if(inheritance.ContainsKey(name))
+        }
+
+        public IList<string> GetInheritanceOrder()
+        {
+
+        }
+        
+    }
+}
