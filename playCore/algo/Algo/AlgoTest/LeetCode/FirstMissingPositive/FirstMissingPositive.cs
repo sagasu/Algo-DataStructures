10a3854046c7ce76c6011536837fb39bbdf0751c
Mateusz Kopij <sagasu@gmail.com>
2020-09-30 18:03:07 +0100

    First Missing Positive

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FirstMissingPositive/FirstMissingPositive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.FirstMissingPositive
+{
+    class FirstMissingPositiveSolution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for(var i = 0; i < nums.Length; i++)
+                while (nums[i] > 0 && nums[i] < nums.Length && nums[nums[i] - 1] != nums[i])
+                    Swap(i, nums[i] - 1);
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                    return i + 1;
+            }
+
+            return nums.Length + 1;
+
+            void Swap(int a, int b)
+            {
+                var temp = nums[a];
+                nums[a] = nums[b];
+                nums[b] = temp;
+            }
+        }
+
+        
+    }
+}
