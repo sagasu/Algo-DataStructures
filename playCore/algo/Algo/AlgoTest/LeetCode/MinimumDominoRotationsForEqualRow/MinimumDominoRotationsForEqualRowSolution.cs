7631d95e4869e807ac26cf71a057f5a0ff2ac17e
Mateusz Kopij <sagasu@gmail.com>
2020-10-20 21:06:42 +0100

    adding test cases

--- playCore/algo/Algo/AlgoTest/LeetCode/MinimumDominoRotationsForEqualRow/MinimumDominoRotationsForEqualRowSolution.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/MinimumDominoRotationsForEqualRow/MinimumDominoRotationsForEqualRowSolution.cs
@@ -1,11 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgoTest.LeetCode.MinimumDominoRotationsForEqualRow
 {
-    class MinimumDominoRotationsForEqualRowSolution
+    public class MinimumDominoRotationsForEqualRowSolution
     {
+
+        [TestMethod]
+        public void Test()
+        {
+            //var t1 = new int[] { 2, 1, 2, 4, 2, 2 };
+            //var t2 = new int[] { 5, 2, 6, 2, 3, 2 };
+            //Assert.AreEqual(2, MinDominoRotations(t1, t2));
+
+            //var t3 = new int[] { 3, 5, 1, 2, 3 };
+            //var t4 = new int[] { 3, 6, 3, 3, 4 };
+            //Assert.AreEqual(-1, MinDominoRotations(t3, t4));
+
+            var t5 = new int[] { 1, 2, 1, 1, 1, 2, 2, 2 };
+            var t6 = new int[] { 2, 1, 2, 2, 2, 2, 2, 2 };
+            Assert.AreEqual(1, MinDominoRotations(t5, t6));
+        }
         //if values of one side must be all the same, either the value should be equal to A[0] or B[0]
         //check for both cases if A[i]==A[0] or B[i]==A[0]
         //                        A[i]==B[0] or B[i]==B[0]
