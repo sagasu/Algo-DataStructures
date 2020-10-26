6cbde05189f781f66bfc127ebdfd111044aa19f0
Mateusz Kopij <sagasu@gmail.com>
2020-10-26 20:56:59 +0000

    ChampagneTower Work

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ChampagneTower/ChampagneTower.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.ChampagneTower
+{
+    [TestClass]
+    public class ChampagneTowerSolution
+    {
+        [TestMethod]
+        public void TesT()
+        {
+            //Assert.AreEqual(0,ChampagneTower(1,1,1));
+            Assert.AreEqual(0.5,ChampagneTower(2,1,1));
+            Assert.AreEqual(1.0,ChampagneTower(100000009, 33,17));
+        }
+
+        // Nice way to handle a Pascal triangle, thru building dynamic size array for each row, and really simple array modification.
+        public double ChampagneTower(int poured, int query_row, int query_glass)
+        {
+            var arr = new double[query_row+2][];
+            arr[0] = new double[1];
+            arr[0][0] = poured;
+            
+            for (var i = 0; i <= query_row; i++)
+            {
+                arr[i + 1] = new double[i + 2];
+                for (var j = 0; j <= i; j++)
+                {
+                    if (arr[i][j] > 1)
+                    {
+                        double next = (arr[i][j] -1) / 2;
+
+                        arr[i + 1][j] += next;
+                        arr[i + 1][j + 1] += next;
+                    }
+                }
+            }
+
+            return arr[query_row][query_glass];
+        }
+    }
+}
