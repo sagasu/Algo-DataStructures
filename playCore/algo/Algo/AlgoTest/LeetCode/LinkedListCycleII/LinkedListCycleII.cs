6cf2dcbfaa8b4ee9ab0db328f6414205dc0b639a
Mateusz Kopij <sagasu@gmail.com>
2020-10-27 17:48:12 +0000

    Linked List Cycle II

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/LinkedListCycleII/LinkedListCycleII.cs
@@ -0,0 +1,47 @@
+﻿using AlgoTest.LeetCode.RotateList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.LinkedListCycleII
+{
+    [TestClass]
+    public class LinkedListCycleII
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+
+        public ListNode DetectCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast?.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    return GetCycleStartingPoint(head, fast);
+                }
+            }
+
+            return null;
+        }
+
+        private ListNode GetCycleStartingPoint(ListNode head, ListNode fast)
+        {
+            var slow = head;
+            while (true)
+            {
+                if (slow == fast)
+                    return slow;
+
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+    }
+}
