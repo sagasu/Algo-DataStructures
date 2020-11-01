5cef207d57e853e399d6dadbbbb77e8ed6c55749
Mateusz Kopij <sagasu@gmail.com>
2020-11-01 16:41:44 +0000

    working Recover Binary Search Tree

--- playCore/algo/Algo/AlgoTest/LeetCode/RecoverBinarySearchTree/RecoverBinarySearchTree.cs
+++ playCore/algo/Algo/AlgoTest/LeetCode/RecoverBinarySearchTree/RecoverBinarySearchTree.cs
@@ -11,10 +11,14 @@ namespace AlgoTest.LeetCode.RecoverBinarySearchTree
         {
             //var t = new TreeNode(1, new TreeNode(3, null, new TreeNode(2)));
             //RecoverTree(t);
-            
-            var t = new TreeNode(3, new TreeNode(1), new TreeNode(4, new TreeNode(2)));
+
+            //var t = new TreeNode(3, new TreeNode(1), new TreeNode(4, new TreeNode(2)));
+            //RecoverTree(t);
+
+            var t = new TreeNode(5, new TreeNode(3, new TreeNode(-2147483648), new TreeNode(2)), new TreeNode(9));
             RecoverTree(t);
         }
+
         //There are exactly two nodes that were swapped
         public void RecoverTree(TreeNode root)
         {
@@ -39,27 +43,19 @@ namespace AlgoTest.LeetCode.RecoverBinarySearchTree
 
             InOrderTraverse(root.left);
 
-            if (root.val <= previous.val)
+            if (first == null && previous.val > root.val)
             {
-                StoreRoot(previous, root);
-                StoreRoot(previous, root);
+                first = previous;
             }
 
+            if (first != null && previous.val > root.val)
+            {
+                second = root;
+            }
             previous = root;
+
             InOrderTraverse(root.right);
         }
 
-        private void StoreRoot(TreeNode node, TreeNode child)
-        {
-            if (first == null)
-            {
-                first = node;
-            }
-            else
-            {
-                second = child;
-            }
-            
-        }
     }
 }
