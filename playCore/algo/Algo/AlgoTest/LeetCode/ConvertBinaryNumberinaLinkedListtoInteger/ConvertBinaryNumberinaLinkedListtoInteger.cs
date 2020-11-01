bef55fbd166c9efc7322205f2d1853948c0404f3
Mateusz Kopij <sagasu@gmail.com>
2020-11-01 17:57:03 +0000

    Convert Binar yNumber in a LinkedList to Integer

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/ConvertBinaryNumberinaLinkedListtoInteger/ConvertBinaryNumberinaLinkedListtoInteger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using AlgoTest.LeetCode.RotateList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.ConvertBinaryNumberinaLinkedListtoInteger
+{
+    [TestClass]
+    public class ConvertBinaryNumberinaLinkedListtoInteger
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var t = new ListNode(1, new ListNode(0, new ListNode(1)));
+            Assert.AreEqual(5, GetDecimalValue(t));
+        }
+
+        public int GetDecimalValue(ListNode head)
+        {
+            
+            var sb = new StringBuilder();
+            sb.Append(head.val);
+
+            while (true)
+            {
+                var n = head.next;
+                if (n == null)
+                    break;
+                
+                sb.Append(n.val);
+                head = head.next;
+            }
+
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}
