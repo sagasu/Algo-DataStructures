43ddd23aadb346272c6d24cb6c19ee8e5bc2b3d6
Mateusz Kopij <sagasu@gmail.com>
2020-11-05 19:35:42 +0000

    Min Cost To Move Chips

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/MinimumCosttoMoveChipstoTheSamePosition/MinimumCosttoMoveChipstoTheSamePosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.MinimumCosttoMoveChipstoTheSamePosition
+{
+    public class MinimumCosttoMoveChipstoTheSamePosition
+    {
+        public void Test()
+        {
+
+        }
+
+        public int MinCostToMoveChips(int[] position)
+        {
+            var odd = 0;
+            var even = 0;
+
+            foreach (var chipIndex in position)
+            {
+                if (chipIndex % 2 == 0)
+                    even += 1;
+                else
+                    odd += 1;
+            }
+
+            return Math.Min(odd, even);
+        }
+    }
+}
