5d88dda71f78ca7dd0bfe9f745748f086543d7e2
Mateusz Kopij <sagasu@gmail.com>
2020-11-08 22:50:49 +0000

    working on Binary Tree Tilt

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/BinaryTreeTilt/BinaryTreeTilt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.BinaryTreeTilt
+{
+    public class BinaryTreeTilt
+    {
+        public void Test()
+        {
+
+        }
+
+        List<int> tree = new List<int>();
+
+        public int FindTilt(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root.left);
+            queue.Enqueue(root.right);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if(node.left != )
+            }
+        }
+
+        private void InOrderTraversal(TreeNode root)
+        {
+            if (root == null)
+            {
+                tree.Add(0);
+                return;
+            }
+
+            tree.Add(root.val);
+
+            InOrderTraversal(root.left);
+            InOrderTraversal(root.right);
+
+        }
+    }
+}
