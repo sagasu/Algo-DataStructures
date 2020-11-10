d5f2aa4e9c2276ffec3ca150e89fe7640b6feee1
Mateusz Kopij <sagasu@gmail.com>
2020-11-10 22:21:07 +0000

    Flipping An Image

---
+++ playCore/algo/Algo/AlgoTest/LeetCode/FlippingAnImage/FlippingAnImage.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.FlippingAnImage
+{
+    [TestClass]
+    public class FlippingAnImage
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var t = new int[][]{new int[]{1, 1, 0},new int[]{1, 0, 1},new int[]{0, 0, 0}};
+            var ans = new int[][]{new int[]{1, 0, 0},new int[]{0, 1, 0},new int[]{1, 1, 1}};
+            CollectionAssert.AreEqual(ans, FlipAndInvertImage(t));
+        }
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            foreach (var elements in A)
+            {
+                var x = 0;
+                var y = A.Length-1;
+                while (x < y)
+                {
+                    Swap(elements, x, y);
+
+                    x += 1;
+                    y -= 1;
+                }
+
+                if (x == y)
+                    elements[x] = 1 - elements[x];
+            }
+
+            return A;
+        }
+
+        private void Swap(int[] elements, int x, int y)
+        {
+            var temp = elements[x];
+            elements[x] = 1- elements[y];
+            elements[y] = 1 - temp;
+        }
+    }
+}
