470ff0ee56d73ca30566307e0f6edacc7f0c241d
Mateusz Kopij <sagasu@gmail.com>
2021-02-21 17:09:07 +0000

    Broken Calculator

---
+++ AlgoTest/LeetCode/BrokenCalculator/BrokenCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.BrokenCalculator
+{
+    class BrokenCalculator
+    {
+        public int BrokenCalc(int X, int Y)
+        {
+            if (X >= Y) return X - Y;
+
+            var steps = 0;
+            while (X < Y)
+            {
+                if (Y % 2 == 1)
+                    Y += 1;
+                else
+                    Y /= 2;
+
+                steps += 1;
+            }
+
+            return steps + (X - Y);
+        }
+    }
+}
