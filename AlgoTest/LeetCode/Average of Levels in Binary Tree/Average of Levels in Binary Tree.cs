7bac888ba17b5011e9ee82595a49710e9ecdf310
Mateusz Kopij <sagasu@gmail.com>
2021-03-05 19:28:27 +0000

    Average of Levels in Binary Tree

---
+++ AlgoTest/LeetCode/Average of Levels in Binary Tree/Average of Levels in Binary Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Average_of_Levels_in_Binary_Tree
+{
+    public class Average_of_Levels_in_Binary_Tree
+    {
+        IDictionary<int, IList<int>> average = new Dictionary<int, IList<int>>();
+
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var ret = new List<double>();
+            BuildAverageOfLevels(root, 0);
+            foreach (var key in average.Keys)
+            {
+                ret.Add(average[key].Average());
+            }
+            ret.Reverse();
+            return ret;
+        }
+
+        private void BuildAverageOfLevels(TreeNode root, int level)
+        {
+            if (root == null) return;
+
+            if (!average.ContainsKey(level))
+            {
+                average.Add(level, new List<int>() { root.val });
+            }
+            else
+            {
+                average[level].Add(root.val);
+            }
+            BuildAverageOfLevels(root.left, level + 1);
+
+            BuildAverageOfLevels(root.right, level + 1);
+        }
+    }
+}
