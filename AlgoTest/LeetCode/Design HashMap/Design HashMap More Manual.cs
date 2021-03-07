772d540956ca4ca65d4ca5a3bea0b2ed49ed173b
Mateusz Kopij <sagasu@gmail.com>
2021-03-07 17:36:04 +0000

    Design HashMap more manual approach - not using contains function and dictionary. Using only Array to perform operations.

--- AlgoTest/LeetCode/Design HashMap/Design HashMap More Manual.cs
+++ AlgoTest/LeetCode/Design HashMap/Design HashMap More Manual.cs
@@ -2,38 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AlgoTest.LeetCode.Design_HashMap2
+namespace AlgoTest.LeetCode.Design_HashMap
 {
     public class MyHashMap
     {
-
-        IDictionary<int, int> dic;
+        private int[] store;
 
         public MyHashMap()
         {
-            dic = new Dictionary<int, int>();
+            store = new int[1000001];
+            Array.Fill(store, -1);
         }
 
         /** value will always be non-negative. */
         public void Put(int key, int value)
         {
-            if (dic.ContainsKey(key))
-                dic[key] = value;
-            else
-                dic.Add(key, value);
+            store[key] = value;
         }
 
         /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
         public int Get(int key)
         {
-            return dic.ContainsKey(key) ? dic[key] : -1;
+            return key < store.Length ? store[key] : -1;
         }
 
         /** Removes the mapping of the specified value key if this map contains a mapping for the key */
         public void Remove(int key)
         {
-            if (dic.ContainsKey(key))
-                dic.Remove(key);
+            store[key] = -1;
         }
     }
 }
