0ce375c19e7ef59bb375a48f91c70e9982e22674
Mateusz Kopij <sagasu@gmail.com>
2021-03-09 18:41:09 +0000

    Add One Row to Tree

---
+++ AlgoTest/LeetCode/Add One Row to Tree/Add One Row to Tree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Add_One_Row_to_Tree
+{
+    public class Add_One_Row_to_Tree
+    {
+        public TreeNode AddOneRow(TreeNode root, int v, int d)
+        {
+            if (root == null) return root;
+
+            if (d == 1) return new TreeNode(v, root);
+
+            if (d == 2)
+            {
+                var temp = root.left;
+                root.left = new TreeNode(v, temp);
+
+                temp = root.right;
+                root.right = new TreeNode(v, null, temp);
+                
+                return root;
+            }
+
+            AddOneRow(root.left, v, d - 1);
+            AddOneRow(root.right, v, d - 1);
+
+            return root;
+        }
+    }
+}
