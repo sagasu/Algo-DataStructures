072214f36220b57f000b6805ea7f86017e14b5c3
Mateusz Kopij <sagasu@gmail.com>
2021-03-11 22:10:22 +0000

    Coin Change, cache is still not working.

---
+++ AlgoTest/LeetCode/CoinChange/CoinChange.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.CoinChange
+{
+    [TestClass]
+    public class CoinChangeSolution
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var t = new int[] {1,2,5 };
+            Assert.AreEqual(3, CoinChange(t, 11));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var t = new int[] {2 };
+            Assert.AreEqual(-1, CoinChange(t, 3));
+        }
+        
+        [TestMethod]
+        public void Test3()
+        {
+            var t = new int[] {1};
+            Assert.AreEqual(0, CoinChange(t, 0));
+        }
+        
+        [TestMethod]
+        public void Test4()
+        {
+            var t = new int[] {1};
+            Assert.AreEqual(2, CoinChange(t, 2));
+        }
+
+
+        public int CoinChange(int[] coins, int amount)
+        {
+            var cache = new int?[coins.Length, amount + 1];
+            
+            int GetMinCoinChange(int index, int sum)
+            {
+                if (sum == 0)
+                    return 0;
+
+                if (sum < 0)
+                    return int.MaxValue;
+
+                if (index == coins.Length)
+                    return int.MaxValue;
+
+                if (cache[index, sum].HasValue) return cache[index, sum].Value;
+
+                var min = Int32.MaxValue;
+                min = Math.Min(min, GetMinCoinChange(index, sum - coins[index]) + 1);
+
+                min = Math.Min(min, GetMinCoinChange(index + 1, sum));
+
+                cache[index, sum] = min;
+
+                return min;
+            }
+
+            var minCoinChange = GetMinCoinChange(0, amount);
+            return minCoinChange == int.MaxValue ? -1 : minCoinChange;
+        }
+
+    }
+}
