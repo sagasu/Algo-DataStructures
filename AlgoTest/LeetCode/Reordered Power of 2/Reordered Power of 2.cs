0d61a2d5a9a19ecff8e8833d9b983ef816af4a7d
Mateusz Kopij <sagasu@gmail.com>
2021-03-21 10:18:29 +0000

    Reordered Power of 2

---
+++ AlgoTest/LeetCode/Reordered Power of 2/Reordered Power of 2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Reordered_Power_of_2
+{
+    [TestClass]
+    public class Reordered_Power_of_2
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(true, ReorderedPowerOf2(46));
+            Assert.AreEqual(true, ReorderedPowerOf2(1));
+            Assert.AreEqual(true, ReorderedPowerOf2(16));
+            Assert.AreEqual(false, ReorderedPowerOf2(10));
+            Assert.AreEqual(false, ReorderedPowerOf2(24));
+        }
+
+        public bool ReorderedPowerOf2(int N)
+        {
+            string Normalize(double d)
+            {
+                var number = d.ToString().ToCharArray();
+                Array.Sort(number);
+                return string.Join("", number);
+            }
+
+            var powers = new List<string>(32);
+
+            // N < 10^9 < 2^32
+            for (int i = 0; i < 32; i++)
+            {
+                powers.Add(Normalize(Math.Pow(2,i)));
+            }
+
+            var normalized = Normalize(N);
+            return powers.Contains(normalized);
+        }
+    }
+}
