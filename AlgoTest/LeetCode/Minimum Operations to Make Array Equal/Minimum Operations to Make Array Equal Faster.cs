135804fae573e737ca39e7f6a034a11048848a35
Mateusz Kopij <sagasu@gmail.com>
2021-04-06 20:30:24 +0100

    Minimum Operations to Make Array Equal Faster

---
+++ AlgoTest/LeetCode/Minimum Operations to Make Array Equal/Minimum Operations to Make Array Equal Faster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Minimum_Operations_to_Make_Array_Equal2
+{
+    [TestClass]
+    public class Minimum_Operations_to_Make_Array_Equal
+    {
+        
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(4, MinOperations(4));
+            Assert.AreEqual(2, MinOperations(3));
+            Assert.AreEqual(6, MinOperations(5));
+            Assert.AreEqual(9, MinOperations(6));
+            Assert.AreEqual(16, MinOperations(8));
+            Assert.AreEqual(20, MinOperations(9));
+            Assert.AreEqual(0, MinOperations(1));
+            Assert.AreEqual(1, MinOperations(2));
+        }
+
+        public int MinOperations(int n)
+        {
+            var factor = n / 2;
+            return factor * (n - factor);
+        }
+    }
+}
