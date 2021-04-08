31b9a465a515b90912d8b6928b63aa9534e24ca4
Mateusz Kopij <sagasu@gmail.com>
2021-04-08 23:16:29 +0100

    still working on Longest Valid Parentheses

--- AlgoTest/LeetCode/Longest Valid Parentheses/Longest Valid Parentheses.cs
+++ AlgoTest/LeetCode/Longest Valid Parentheses/Longest Valid Parentheses.cs
@@ -38,6 +38,18 @@ namespace AlgoTest.LeetCode.Longest_Valid_Parentheses
         {
             Assert.AreEqual(4, LongestValidParentheses("(()()"));
         }
+        
+        [TestMethod]
+        public void Test6()
+        {
+            Assert.AreEqual(2, LongestValidParentheses("()(()"));
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            Assert.AreEqual(12, LongestValidParentheses("))((())()())()"));
+        }
 
         public int LongestValidParentheses(string s)
         {
@@ -45,6 +57,7 @@ namespace AlgoTest.LeetCode.Longest_Valid_Parentheses
             var stack = new Stack<int>();
             var max = 0;
             var start = -1;
+            var previousStart = -1;
 
             for (var i = 0; i < chars.Length; i++)
             {
@@ -55,8 +68,13 @@ namespace AlgoTest.LeetCode.Longest_Valid_Parentheses
                     if (!stack.TryPop(out var openIndex))
                     {
                         start = i;
+                        previousStart = -1;
                         continue;
                     }
+
+                    if (previousStart == -1) previousStart = openIndex;
+
+                    if (i == chars.Length -1 && previousStart != -1 && stack.Count != 0) return Math.Max(max, i - previousStart + 1);
 
                     max = stack.Count == 0 ? Math.Max(max, i - start) : Math.Max(max, i - openIndex + 1);
                 }
