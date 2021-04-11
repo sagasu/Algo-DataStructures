1d5e485a8aecd8feaa0fe07a5bf88759e3ec6c65
Mateusz Kopij <sagasu@gmail.com>
2021-04-11 21:29:20 +0100

    Deepest Leaves Sum, Faster solution

---
+++ AlgoTest/LeetCode/Deepest Leaves Sum/Deepest Leaves Sum Faster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Deepest_Leaves_Sum
+{
+    public class Deepest_Leaves_Sum_Faster
+    {
+
+        public void Test()
+        {
+        }
+
+
+        // Runtime: 108 ms, Memory Usage: 34.4 MB
+        // It is interesting that it is faster, because we are iterating 2 times over tree.
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            BuilMaxLevel(root, 0);
+            SumMaxLevelNodes(root, 0);
+
+            return maxSum;
+        }
+        int maxLevel = 0;
+        int maxSum = 0;
+
+        void BuilMaxLevel(TreeNode tn, int level)
+        {
+            if (level > maxLevel) maxLevel = level;
+
+            if (tn.left != null) BuilMaxLevel(tn.left, level + 1);
+
+            if (tn.right != null) BuilMaxLevel(tn.right, level + 1);
+        }
+        void SumMaxLevelNodes(TreeNode node, int level)
+        {
+            if (level == maxLevel) maxSum += node.val;
+
+            if (node.left != null) SumMaxLevelNodes(node.left, level + 1);
+
+            if (node.right != null) SumMaxLevelNodes(node.right, level + 1);
+        }
+    }
+
+}
