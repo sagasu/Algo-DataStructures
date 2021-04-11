0b637ff374d7b696e70bb996ad887651465dd4c0
Mateusz Kopij <sagasu@gmail.com>
2021-04-11 19:00:53 +0100

    Deepest Leaves Sum

---
+++ AlgoTest/LeetCode/Deepest Leaves Sum/Deepest Leaves Sum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Deepest_Leaves_Sum
+{
+    public class Deepest_Leaves_Sum
+    {
+
+        public void Test()
+        {
+        }
+
+
+        public int DeepestLeavesSum(TreeNode root)
+        {
+
+            var leavesSum = new List<int>();
+
+            void BuildDeepestLeavesSum(TreeNode tn, int level)
+            {
+                if (tn == null) return;
+
+                if (leavesSum.Count <= level)
+                    leavesSum.Add(tn.val);
+                else
+                    leavesSum[level] += tn.val;
+
+                BuildDeepestLeavesSum(tn.left, level + 1);
+
+                BuildDeepestLeavesSum(tn.right, level + 1);
+            }
+
+             BuildDeepestLeavesSum(root, 0);
+
+             return leavesSum[leavesSum.Count];
+        }
+    }
+
+}
