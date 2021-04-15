b709ca441e337a09cb3bee50fd6634209980f899
Mateusz Kopij <sagasu@gmail.com>
2021-04-15 19:12:10 +0100

    commenting code to build

---
+++ AlgoTest/LeetCode/Flatten Nested List Iterator/Flatten Nested List Iterator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Flatten_Nested_List_Iterator
+{
+    public class Flatten_Nested_List_Iterator
+    {
+        //public NestedIterator(IList<NestedInteger> nestedList)
+        //{
+
+        //}
+
+        //public bool HasNext()
+        //{
+
+        //}
+
+        //public int Next()
+        //{
+
+        //}
+    }
+}
