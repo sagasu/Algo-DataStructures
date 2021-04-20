cd0120d0a073195806b8e98876eb2d6525b37ce7
Mateusz Kopij <sagasu@gmail.com>
2021-04-20 18:51:03 +0100

    N-ary Tree Preorder Traversal

---
+++ AlgoTest/LeetCode/N-ary Tree Preorder Traversal/N-ary Tree Preorder Traversal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AlgoTest.LeetCode.N_ary_Tree_Preorder_Traversal
+{
+    public class N_ary_Tree_Preorder_Traversal
+    {
+        public IList<int> Preorder(Node root)
+        {
+            IList<int> traverse = new List<int>();
+
+            void TraversePreorder(Node node)
+            {
+                if (node == null) return;
+
+                traverse.Add(node.val);
+
+                foreach (var nodeChild in node.children)
+                {
+                    TraversePreorder(nodeChild);
+                }
+            }
+            TraversePreorder(root);
+            return traverse;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
