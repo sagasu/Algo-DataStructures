d6f30e3e97b2049371ba0c94e8d2bcb37b9c2fee
Mateusz Kopij <sagasu@gmail.com>
2021-04-29 20:47:45 +0100

    unfinished

---
+++ AlgoTest/LeetCode/Critical Connections in a Network/Critical Connections in a Network.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Critical_Connections_in_a_Network
+{
+    public class Critical_Connections_in_a_Network
+    {
+        public IList<IList<int>> CriticalConnections(int n, IList<IList<int>> connections)
+        {
+            return new List<IList<int>>();
+        }
+    }
+}
