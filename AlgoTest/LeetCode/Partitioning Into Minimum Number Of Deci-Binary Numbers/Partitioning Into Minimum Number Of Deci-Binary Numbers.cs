a82d41a3ab4c382a0bb2e8316a4213fbef31aa62
Mateusz Kopij <sagasu@gmail.com>
2021-05-26 18:56:27 +0100

    Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ AlgoTest/LeetCode/Partitioning Into Minimum Number Of Deci-Binary Numbers/Partitioning Into Minimum Number Of Deci-Binary Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers
+{
+    [TestClass]
+    public class Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(9,MinPartitions("27346209830709182346"));
+            Assert.AreEqual(8,MinPartitions("82734"));
+            Assert.AreEqual(3,MinPartitions("32"));
+        }
+
+        public int MinPartitions(string n)
+        {
+            return n.ToCharArray().Max(x => int.Parse(x.ToString()));
+        }
+    }
+}
