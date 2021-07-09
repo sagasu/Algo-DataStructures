f4d4cd93c6e724ccd97f2dc7610d73bd38198f42
Mateusz Kopij <sagasu@gmail.com>
2021-07-09 18:49:59 +0100

    Maximum Length of Repeated Subarray

--- AlgoTest/LeetCode/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.cs
+++ AlgoTest/LeetCode/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.cs
@@ -15,7 +15,17 @@ namespace AlgoTest.LeetCode.Maximum_Length_of_Repeated_Subarray
             var t2 = new int[] { 3, 2, 1, 4, 7 };
             Assert.AreEqual(3, FindLength(t1, t2));
         }
-        
+        /*
+            *   1 2 3 2 1
+            * 3 0 0 1 1 1
+            * 2 0 0 1 2 1
+            * 1 0 0 1 2 3
+            * 4
+            * 7
+            *
+            */
+
+
         [TestMethod]
         public void Test2()
         {
@@ -41,30 +51,36 @@ namespace AlgoTest.LeetCode.Maximum_Length_of_Repeated_Subarray
             Assert.AreEqual(9, FindLength(t1, t2));
         }
 
+        /*
+         *    0, 0, 0, 0, 0, 0, 1, 0, 0, 0 
+         * 0, 1  1  1  1  1  1  0  1  1  1
+         * 0, 1  2  2  2  2  2  1  1  2  2
+         * 0, 0     3
+         * 0,
+         * 0,
+         * 0,
+         * 0,
+         * 1,
+         * 0,
+         * 0 
+         *
+         *
+         */
+
         public int FindLength(int[] nums1, int[] nums2)
         {
-            /*
-             *   1 2 3 2 1
-             * 3 0 0 1 1 1
-             * 2 0 0 1 2 1
-             * 1 0 0 1 2 3
-             * 4
-             * 7
-             *
-             */
-            var dp = new int[nums2.Length, nums1.Length];
+           
+            var dp = new int[nums2.Length + 1, nums1.Length + 1];
 
             var max = 0;
 
-            for (var i = 0; i < nums2.Length; i++)
+            for (var i = 1; i <= nums2.Length; i++)
             {
-                for (var j = 0; j < nums1.Length; j++)
+                for (var j = 1; j <= nums1.Length; j++)
                 {
-                    if (nums1[j] == nums2[i])
+                    if (nums1[j-1] == nums2[i-1])
                     {
-                        var iIndex = i == 0 ? 0 : i - 1;
-                        var jIndex = j > 0 ? j - 1: j;
-                        dp[i, j] = dp[iIndex, jIndex] + 1;
+                        dp[i, j] = dp[i-1, j-1] + 1;
 
                         max = Math.Max(max, dp[i, j]);
                     }
