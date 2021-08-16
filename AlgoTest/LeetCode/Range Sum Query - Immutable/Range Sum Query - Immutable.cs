76a17b31fa4a08de0a1fe6c8052f33e4a3d0a378
Mateusz Kopij <sagasu@gmail.com>
2021-08-16 18:43:00 +0100

    Range Sum Query - Immutable

---
+++ AlgoTest/LeetCode/Range Sum Query - Immutable/Range Sum Query - Immutable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Range_Sum_Query___Immutable
+{
+    public class NumArray
+    {
+        int[] nums;
+
+        public NumArray(int[] nums)
+        {
+            for (var i = 1; i < nums.Length; i++)
+                nums[i] += nums[i - 1];
+
+            this.nums = nums;
+        }
+
+        public int SumRange(int left, int right)
+        {
+            if (left == 0)
+                return nums[right];
+
+            return nums[right] - nums[left - 1];
+        }
+    }
+}
