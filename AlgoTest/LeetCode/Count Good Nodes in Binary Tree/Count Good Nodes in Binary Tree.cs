6d39f13cdb95e79f67b6f059898306fbeff2a9b7
Mateusz Kopij <sagasu@gmail.com>
2021-08-17 22:15:58 +0100

    Count Good Nodes in Binary Tree

---
+++ AlgoTest/LeetCode/Count Good Nodes in Binary Tree/Count Good Nodes in Binary Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Count_Good_Nodes_in_Binary_Tree
+{
+    class Count_Good_Nodes_in_Binary_Tree
+    {
+        public int GoodNodes(TreeNode root)
+        {
+            if (root == null) { return 0; }
+
+            var goodNodeCount = 0;
+            Traverse(root, root.val, ref goodNodeCount);
+            return goodNodeCount;
+        }
+
+        private void Traverse(TreeNode node, int maxPathValue, ref int goodNodeCount)
+        {
+            if (node == null) { return; }
+
+            if (maxPathValue <= node.val) { maxPathValue = node.val; goodNodeCount++; }
+
+            Traverse(node.left, maxPathValue, ref goodNodeCount);
+            Traverse(node.right, maxPathValue, ref goodNodeCount);
+        }
+    }
+}
