715146f51d78ca060c7dfec1fc46c07d40bdba69
Mateusz Kopij <sagasu@gmail.com>
2021-08-25 14:41:18 +0100

    Sum of Square Numbers

---
+++ AlgoTest/LeetCode/Sum of Square Numbers/Sum of Square Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Sum_of_Square_Numbers
+{
+    [TestClass]
+    public class Sum_of_Square_Numbers
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(true, JudgeSquareSum(5));
+            Assert.AreEqual(true, JudgeSquareSum(4));
+            Assert.AreEqual(true, JudgeSquareSum(2));
+            Assert.AreEqual(true, JudgeSquareSum(1));
+            Assert.AreEqual(false, JudgeSquareSum(3));
+        }
+        
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(false, JudgeSquareSum(999999999));
+
+        }
+        
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(false, JudgeSquareSum(2147483646));
+
+        }
+        
+        public bool JudgeSquareSum(int c)
+        {
+            var max = (int)Math.Sqrt(c);
+            for (var i = 0; i <= max; i++)
+            {
+                var b = Math.Sqrt(c - i * i);
+                if (b == (int)b)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
