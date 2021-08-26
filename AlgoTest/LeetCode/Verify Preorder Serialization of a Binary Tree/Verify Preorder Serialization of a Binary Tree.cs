5e9d3de806ecb93a495da259927c0f88005ca236
Mateusz Kopij <sagasu@gmail.com>
2021-08-26 17:28:45 +0100

    Verify Preorder Serialization of a Binary Tree

---
+++ AlgoTest/LeetCode/Verify Preorder Serialization of a Binary Tree/Verify Preorder Serialization of a Binary Tree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Verify_Preorder_Serialization_of_a_Binary_Tree
+{
+    [TestClass]
+    public class Verify_Preorder_Serialization_of_a_Binary_Tree
+    {
+
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(true, IsValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+            Assert.AreEqual(false, IsValidSerialization("1,#"));
+            Assert.AreEqual(false, IsValidSerialization("9,#,#,1"));
+        }
+
+        public bool IsValidSerialization(string preorder)
+        {
+            var tree = preorder.Split(',');
+            var index = 0;
+            var good = true;
+
+            void Preorder()
+            {
+                if (index >= tree.Length)
+                {
+                    good = false;
+                    return;
+                }
+
+                if (tree[index] == "#")
+                {
+                    index += 1;
+                    return;
+                }
+
+                index += 1;
+                Preorder();
+                Preorder();
+            }
+
+            Preorder();
+            return good && index == tree.Length;
+        }
+    }
+}
