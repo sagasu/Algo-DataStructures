11a0d2a847d95966147048f4d3bc15887b396736
Mateusz Kopij <sagasu@gmail.com>
2021-09-01 16:47:04 +0100

    Array Nesting

---
+++ AlgoTest/LeetCode/Array Nesting/Array Nesting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Array_Nesting
+{
+    class Array_Nesting
+    {
+        public int ArrayNesting(int[] nums)
+        {
+            var maxLen = 0;
+            var visitedIndexes = new HashSet<int>();
+            for (var i = 0; i < nums.Length; i++)
+                if (!visitedIndexes.Contains(i))
+                    maxLen = Math.Max(maxLen, Dfs(i, new HashSet<int>()));
+
+            return maxLen;
+
+            int Dfs(int index, ISet<int> set)
+            {
+                if (!set.Add(nums[index])) return set.Count;
+                visitedIndexes.Add(index);
+                return Dfs(nums[index], set);
+            }
+        }
+    }
+}
