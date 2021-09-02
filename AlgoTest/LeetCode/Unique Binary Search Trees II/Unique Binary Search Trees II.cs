312cbcaa1864c07c263173b64a11301753dde213
Mateusz Kopij <sagasu@gmail.com>
2021-09-02 16:10:23 +0100

    Unique Binary Search Trees II

---
+++ AlgoTest/LeetCode/Unique Binary Search Trees II/Unique Binary Search Trees II.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Unique_Binary_Search_Trees_II
+{
+    class Unique_Binary_Search_Trees_II
+    {
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            return n == 0 ? new List<TreeNode>() : DFS(1, n);
+
+            IList<TreeNode> DFS(int start, int end)
+            {
+                if (start > end) return new List<TreeNode>() { null };
+                
+                var result = new List<TreeNode>();
+                for (var i = start; i <= end; i++)
+                {
+                    var leftList = DFS(start, i - 1);
+                    var rightList = DFS(i + 1, end);
+
+                    foreach (var left in leftList)
+                        foreach (var right in rightList) {
+                            var root = new TreeNode(i) {left = left, right = right};
+                            result.Add(root);
+                        }
+                    
+                }
+
+                return result;
+            }
+        }
+    }
+}
