31f98a242ec0cec9e77cee0a5d386b8384cd6e59
Mateusz Kopij <sagasu@gmail.com>
2021-09-05 21:37:44 +0100

    Orderly Queue

---
+++ AlgoTest/LeetCode/Orderly Queue/Orderly Queue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Orderly_Queue
+{
+    class Orderly_Queue
+    {
+        public virtual string OrderlyQueue(string s, int k)
+        {
+            if (k > 1) {
+                var temp = s.ToCharArray();
+                Array.Sort(temp);
+                s = new string(temp);
+            } else {
+                var sTwice = s + s;
+                for (var i = 1; i < s.Length; i++) {
+                    var tmp = sTwice.Substring(i, s.Length);
+                    s = (s.CompareTo(tmp) > 0) ? tmp : s;
+                }
+            }
+            return s;
+        }
+	}
+}
