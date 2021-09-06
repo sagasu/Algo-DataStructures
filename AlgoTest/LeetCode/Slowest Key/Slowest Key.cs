5e3a15c676b73cbbe9d114f1aa279a908ca171bf
Mateusz Kopij <sagasu@gmail.com>
2021-09-06 21:11:14 +0100

    Slowest Key

---
+++ AlgoTest/LeetCode/Slowest Key/Slowest Key.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Slowest_Key
+{
+    class Slowest_Key
+    {
+        public char SlowestKey(int[] releaseTimes, string keysPressed)
+        {
+            if (releaseTimes.Length == 0 || string.IsNullOrEmpty(keysPressed)) return '\0';
+
+            var res = keysPressed[0];
+            var max = releaseTimes[0];
+
+            for (var i = 1; i < releaseTimes.Length; i++)
+                if (releaseTimes[i] - releaseTimes[i - 1] > max || (releaseTimes[i] - releaseTimes[i - 1] == max && keysPressed[i] > res))
+                {
+                    res = keysPressed[i];
+                    max = releaseTimes[i] - releaseTimes[i - 1];
+                }
+
+            return res;
+        }
+    }
+}
