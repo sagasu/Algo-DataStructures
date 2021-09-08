9a36e8296b708972cde6e22f22a36073d31350e6
Mateusz Kopij <sagasu@gmail.com>
2021-09-08 18:07:40 +0100

    Shifting Letters

---
+++ AlgoTest/LeetCode/Shifting Letters/Shifting Letters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Shifting_Letters
+{
+    class Shifting_Letters
+    {
+        public string ShiftingLetters(string s, int[] shifts)
+        {
+            var chars = s.ToCharArray();
+            var requiredShifts = new int[shifts.Length];
+
+            var sum = 0;
+            for (var i = shifts.Length - 1; i >= 0; i--)
+            {
+                sum = (sum + shifts[i]) % 26;
+                requiredShifts[i] = sum;
+            }
+
+            for (var j = 0; j < chars.Length; j++)
+            {
+                var next = (chars[j] - 'a' + requiredShifts[j] + 26) % 26;
+                chars[j] = (char)(next + 'a');
+            }
+
+            return new string(chars);
+		}
+    }
+}
