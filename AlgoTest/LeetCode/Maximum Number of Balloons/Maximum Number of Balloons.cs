bdb97f664f6a2553d4372ed3d9b2ad26483e04f3
Mateusz Kopij <sagasu@gmail.com>
2021-09-13 19:55:04 +0100

    Maximum Number of Balloons

---
+++ AlgoTest/LeetCode/Maximum Number of Balloons/Maximum Number of Balloons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Maximum_Number_of_Balloons
+{
+    class Maximum_Number_of_Balloons
+    {
+        public int MaxNumberOfBalloons(string text)
+        {
+            var s = new HashSet<char>(new[] { 'b', 'a', 'l', 'o', 'n' });
+            var source = text.ToCharArray();
+
+            if (s.Intersect(source).Count() != s.Count) return 0;
+
+            return source.GroupBy(i => i)
+                .Where(j => s.Contains(j.Key))
+                .Select(k => k.Key == 'l' || k.Key == 'o' ? k.Count() / 2 : k.Count())
+                .Aggregate(int.MaxValue, (acc, val) => Math.Min(acc, val));
+        }
+    }
+}
