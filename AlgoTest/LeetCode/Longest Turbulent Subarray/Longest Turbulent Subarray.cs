9f5b61fada4cb2454e5036d8e35f91733b247404
Mateusz Kopij <sagasu@gmail.com>
2021-09-15 18:00:31 +0100

    Longest Turbulent Subarray

---
+++ AlgoTest/LeetCode/Longest Turbulent Subarray/Longest Turbulent Subarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Longest_Turbulent_Subarray
+{
+    class Longest_Turbulent_Subarray
+    {
+        public int MaxTurbulenceSize(int[] arr)
+        {
+            int start = 0, end = 0, ret = 1;
+            while (++end < arr.Length)
+            {
+                var c = arr[end - 1].CompareTo(arr[end]);
+                if (c == 0)
+                    start = end;
+                else if (end == arr.Length - 1 || c * arr[end].CompareTo(arr[end + 1]) != -1)
+                {
+                    ret = Math.Max(ret, end - start + 1);
+                    start = end;
+                }
+            }
+            return ret;
+        }
+    }
+}
