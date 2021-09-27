a06f3b581f2fcf7c3d32a8bc9afbd8ce68e4a425
Mateusz Kopij <sagasu@gmail.com>
2021-09-27 22:00:01 +0100

    Unique Email Addresses

---
+++ AlgoTest/LeetCode/Unique Email Addresses/Unique Email Addresses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Unique_Email_Addresses
+{
+    class Unique_Email_Addresses
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var emailSet = new HashSet<string>();
+
+            foreach (var email in emails)
+            {
+                var locAndDom = email.Split('@');
+                locAndDom[0] = locAndDom[0].Split('+')[0];                  
+                locAndDom[0] = locAndDom[0].Replace(".", "");               
+                emailSet.Add(String.Concat(locAndDom[0] + "@", locAndDom[1]));
+            }
+
+            return emailSet.Count;
+        }
+    }
+}
