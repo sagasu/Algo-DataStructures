cc872ac342641e1f4a961a2de73793a85184ac94
Mateusz Kopij <sagasu@gmail.com>
2021-09-29 17:27:45 +0100

    Sort_Array_By_Parity_II

---
+++ AlgoTest/LeetCode/Sort Array By Parity II/Sort Array By Parity II.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Sort_Array_By_Parity_II
+{
+    class Sort_Array_By_Parity_II
+    {
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int i = 0, j = 1;
+            while (i < nums.Length && j < nums.Length)
+            {
+                if (i % 2 == 0 && nums[i] % 2 == 0)
+                    i += 2;
+
+                else if (j % 2 == 1 && nums[j] % 2 == 1)
+                    j += 2;
+
+                else
+                {
+                    var temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i += 2;
+                    j += 2;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
