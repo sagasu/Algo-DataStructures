17a0d6a54e28a9d9e5cbd9c8a23ec291294714e5
Mateusz Kopij <sagasu@gmail.com>
2021-10-05 14:51:11 +0100

    Climbing Stairs

---
+++ AlgoTest/LeetCode/Climbing Stairs/Climbing Stairs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Climbing_Stairs
+{
+    class Climbing_Stairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            var prePre = 1;
+            var pre = 2;
+
+            for (var i = 2; i < n; i++)
+            {
+                var cur = prePre + pre;
+                prePre = pre;
+                pre = cur;
+            }
+
+            return pre;
+        }
+    }
+}
