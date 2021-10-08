6897ebe79a6eaf77a9cb48c756929a51035a9cd9
Mateusz Kopij <sagasu@gmail.com>
2021-10-08 15:36:05 +0100

    Implement Trie

---
+++ AlgoTest/LeetCode/Implement Trie (Prefix Tree)/Implement Trie (Prefix Tree).cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Implement_Trie__Prefix_Tree_
+{
+    class Trie
+    {
+
+        TrieNode root;
+        public Trie()
+        {
+            root = new TrieNode();
+        }
+        public void Insert(string word)
+        {
+            var n = word.Length;
+            TrieNode curNode = root;
+            for (int i = 0; i < n; i++)
+            {
+                var index = word[i] - 'a';
+                if (curNode.nodes[index] == null)
+                {
+                    curNode.nodes[index] = new TrieNode();
+                }
+                curNode = curNode.nodes[index];
+
+                if (i == n - 1)
+                {
+                    curNode.isWord = true;
+                }
+            }
+        }
+        public bool Search(string word)
+        {
+            var n = word.Length;
+            TrieNode curNode = root;
+            for (int i = 0; i < n; i++)
+            {
+                var index = word[i] - 'a';
+                if (curNode.nodes[index] == null)
+                {
+                    return false;
+                }
+                curNode = curNode.nodes[index];
+            }
+            return curNode.isWord;
+        }
+        public bool StartsWith(string prefix)
+        {
+            var n = prefix.Length;
+            TrieNode curNode = root;
+            for (int i = 0; i < n; i++)
+            {
+                var index = prefix[i] - 'a';
+                if (curNode.nodes[index] == null)
+                {
+                    return false;
+                }
+                curNode = curNode.nodes[index];
+            }
+            return true;
+        }
+    }
+
+    class TrieNode
+    {
+        public TrieNode[] nodes;
+        public bool isWord;
+
+        public TrieNode()
+        {
+            nodes = new TrieNode[26];
+            isWord = false;
+        }
+    }
+}
