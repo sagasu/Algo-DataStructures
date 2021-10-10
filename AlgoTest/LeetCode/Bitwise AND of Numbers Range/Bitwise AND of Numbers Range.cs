f0379210eda61d07743e4f951724c62a7a43c9d3
Mateusz Kopij <sagasu@gmail.com>
2021-10-10 19:44:12 +0100

    Bitwise AND of Numbers Range

---
+++ AlgoTest/LeetCode/Bitwise AND of Numbers Range/Bitwise AND of Numbers Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Bitwise_AND_of_Numbers_Range
+{
+    class Bitwise_AND_of_Numbers_Range
+    {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            var shift = 0;
+            while (left < right)
+            {
+                left >>= 1;
+                right >>= 1;
+                shift++;
+            }
+            return left << shift;
+        }
+    }
+}
