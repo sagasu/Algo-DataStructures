4b7ca147d2b298e4f315500bdd13ac870e49e33f
Mateusz Kopij <sagasu@gmail.com>
2021-10-11 19:12:29 +0100

    Diameter of Binary Tree

---
+++ AlgoTest/LeetCode/Diameter of Binary Tree/Diameter of Binary Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Diameter_of_Binary_Tree
+{
+    class Diameter_of_Binary_Tree
+    {
+        int maxDiameter;
+
+        public int Depth(TreeNode node)
+        {
+            if (node == null) return 0;
+
+            var leftDepth = Depth(node.left);
+            var rightDepth = Depth(node.right);
+            var diameter = leftDepth + rightDepth;
+
+            maxDiameter = Math.Max(diameter, maxDiameter);
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return 0;
+            Depth(root);
+            return maxDiameter;
+        }
+    }
+}
