a53e03833fda1a721a672179e25666856af378e0
Mateusz Kopij <sagasu@gmail.com>
2021-10-14 14:14:16 +0100

    Perfect Squares

---
+++ AlgoTest/LeetCode/Perfect Squares/Perfect Squares.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Perfect_Squares
+{
+    class Perfect_Squares
+    {
+        public int NumSquares(int n)
+        {
+            var dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                var a = 1;
+                var min = i;
+                while (a * a <= i)
+                {
+                    if (dp[i - a * a] + 1 < min)
+                    {
+                        min = dp[i - a * a] + 1;
+                    }
+                    a++;
+                }
+                dp[i] = min;
+            }
+            return dp[n];
+        }
+    }
+}
