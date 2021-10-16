51fc6a2c1420727d24fcd76cfe784cbcb75983a1
Mateusz Kopij <sagasu@gmail.com>
2021-10-16 21:08:35 +0100

    Best Time to Buy and Sell Stock III

--- AlgoTest/LeetCode/BestTimeToBuyAndSellStockIII/BestTimeToBuyAndSellStockIII.cs
+++ AlgoTest/LeetCode/BestTimeToBuyAndSellStockIII/BestTimeToBuyAndSellStockIII.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,49 +14,36 @@ namespace AlgoTest.LeetCode.BestTimeToBuyAndSellStockIII
         {
             var t = new int[] {3, 3, 5, 0, 0, 3, 1, 4};
             Assert.AreEqual(6,MaxProfit(t));
-            maxProfit = 0;
 
             t = new int[] { 1, 2, 3, 4, 5 };
             Assert.AreEqual(4,MaxProfit(t));
-            maxProfit = 0;
 
             t = new int[] { 7, 6, 4, 3, 1 };
             Assert.AreEqual(0, MaxProfit(t));
         }
 
-        // This solution times out
         public int MaxProfit(int[] prices)
         {
-            Dfs(prices, 0, 0, false, 0, 0);
-            return maxProfit;
-        }
-
-        int maxProfit = 0;
-        private void Dfs(int[] prices, int index, int value, bool isBought, int buyPrice, int nrTransactions)
-        {
-            if (nrTransactions == 2 || index == prices.Length)
+            var n = prices.Length;
+            if (n < 2)
             {
-                maxProfit = Math.Max(maxProfit, value);
-                return;
+                return 0;
             }
-
-            for (var i = index; i < prices.Length; i++)
+            var dp1 = new int[n];
+            var minPrice = prices[0];
+            for (var i = 1; i < n; i++)
             {
-                if (isBought)
-                {
-                    if (buyPrice < prices[i])
-                    {
-                        Dfs(prices, i+1, value + prices[i] - buyPrice, false, 0, nrTransactions+1);
-                    }
-                }
-                else
-                {
-                    Dfs(prices, i+1, value, isBought, buyPrice, nrTransactions);
-                    Dfs(prices, i+1, value, true, prices[i], nrTransactions);
-                }
-
-
+                minPrice = Math.Min(minPrice, prices[i]);
+                dp1[i] = Math.Max(dp1[i - 1], prices[i] - minPrice);
             }
+            var dp2 = new int[n];
+            var maxPrice = prices[n - 1];
+            for (var i = n - 2; i >= 0; i--)
+            {
+                maxPrice = Math.Max(maxPrice, prices[i]);
+                dp2[i] = Math.Max(dp2[i + 1], maxPrice - prices[i]);
+            }
+            return dp1.Zip(dp2, (x, y) => x + y).Max();
         }
     }
 }
