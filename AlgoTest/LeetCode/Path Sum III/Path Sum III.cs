3c2bbe5fc7aab432dfd67e70bb70eb636bc148a9
Mateusz Kopij <sagasu@gmail.com>
2021-10-17 18:25:01 +0100

    Path Sum III

---
+++ AlgoTest/LeetCode/Path Sum III/Path Sum III.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Path_Sum_III
+{
+    class Path_Sum_III
+    {
+        public int PathSum(TreeNode root, int sum, bool start = true)
+        {
+            if (root == null) return 0;
+
+            var count = PathSum(root.left, sum - root.val, false) + PathSum(root.right, sum - root.val, false);
+
+            if (start)
+            {
+                count += PathSum(root.left, sum, true) + PathSum(root.right, sum, true);
+            }
+
+            if (sum - root.val == 0) count++;
+
+            return count;
+        }
+    }
+}
