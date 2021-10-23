a4f1e0aee67a78549462f5b4012b0c52b50c48c2
Mateusz Kopij <sagasu@gmail.com>
2021-10-23 23:01:46 +0100

    Next_Greater_Element_I

---
+++ AlgoTest/LeetCode/Next Greater Element I/Next Greater Element I.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Next_Greater_Element_I
+{
+    class Next_Greater_Element_I
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            var dict = new Dictionary<int, int>();
+            var stack = new Stack<int>();
+
+            foreach (var n in nums2)
+            {
+                while (stack.Any() && stack.Peek() < n)
+                    dict[stack.Pop()] = n;
+                stack.Push(n);
+            }
+
+            return nums1.Select(x => dict.GetValueOrDefault(x, -1)).ToArray();
+        }
+    }
+}
