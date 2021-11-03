f0e58b13fd8e6efd4bdf494a611ef5ce0c7525d0
Mateusz Kopij <sagasu@gmail.com>
2021-11-03 23:31:35 +0000

    Sum Root to Leaf Numbers

---
+++ AlgoTest/LeetCode/SumRoottoLeafNumbers/SumRoottoLeafNumbers Nicer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SumRoottoLeafNumbers
+{
+    class SumRoottoLeafNumbers_Nicer
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            SumNumb(root, "");
+            return sum;
+        }
+
+        private int sum = 0;
+
+        private void SumNumb(TreeNode root, string numb)
+        {
+            numb += root.val;
+
+            if(root.left != null) SumNumb(root.left, numb);
+
+            if(root.right != null) SumNumb(root.right, numb);
+
+            if (root.left == null && root.right == null && !string.IsNullOrEmpty(numb)) sum += int.Parse(numb);
+        }
+    }
+}
