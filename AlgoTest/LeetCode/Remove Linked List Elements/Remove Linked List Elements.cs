2e1290a6448346f74bc67c8dce9351319cc6a445
Mateusz Kopij <sagasu@gmail.com>
2021-11-12 19:18:21 +0000

    Remove_Linked_List_Elements

---
+++ AlgoTest/LeetCode/Remove Linked List Elements/Remove Linked List Elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Remove_Linked_List_Elements
+{
+    public class Remove_Linked_List_Elements
+    {
+        public void Test()
+        {
+
+        }
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null) return null;
+
+            head.next = RemoveElements(head.next, val);
+
+            return head.val == val ? head.next : head;
+        }
+    }
+}
