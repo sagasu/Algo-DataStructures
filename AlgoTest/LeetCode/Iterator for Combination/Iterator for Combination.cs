95684ffc3d6d66502a9a064c67bc5dddcec9aa5f
Mateusz Kopij <sagasu@gmail.com>
2021-11-14 13:56:28 +0000

    Iterator for Combination

---
+++ AlgoTest/LeetCode/Iterator for Combination/Iterator for Combination.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Iterator_for_Combination
+{
+    class CombinationIterator
+    {
+        IList<string> comb = new List<string>();
+        int prevIndex = -1;
+
+        public CombinationIterator(string characters, int combinationLength)
+        {
+            Backtrack(characters, combinationLength, 0, new List<char>(), comb);
+        }
+
+        private void Backtrack(string chars, int len, int st, IList<char> li, IList<string> comb)
+        {
+            if (li.Count == len)
+            {
+                var str = new string(li.ToArray());
+                comb.Add(str);
+                return;
+            }
+
+            for (var i = st; i < chars.Length; i++)
+            {
+                if (li.Count + 1 > len) continue;
+                
+                li.Add(chars[i]);
+                Backtrack(chars, len, i + 1, li, comb);
+                li.RemoveAt(li.Count - 1);
+            }
+        }
+
+        public string Next()
+        {
+            return comb[++prevIndex];
+        }
+
+        public bool HasNext()
+        {
+            return prevIndex != comb.Count - 1;
+        }
+    }
+}
