befe21755a8624fba55a9e7ec185dbdc61f7081e
Mateusz Kopij <sagasu@gmail.com>
2021-11-20 08:53:18 +0000

    Single Element in a Sorted Array

---
+++ AlgoTest/LeetCode/Single Element in a Sorted Array/Single Element in a Sorted Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Single_Element_in_a_Sorted_Array
+{
+    class SingleElementinaSortedArray
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            var ans = 0;
+            var numsCount = (from num in nums
+                group num by num into numGrp
+                orderby numGrp.Count() ascending
+                select numGrp.Key).ToList();
+
+            return numsCount[0];
+        }
+    }
+}
