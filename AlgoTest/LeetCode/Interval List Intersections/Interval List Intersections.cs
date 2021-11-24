46d7abab5f59c39e93e5eafe6e147d2a291bb5c8
Mateusz Kopij <sagasu@gmail.com>
2021-11-24 20:01:51 +0000

    Interval List Intersections

---
+++ AlgoTest/LeetCode/Interval List Intersections/Interval List Intersections.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Interval_List_Intersections
+{
+    class Interval_List_Intersections
+    {
+        public int[][] IntervalIntersection(int[][] a, int[][] b)
+        {
+            IList<int[]> res = new List<int[]>();
+
+            var i = 0;
+            var j = 0;
+
+            while (i != a.Length && j != b.Length)
+            {
+                var intervalA = a[i];
+                var intervalB = b[j];
+
+                if (intervalB[0] > intervalA[1]) {
+                    i++;
+                    continue;
+                }
+
+                if (intervalB[1] < intervalA[0]) {
+                    j++;
+                    continue;
+                }
+
+                res.Add(new int[] {
+                    Math.Max(intervalA[0], intervalB[0]),
+                    Math.Min(intervalA[1], intervalB[1])
+                });
+
+                if (intervalA[1] < intervalB[1]) i++;
+                else j++;
+            }
+
+            return res.ToArray();
+        }
+    }
+}
