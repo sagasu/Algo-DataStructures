208ce1d3970d609bd3d15c8d031b0ce09a0a5568
Mateusz Kopij <sagasu@gmail.com>
2021-11-26 19:31:07 +0000

    Search_Insert_Position

---
+++ AlgoTest/LeetCode/SearchInsertPosition/Search Insert Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.SearchInsertPosition
+{
+    class Search_Insert_Position
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums[nums.Length - 1] < target) return nums.Length;
+
+            var low = 0;
+            var hight = nums.Length - 1;
+
+            while (low < hight) {
+                var mid = (hight + low) / 2;
+
+                if (target > nums[mid]) low = mid + 1;
+                else hight = mid;
+            }
+
+            return low;
+        }
+    }
+}
+
