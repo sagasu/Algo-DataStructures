85aa123ea7785f65f6391dc870ee9bfec3168a85
Mateusz Kopij <sagasu@gmail.com>
2021-11-28 17:50:04 +0000

    All Paths From Source To Target

---
+++ AlgoTest/LeetCode/All Paths From Source to Target/AllPathsFromSourceToTarget.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.All_Paths_From_Source_to_Target
+{
+    class AllPathsFromSourceToTarget
+    {
+        public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+            var list = new List<IList<int>>();
+
+            Backtrack(list, graph, new List<int> { 0 }, 0);
+
+            return list;
+        }
+
+        private void Backtrack(List<IList<int>> list, int[][] graph, List<int> temp, int p)
+        {
+            if (temp.Count > 1 && temp[temp.Count - 1] == graph.Length - 1) list.Add(new List<int>(temp));
+
+            for (var i = 0; i < graph[p].Length; i++)
+            {
+                temp.Add(graph[p][i]);
+
+                Backtrack(list, graph, temp, graph[p][i]);
+
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
