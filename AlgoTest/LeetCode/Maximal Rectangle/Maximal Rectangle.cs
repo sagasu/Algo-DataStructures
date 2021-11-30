deb4be245aa22e1d63073ff65b725816b45c8551
Mateusz Kopij <sagasu@gmail.com>
2021-11-30 20:02:57 +0000

    Maximal Rectangle

---
+++ AlgoTest/LeetCode/Maximal Rectangle/Maximal Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Maximal_Rectangle
+{
+    class Maximal_Rectangle
+    {
+        public int MaximalRectangle(char[][] matrix)
+        {
+            if (matrix == null || matrix.Length <= 0) return 0;
+
+            var maxArea = 0;
+            var dp = new int[matrix[0].Length];
+
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            for (var j = 0; j < matrix[i].Length; j++)
+                if (matrix[i][j] != '0'){
+                        var minHeight = int.MaxValue;
+                        dp[j] += Convert.ToInt16(matrix[i][j] - '0');
+
+                        for (var k = j; k >= 0 && matrix[i][k] > 0; k--)
+                        {
+                            minHeight = Math.Min(minHeight, dp[k]);
+                            maxArea = Math.Max(maxArea, minHeight * (j - k + 1));
+                        }
+                }else dp[j] = 0;
+    
+            return maxArea;
+        }
+	}
+}
