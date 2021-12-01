fb3ee476c7048fe3d32ef18b90666ca31c6515d8
Mateusz Kopij <sagasu@gmail.com>
2021-12-01 20:11:54 +0000

    House Robber

---
+++ AlgoTest/LeetCode/House Robber/House Robber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.House_Robber
+{
+    class House_Robber
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            
+            var firstPrevious = 0;
+            var current = 0;
+
+            foreach (var num in nums) {
+                var secondPrevious = firstPrevious;
+                firstPrevious = current;
+                current = Math.Max(firstPrevious, secondPrevious + num);
+            }
+
+            return current;
+        }
+    }
+}
