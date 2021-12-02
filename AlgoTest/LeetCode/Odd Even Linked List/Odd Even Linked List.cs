47dfc06ae93491d084b600e7d574ad22c9fee1a0
Mateusz Kopij <sagasu@gmail.com>
2021-12-02 07:18:26 +0000

    Odd Even Linked List

---
+++ AlgoTest/LeetCode/Odd Even Linked List/Odd Even Linked List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Odd_Even_Linked_List
+{
+    public class Odd_Even_Linked_List
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            var oddHead = new ListNode();
+            var oddCurrent = oddHead;
+            var evenHead = new ListNode();
+            var evenCurrent = evenHead;
+
+            var current = head;
+            while (current != null)
+            {
+                oddCurrent.next = current;
+                oddCurrent = oddCurrent.next;
+
+                current = current.next;
+
+                if (current != null)
+                {
+                    evenCurrent.next = current;
+                    evenCurrent = evenCurrent.next;
+                    current = current.next;
+                }
+
+            }
+
+            oddCurrent.next = evenHead.next;
+            evenCurrent.next = null;
+            return oddHead.next;
+        }
+    }
+}
