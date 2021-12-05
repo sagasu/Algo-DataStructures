f267af6d85d108d51bf584ac442d5f5db08162fa
Mateusz Kopij <sagasu@gmail.com>
2021-12-05 17:54:45 +0000

    House Robber III

---
+++ AlgoTest/LeetCode/House Robber III/House Robber III.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.House_Robber_III
+{
+    public class House_Robber_III
+    {
+        public int Rob(TreeNode root)
+        {
+            var arr = DFS(root);
+            return Math.Max(arr[0], arr[1]);
+        }
+
+        private int[] DFS(TreeNode node)
+        {
+            if (node == null) return new int[2];
+            
+            var left = DFS(node.left);
+            var right = DFS(node.right);
+            var ret = new int[2];
+
+            ret[0] = left.Max() + right.Max(); 
+            ret[1] = node.val + left[0] + right[0]; 
+            return ret;
+        }
+    }
+}
