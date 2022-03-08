d7582a92902aefc07a1ff5d8140e6c784758f3d1
Mateusz Kopij <sagasu@gmail.com>
2022-03-08 19:07:53 +0000

    Linked List Cycle

---
+++ AlgoTest/LeetCode/Linked List Cycle/Linked List Cycle.cs
@@ -0,0 +1,27 @@
+﻿using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Linked_List_Cycle
+{
+    internal class Linked_List_Cycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            var slow = head;
+            var fast = head.next;
+
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null)
+                {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return true;
+        }
+    }
+}
