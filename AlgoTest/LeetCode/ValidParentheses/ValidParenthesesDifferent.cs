f468b1f7b4821d06cbd3d928a41c41296fb86da5
Mateusz Kopij <sagasu@gmail.com>
2022-03-13 20:36:28 +0000

    Valid Parentheses

---
+++ AlgoTest/LeetCode/ValidParentheses/ValidParenthesesDifferent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.ValidParentheses
+{
+    public class ValidParenthesesDifferent
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                switch (stack.Count)
+                {
+                    case > 0 when c == ')' && stack.Peek() == '(':
+                    case > 0 when c == '}' && stack.Peek() == '{':
+                    case > 0 when c == ']' && stack.Peek() == '[':
+                        stack.Pop();
+                        break;
+                    default:
+                        stack.Push(c);
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
