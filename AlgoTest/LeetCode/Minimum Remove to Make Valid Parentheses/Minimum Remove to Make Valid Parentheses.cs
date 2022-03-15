e335199654741121bea351cb63a355d0a8cec91a
Mateusz Kopij <sagasu@gmail.com>
2022-03-15 20:33:06 +0000

    Minimum Remove to Make Valid Parentheses

---
+++ AlgoTest/LeetCode/Minimum Remove to Make Valid Parentheses/Minimum Remove to Make Valid Parentheses.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Minimum_Remove_to_Make_Valid_Parentheses
+{
+    internal class Minimum_Remove_to_Make_Valid_Parentheses
+    {
+        public string MinRemoveToMakeValid(string s)
+        {
+            var stack = new Stack<(char c, int index)>();
+
+            var n = s.Length;
+            for (var i = 0; i < n; i++)
+            {
+                var c = s[i];
+                if (stack.Count > 0 && stack.Peek().c == '(' && c == ')') stack.Pop();
+                else if (c == '(' || c == ')') stack.Push((c, i));
+            }
+
+            var sb = new StringBuilder(s);
+            foreach (var kv in stack) sb = sb.Remove(kv.index, 1);
+            
+            return sb.ToString();
+        }
+    }
+}
