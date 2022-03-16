97d40caa3b68b68f0e6cbec304fb8c497e554343
Mateusz Kopij <sagasu@gmail.com>
2022-03-16 23:47:54 +0000

    Validate Stack Sequences

---
+++ AlgoTest/Validate Stack Sequences/Validate Stack Sequences.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.Validate_Stack_Sequences
+{
+    internal class Validate_Stack_Sequences
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            var st = new Stack<int>();
+            var j = 0;
+
+            foreach (var item in pushed)
+            {
+                st.Push(item);
+                while (st.Count > 0 && st.Peek() == popped[j])
+                {
+                    st.Pop();
+                    j++;
+                }
+            }
+
+            return j == popped.Length;
+        }
+    }
+}
