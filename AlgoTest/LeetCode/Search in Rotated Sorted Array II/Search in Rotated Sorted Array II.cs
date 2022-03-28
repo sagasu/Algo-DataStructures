cd54165fb2f5ed1c16d89334b87e33272e75aeba
mateuszkopij <mateusz.kopij@etxcapital.com>
2022-03-28 21:06:58 +0200

    Rotated Sorted Array II

---
+++ AlgoTest/LeetCode/Search in Rotated Sorted Array II/Search in Rotated Sorted Array II.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Search_in_Rotated_Sorted_Array_II
+{
+    internal class Search_in_Rotated_Sorted_Array_II
+    {
+        public bool Search(int[] nums, int target)
+        {
+            var i = nums.Length - 1;
+            for (; i >= 0; i--)
+            {
+                if (nums[i] == target) return true;
+                if (nums[i] < target) break;
+            }
+
+            for (var j = 0; j < i; j++)
+            {
+                if (nums[j] == target) return true;
+                if (nums[j] > target) return false;
+            }
+
+            return false;
+        }
+    }
+}
