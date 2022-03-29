dfdb28b59b193d23c3d3693892aa6006887f8d4a
mateuszkopij <mateusz.kopij@etxcapital.com>
2022-03-29 19:18:19 +0200

    Break a Palindrome

---
+++ AlgoTest/LeetCode/BreakAPalidrome/BreakAPalidrome.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgoTest.LeetCode.BreakAPalidrome
+{
+    internal class BreakAPalidrome
+    {
+        public string BreakPalindrome(string palindrome)
+        {
+            if (palindrome == null || palindrome.Length <= 1)
+                return "";
+
+            var arr = palindrome.ToCharArray();
+            var i = 0;
+            for (; i < arr.Length / 2; i++)
+            {
+                if (arr[i] != 'a')
+                {
+                    arr[i] = 'a';
+                    break;
+                }
+            }
+
+            if (i == arr.Length / 2)
+                arr[arr.Length - 1] = 'b';
+
+            return new string(arr);
+        }
+    }
+}
