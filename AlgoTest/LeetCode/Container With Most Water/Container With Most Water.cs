220a0d23a7264bc86d3e7283b466e6e0ea3a37ff
Mateusz Kopij <sagasu@gmail.com>
2022-04-05 20:14:31 +0100

    Container_With_Most_Water

---
+++ AlgoTest/LeetCode/Container With Most Water/Container With Most Water.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Container_With_Most_Water;
+
+internal class Container_With_Most_Water
+{
+    public int MaxArea(int[] h)
+    {
+        int start = 0, end = h.Length - 1, maxArea = 0;
+        while (start < end)
+        {
+            maxArea = Math.Max(maxArea, Math.Min(h[start], h[end]) * (end - start));
+            if (h[start] < h[end]) start++;
+            else end--;
+        }
+
+        return maxArea;
+    }
+}
