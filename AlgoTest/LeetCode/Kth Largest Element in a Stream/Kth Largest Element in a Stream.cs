f2e86de56d8fae680db9489d660cb4989a1a434a
Mateusz Kopij <sagasu@gmail.com>
2022-04-08 20:19:00 +0100

    Kth Largest Element in a Stream

---
+++ AlgoTest/LeetCode/Kth Largest Element in a Stream/Kth Largest Element in a Stream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Kth_Largest_Element_in_a_Stream
+{
+    internal class KthLargest
+    {
+        PriorityQueue<int, int> pq = new();
+        int k = 0;
+        public KthLargest(int k, int[] nums)
+        {
+            this.k = k;
+            foreach (int num in nums)
+                this.Add(num);
+        }
+
+        public int Add(int val)
+        {
+            pq.Enqueue(val, val);
+            if (pq.Count > k) pq.Dequeue();
+            return pq.Peek();
+        }
+    }
+}
