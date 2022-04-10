03d0cbe67da9318c9d2f180dc9bbd8953eb5d536
Mateusz Kopij <sagasu@gmail.com>
2022-04-10 21:45:29 +0100

    Climbing Stairs

---
+++ AlgoTest/LeetCode/Baseball Game/Baseball Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Baseball_Game
+{
+    internal class Baseball_Game
+    {
+        public int CalPoints(string[] ops)
+        {
+            var stack = new Stack<int>();
+            foreach (var op in ops)
+            {
+                switch (op)
+                {
+                    case "D":
+                        stack.Push(2 * stack.Peek());
+                        break;
+                    case "C":
+                        stack.Pop();
+                        break;
+                    case "+":
+                        var poppedValue = stack.Pop();
+                        var newValue = poppedValue + stack.Peek();
+                        stack.Push(poppedValue);
+                        stack.Push(newValue);
+                        break;
+                    default:
+                        stack.Push(Convert.ToInt32(op));
+                        break;
+                }
+            }
+            return stack.Sum();
+        }
+    }
+}
