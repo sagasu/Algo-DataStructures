63401a9a936d7f1a6bc9375c9311247b25c0c90f
Mateusz Kopij <sagasu@gmail.com>
2022-04-11 21:37:16 +0100

    Shift 2D Grid

---
+++ AlgoTest/LeetCode/Shift 2D Grid/Shift 2D Grid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Shift_2D_Grid;
+
+internal class Shift_2D_Grid
+{
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+    {
+        List<IList<int>> res = new List<IList<int>>();
+        List<int> all = grid.SelectMany(o => o).ToList();
+        int tot = all.Count();
+        for (int i = 0; i < grid.Length; i++)
+        {
+            List<int> tmp = new List<int>();
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                int pt = (i * grid[0].Length + j - k) % tot;
+                while (pt < 0)
+                {
+                    pt += tot;
+                }
+                tmp.Add(all[pt]);
+            }
+            res.Add(tmp);
+        }
+        return res;
+    }
+}
