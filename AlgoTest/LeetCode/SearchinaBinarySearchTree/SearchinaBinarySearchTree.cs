3ed95763e1c30e4b2849274b08c8d02d9d7a6c11
mateuszkopij <mateusz.kopij@etxcapital.com>
2022-04-14 18:54:59 +0200

    Searchin a Binary Search Tree

--- AlgoTest/LeetCode/SearchinaBinarySearchTree/SearchinaBinarySearchTree.cs
+++ AlgoTest/LeetCode/SearchinaBinarySearchTree/SearchinaBinarySearchTree.cs
@@ -8,17 +8,13 @@ namespace AlgoTest.LeetCode.SearchinaBinarySearchTree
     {
         public TreeNode SearchBST(TreeNode root, int val)
         {
-            if (root == null)
-                return null;
+            if (root == null) return null;
 
-            if (root.val == val)
-                return root;
+            if (root.val == val) return root;
 
-            if (root.val < val)
-                return SearchBST(root.right, val);
+            if (root.val < val) return SearchBST(root.right, val);
 
-            if (root.val > val)
-                return SearchBST(root.left, val);
+            if (root.val > val) return SearchBST(root.left, val);
 
             return null;
         }
