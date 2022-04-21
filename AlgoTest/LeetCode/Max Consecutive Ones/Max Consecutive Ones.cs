12b437ddf392489146d1d0e649bfc493e0dad358
Mateusz Kopij <sagasu@gmail.com>
2022-04-21 22:46:13 +0100

    Max Consecutive Ones

---
+++ AlgoTest/LeetCode/Max Consecutive Ones/Max Consecutive Ones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.MaxCosecutiveOnes
+{
+    internal class MaxCosecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var s = 0;
+            var maxCon = -1;
+            for (var e = 0; e < nums.Length; e++)
+            {
+                if (nums[e] == 0) { s = e + 1; }
+                maxCon = Math.Max(maxCon, e - s + 1);
+            }
+            return maxCon;
+        }
+    }
+}
--- AlgoTest/LeetCode/Maxcosecutiveones.cs
+++
@@ -1,8 +0,0 @@
-var s=0;
-        int maxCon=-1;
-        for(int e=0;e<nums.Length;e++)
-        {
-            if(nums[e]==0){s=e+1;}
-            maxCon=Math.Max(maxCon,e-s+1);
-        }
-        return maxCon;
