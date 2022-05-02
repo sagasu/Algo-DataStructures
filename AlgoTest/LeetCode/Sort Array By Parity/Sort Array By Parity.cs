27beb08a0685ff471fd3a8d02d4b2b065ab34138
Mateusz Kopij <sagasu@gmail.com>
2022-05-02 19:44:52 +0100

    Sort Array By Parity

---
+++ AlgoTest/LeetCode/Sort Array By Parity/Sort Array By Parity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Sort_Array_By_Parity
+{
+    internal class Sort_Array_By_Parity
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            var sorted = new List<int>();
+
+            foreach (var i in A)
+            {
+                if (i % 2 == 0) sorted.Insert(0, i);
+                else sorted.Add(i);
+            }
+
+            return sorted.ToArray();
+        }
+    }
+}
