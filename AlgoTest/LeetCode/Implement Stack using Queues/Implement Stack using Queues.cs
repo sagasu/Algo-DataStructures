5679ef844cb9fe30865632b6319b7b039e9ea097
Mateusz Kopij <sagasu@gmail.com>
2022-05-05 06:31:31 +0100

    Implement  Stack using Queues

---
+++ AlgoTest/LeetCode/Implement Stack using Queues/Implement Stack using Queues.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Implement_Stack_using_Queues
+{
+    internal class MyStack //Implement_Stack_using_Queues
+    {
+        internal Queue<int> q1 = null;
+        internal List<int> lst = null;
+        public MyStack()
+        {
+            this.q1 = new Queue<int>();
+            this.lst = new List<int>();
+        }
+        public void Push(int x)
+        {
+            q1.Enqueue(x);
+            lst.Insert(0, q1.Dequeue());
+        }
+        public int Pop()
+        {
+            var x = lst[0];
+            lst.RemoveAt(0);
+            return x;
+        }
+        public int Top()
+        {
+            return lst[0];
+        }
+        public bool Empty()
+        {
+            return lst.Count() == 0;
+        }
+    }
+}
