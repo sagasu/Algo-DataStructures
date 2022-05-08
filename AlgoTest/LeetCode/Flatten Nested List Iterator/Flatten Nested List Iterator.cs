086d0be47cd1bd2a0a81d879fae537631f1dd293
Mateusz Kopij <sagasu@gmail.com>
2022-05-08 07:22:44 +0100

    Nested Iterator

--- AlgoTest/LeetCode/Flatten Nested List Iterator/Flatten Nested List Iterator.cs
+++ AlgoTest/LeetCode/Flatten Nested List Iterator/Flatten Nested List Iterator.cs
@@ -1,24 +1,57 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AlgoTest.LeetCode.Flatten_Nested_List_Iterator
 {
-    public class Flatten_Nested_List_Iterator
+    public class NestedIterator
     {
-        //public NestedIterator(IList<NestedInteger> nestedList)
-        //{
+        private Stack<NestedInteger> stack = new Stack<NestedInteger>();
+        private void pushList(IList<NestedInteger> list)
+        {
+            for (int i = list.Count - 1; i >= 0; --i)
+            {
+                if (list[i].IsInteger() || list[i].GetList().Count > 0)
+                {
+                    stack.Push(list[i]);
+                }
+            }
+        }
 
-        //}
+        private void skipToNextInteger()
+        {
+            while (stack.Count > 0 && !stack.Peek().IsInteger())
+            {
+                var t = stack.Pop();
+                pushList(t.GetList());
+            }
+        }
 
-        //public bool HasNext()
-        //{
+        public NestedIterator(IList<NestedInteger> nestedList)
+        {
+            pushList(nestedList);
+            skipToNextInteger();
+        }
 
-        //}
+        public bool HasNext()
+        {
+            return stack.Count > 0;
+        }
 
-        //public int Next()
-        //{
+        public int Next()
+        {
+            var v = stack.Pop();
+            skipToNextInteger();
+            return v.GetInteger();
+        }
+    }
 
-        //}
+    public interface NestedInteger
+    {
+
+           bool IsInteger(); 
+           int GetInteger();
+           IList<NestedInteger> GetList();
     }
 }
