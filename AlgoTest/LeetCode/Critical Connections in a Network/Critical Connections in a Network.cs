db9eb4b6543de49664eb8e0c3d51a855d5ea7303
Mateusz Kopij <sagasu@gmail.com>
2022-05-18 21:35:25 +0100

    Critical Connections in a Network

--- AlgoTest/LeetCode/Critical Connections in a Network/Critical Connections in a Network.cs
+++ AlgoTest/LeetCode/Critical Connections in a Network/Critical Connections in a Network.cs
@@ -8,7 +8,45 @@ namespace AlgoTest.LeetCode.Critical_Connections_in_a_Network
     {
         public IList<IList<int>> CriticalConnections(int n, IList<IList<int>> connections)
         {
-            return new List<IList<int>>();
+            var critCon = new List<IList<int>>();
+            var graph = GetGraph(n, connections);
+            var disTime = 1;
+            GetDisAndLowValue(graph, 0, 0, ref disTime, new int[n], new int[n], critCon);
+            return critCon;
+        }
+
+        private int GetDisAndLowValue(IList<IList<int>> graph, int node, int parent, ref int disTime, int[] dicArr, int[] lowArr, IList<IList<int>> critCon)
+        {
+            if (dicArr[node] != 0) return dicArr[node];
+
+            dicArr[node] = disTime++;
+            lowArr[node] = dicArr[node];
+
+            foreach (var nd in graph[node])
+            {
+                if (nd == parent) continue;
+
+                var lowV = GetDisAndLowValue(graph, nd, node, ref disTime, dicArr, lowArr, critCon);
+                lowArr[node] = Math.Min(lowArr[node], lowV);
+
+                if (dicArr[node] < lowArr[nd]) critCon.Add(new List<int> { node, nd });
+            }
+
+            return lowArr[node];
+        }
+
+        private IList<IList<int>> GetGraph(int n, IList<IList<int>> connections)
+        {
+            var graph = new List<IList<int>>();
+
+            for (var i = 0; i < n; i++) graph.Add(new List<int>());
+
+            foreach (var con in connections)
+            {
+                graph[con[0]].Add(con[1]);
+                graph[con[1]].Add(con[0]);
+            }
+            return graph;
         }
     }
 }
