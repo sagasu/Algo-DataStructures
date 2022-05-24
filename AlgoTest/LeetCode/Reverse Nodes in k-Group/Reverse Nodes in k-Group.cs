7e6f58b8689939c5d9b08bf2f3361c14c4fc1b66
Mateusz Kopij <sagasu@gmail.com>
2022-05-24 21:30:45 +0100

    Reverse Nodes in k-Group

---
+++ AlgoTest/LeetCode/Reverse Nodes in k-Group/Reverse Nodes in k-Group.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Reverse_Nodes_in_k_Group
+{
+    internal class Reverse_Nodes_in_k_Group
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            var kList = new List<ListNode>();
+            var counter = 0;
+            while (head != null)
+            {
+                if (counter == 0) kList.Add(head);
+                
+                counter++;
+
+                var temp = head.next;
+                
+                if (counter == k)
+                {
+                    counter = 0;
+                    head.next = null;
+                }
+
+                head = temp;
+            }
+
+            if (kList.Count == 0) return head;
+            
+            var dummy = new ListNode(0);
+            var runner = dummy;
+
+            for (var i = 0; i < kList.Count - 1; i++)
+            {
+                var temp = kList[i];
+                runner.next = ReverseList(kList[i]);
+                runner = temp;
+            }
+
+            runner.next = counter > 0 ? kList[^1] : ReverseList(kList[^1]);
+            
+            return dummy.next;
+        }
+
+
+        private ListNode ReverseList(ListNode head)
+        {
+            ListNode pre = null;
+            while (head != null)
+            {
+                var temp = head.next;
+                head.next = pre;
+                pre = head;
+                head = temp;
+            }
+            return pre;
+        }
+    }
+}
