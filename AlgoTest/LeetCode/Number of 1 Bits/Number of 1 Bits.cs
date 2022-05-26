586f4fbf11a52ed183a6a796bcd0152656513145
Mateusz Kopij <sagasu@gmail.com>
2022-05-26 14:23:35 +0100

    Number of 1 Bits

---
+++ AlgoTest/LeetCode/Number of 1 Bits/Number of 1 Bits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Number_of_1_Bits
+{
+    internal class Number_of_1_Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int result = 0;
+            while (n > 0)
+            {
+                result += (int)n & 1;
+                n = n >> 1;
+            }
+
+            return result;
+        }
+    }
+}
