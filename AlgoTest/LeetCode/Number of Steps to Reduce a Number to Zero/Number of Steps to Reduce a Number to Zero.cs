e770caf403612d335c0b62b1aa7acbc140b76462
Mateusz Kopij <sagasu@gmail.com>
2022-05-27 12:18:33 +0100

    Number of Steps to Reduce a Number to Zero

---
+++ AlgoTest/LeetCode/Number of Steps to Reduce a Number to Zero/Number of Steps to Reduce a Number to Zero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Number_of_Steps_to_Reduce_a_Number_to_Zero
+{
+    internal class Number_of_Steps_to_Reduce_a_Number_to_Zero
+    {
+        public int NumberOfSteps(int num)
+        {
+            var count = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0) num /= 2;
+                else num--;
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
