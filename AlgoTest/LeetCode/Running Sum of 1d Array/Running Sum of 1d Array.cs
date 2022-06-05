b0e2d4dba0fd421f2e1d03210ac49eaccbf99162
Mateusz Kopij <sagasu@gmail.com>
2022-06-05 21:27:48 +0100

    Running_Sum_of_1d_Array

---
+++ AlgoTest/LeetCode/Running Sum of 1d Array/Running Sum of 1d Array.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Running_Sum_of_1d_Array
+{
+    internal class Running_Sum_of_1d_Array
+    {
+        public int[] RunningSum(int[] nums) => nums.Select((num, i) => i is 0 ? num : nums[i] += nums[i - 1]).ToArray();
+    }
+}
