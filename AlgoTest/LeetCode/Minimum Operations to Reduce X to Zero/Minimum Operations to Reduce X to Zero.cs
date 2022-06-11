d3ac7df147092b3ca5437e0c80d05b148f2e85dd
Mateusz Kopij <sagasu@gmail.com>
2022-06-11 11:40:04 +0100

    Minimum Operations to Reduce X to Zero

---
+++ AlgoTest/LeetCode/Minimum Operations to Reduce X to Zero/Minimum Operations to Reduce X to Zero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Minimum_Operations_to_Reduce_X_to_Zero
+{
+    internal class Minimum_Operations_to_Reduce_X_to_Zero
+    {
+        public int MinOperations(int[] nums, int x)
+        {
+            var target = nums.Sum() - x;
+
+            var maximumLengthOfMiddle = int.MinValue;
+            var sum = 0;
+            var left = 0;
+            for (var right = 0; right < nums.Length; right++)
+            {
+                sum += nums[right];
+                while (left <= right && target < sum)
+                {
+                    sum -= nums[left];
+                    left++;
+                }
+
+                if (sum == target) maximumLengthOfMiddle = Math.Max(maximumLengthOfMiddle, right - left + 1);
+            }
+
+            return maximumLengthOfMiddle == int.MinValue ? -1 : nums.Length - maximumLengthOfMiddle;
+        }
+    }
+}
