4a024fc04e3fc8019926cd91f5cbb7f4857a1393
Mateusz Kopij <sagasu@gmail.com>
2022-06-22 19:43:44 +0100

    Kth Largest Element in an Array

---
+++ AlgoTest/LeetCode/Kth Largest Element in an Array/Kth Largest Element in an Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Kth_Largest_Element_in_an_Array
+{
+    internal class Kth_Largest_Element_in_an_Array
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var queue = new PriorityQueue<int, int>();
+
+            foreach (var num in nums)
+            {
+                queue.Enqueue(num, num);
+                if (queue.Count <= k) continue;
+                queue.Dequeue();
+            }
+
+            return queue.Peek();
+        }
+    }
+}
