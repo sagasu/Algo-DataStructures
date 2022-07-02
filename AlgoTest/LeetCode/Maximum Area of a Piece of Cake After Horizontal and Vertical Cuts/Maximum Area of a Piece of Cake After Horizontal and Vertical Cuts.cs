e2197b3dd43452ca28563c62370046f3999227bb
Mateusz Kopij <sagasu@gmail.com>
2022-07-02 18:36:03 +0100

    Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ AlgoTest/LeetCode/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Maximum_Area_of_a_Piece_of_Cake_After_Horizontal_and_Vertical_Cuts
+{
+    internal class Maximum_Area_of_a_Piece_of_Cake_After_Horizontal_and_Vertical_Cuts
+    {
+        public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) => (int)(((long)
+                horizontalCuts.OrderBy(c => c).Prepend(0).Zip(horizontalCuts.OrderBy(c => c).Append(h), (f, s) => s - f).Max() *
+                verticalCuts.OrderBy(c => c).Prepend(0).Zip(verticalCuts.OrderBy(c => c).Append(w), (f, s) => s - f).Max()
+            ) % 1000000007);
+    }
+}
