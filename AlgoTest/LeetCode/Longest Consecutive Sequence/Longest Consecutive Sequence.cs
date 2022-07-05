245a0197d5b53dc0b7646020e9ef83f481b37d6f
Mateusz Kopij <sagasu@gmail.com>
2022-07-05 19:50:19 +0100

    Longest Consecutive Sequence

---
+++ AlgoTest/LeetCode/Longest Consecutive Sequence/Longest Consecutive Sequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Longest_Consecutive_Sequence
+{
+    internal class Longest_Consecutive_Sequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            var hs = new HashSet<int>(nums);
+            var ret = 0;
+            foreach (var n in hs)
+            {
+                if (hs.Contains(n - 1)) continue;
+                var curr = n;
+                var cnt = 1;
+                while (hs.Contains(curr + cnt)) cnt++;
+
+                ret = Math.Max(ret, cnt);
+            }
+            return ret;
+        }
+    }
+}
