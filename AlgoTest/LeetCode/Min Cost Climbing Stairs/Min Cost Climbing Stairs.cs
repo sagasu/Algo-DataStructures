396c43a9156d0e7bb3898d9eb15d0c8dea09adac
Mateusz Kopij <sagasu@gmail.com>
2022-07-10 20:09:48 +0100

    Min Cost Climbing Stairs

---
+++ AlgoTest/LeetCode/Min Cost Climbing Stairs/Min Cost Climbing Stairs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Min_Cost_Climbing_Stairs
+{
+    internal class Min_Cost_Climbing_Stairs
+    {
+        public int MinCostClimbingStairs(int[] cost) => cost
+            .Aggregate(
+                new[] { 0, 0 },
+                (arr, c) => new[] { arr[1], c + arr.Min() }
+            )
+            .Min();
+    }
+}
