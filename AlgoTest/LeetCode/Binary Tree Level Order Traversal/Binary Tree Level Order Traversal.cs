af8bf5a914534b1595dca368c54d94d77efc4a83
Mateusz Kopij <sagasu@gmail.com>
2022-07-13 05:34:54 +0100

    Binary Tree Level Order Traversal

---
+++ AlgoTest/LeetCode/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Binary_Tree_Level_Order_Traversal
+{
+    internal class Binary_Tree_Level_Order_Traversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+
+            var res = new List<IList<int>>();
+
+            if (root == null)
+                return res;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var count = queue.Count;
+                var row = new List<int>();
+
+                for (var i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    row.Add(node.val);
+
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                }
+
+                res.Add(row);
+            }
+
+            return res;
+        }
+    }
+}
