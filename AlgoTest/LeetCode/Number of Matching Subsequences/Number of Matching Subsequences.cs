bf004ac30db1824937e89606515fd4e2211192ef
Mateusz Kopij <sagasu@gmail.com>
2022-07-20 19:48:03 +0100

    Number of Matching Subsequences

---
+++ AlgoTest/LeetCode/Number of Matching Subsequences/Number of Matching Subsequences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Number_of_Matching_Subsequences
+{
+    internal class Number_of_Matching_Subsequences
+    {
+        public int NumMatchingSubseq(string s, string[] words)
+        {
+            var dict = words.GroupBy(o => o).ToDictionary(o => o.Key, o => o.Count());
+            var res = 0;
+            foreach (var item in dict)
+            {
+                var pt = 0;
+                foreach (var ts in s)
+                {
+                    if (item.Key[pt] == ts) pt++;
+                    if (pt != item.Key.Length) continue;
+                    res += item.Value;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
