b1fdc7ee67d1b6d6ede1b93e7dab4fc11d8c872d
mateuszkopij <mateusz.kopij@etxcapital.com>
2022-07-27 11:29:27 +0200

    flatten binary tree

---
+++ AlgoTest/LeetCode/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Flatten_Binary_Tree_to_Linked_List
+{
+    internal class Flatten_Binary_Tree_to_Linked_List
+    {
+        public void Flatten(TreeNode root)
+        {
+            while (root != null)
+            {
+                if (root.left != null)
+                {
+                    if (root.right == null)
+                    {
+                        root.right = root.left;
+                    }
+                    else
+                    {
+                        var nextRight = root.right;
+                        root.right = root.left;
+                        var cur = root.right;
+                        while (cur.right != null)
+                        {
+                            cur = cur.right;
+                        }
+
+                        cur.right = nextRight;
+                    }
+                    root.left = null;
+                }
+                root = root.right;
+            }
+        }
+}
