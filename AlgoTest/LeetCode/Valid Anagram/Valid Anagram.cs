fc7dab16b5ee9836908d184be7db21aa652094c6
Mateusz Kopij <sagasu@gmail.com>
2022-07-28 16:05:53 +0100

    Valid Anagram

---
+++ AlgoTest/LeetCode/Valid Anagram/Valid Anagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Valid_Anagram
+{
+    [TestClass]
+    public class Valid_Anagram
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(IsAnagram("anagram", "nagaram"));
+            Assert.IsTrue(IsAnagram("", ""));
+            Assert.IsTrue(IsAnagram("a", "a"));
+            Assert.IsTrue(IsAnagram("aaa", "aaa"));
+            Assert.IsFalse(IsAnagram("a", "b"));
+            Assert.IsFalse(IsAnagram("aaa", "bbb"));
+            Assert.IsFalse(IsAnagram("rat", "car"));
+            Assert.IsFalse(IsAnagram("rat", "nagaram"));
+            Assert.IsFalse(IsAnagram("anagram", "car"));
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            var dic = new Dictionary<char, int>();
+            foreach (var chars in s)
+                if (!dic.TryAdd(chars, 1)) dic[chars] = dic[chars] + 1;
+
+            foreach (var chart in t)
+            {
+                if (dic.ContainsKey(chart)) dic[chart] = dic[chart] - 1;
+                else return false;
+            }
+
+            return dic.Values.All(x => x == 0);
+        }
+    }
+}
