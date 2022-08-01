2119aff6cff5b502f603c81a14b2c0edd0b86024
Mateusz Kopij <sagasu@gmail.com>
2022-08-01 18:59:50 +0100

    Unique Paths

--- AlgoTest/LeetCode/UniquePaths/UniquePathsProblem.cs
+++ AlgoTest/LeetCode/UniquePaths/UniquePathsProblem.cs
@@ -18,19 +18,18 @@ namespace AlgoTest.LeetCode.UniquePaths
         public int UniquePaths(int m, int n)
         {
             var dp = new int[m,n];
-            for (var i = 0; i < m; i++) {
+            for (var i = 0; i < m; i++) 
                 dp[i,0] = 1;
-            }
+            
 
             for (var i = 0; i < n; i++)
-            {
                 dp[0,i] = 1;
-            }
+            
 
             for (var i = 1; i < m; i++)
-                for (var j = 1; j < n; j++) {
+                for (var j = 1; j < n; j++)
                     dp[i,j] = dp[i - 1,j] + dp[i,j - 1];
-                }
+                
 
             return dp[m - 1,n - 1];
         }
