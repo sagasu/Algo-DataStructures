b097ce27d5040716f5e2ca3bf7f358412e9f1b57
Mateusz Kopij <sagasu@gmail.com>
2022-08-06 19:11:25 +0100

    Poor Pigs

---
+++ AlgoTest/LeetCode/Poor Pigs/Poor Pigs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Poor_Pigs
+{
+    internal class Poor_Pigs
+    {
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            var i = minutesToTest / minutesToDie + 1;
+            return (int)Math.Ceiling(Math.Log(buckets, i));
+        }
+    }
+}
