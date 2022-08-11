fc8870b3d739a5b96eea44a1b4464ce84849bbfd
Mateusz Kopij <sagasu@gmail.com>
2022-08-11 23:32:52 +0100

    Validate Binary Search Tree

---
+++ AlgoTest/LeetCode/Validate Binary Search Tree/Validate Binary Search Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Validate_Binary_Search_Tree
+{
+    internal class Validate_Binary_Search_Tree
+    {
+        public bool IsValidBST(TreeNode root) => DFS(root, long.MinValue, long.MaxValue);
+        
+        private bool DFS(TreeNode root, long min, long max)
+        {
+            if (root == null) return true;
+
+            if (min < root.val && root.val < max)
+            {
+                var leftResult = DFS(root.left, min, root.val);
+                var rightResult = DFS(root.right, root.val, max);
+
+                if (leftResult && rightResult) return true;
+            }
+
+            return false;
+        }
+    }
+}
