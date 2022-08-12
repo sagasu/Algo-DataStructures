42ac76b3ac66e17976b9247ef6684b1767a4e263
Mateusz Kopij <sagasu@gmail.com>
2022-08-12 22:32:29 +0100

    Lowest Common Ancestor of a Binary Search Tree

---
+++ AlgoTest/LeetCode/Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Lowest_Common_Ancestor_of_a_Binary_Search_Tree
+{
+    internal class Lowest_Common_Ancestor_of_a_Binary_Search_Tree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == p || root == q) return root;
+
+            if (p.val < root.val && q.val < root.val)
+                return LowestCommonAncestor(root.left, p, q);
+            
+
+            if (p.val > root.val && q.val > root.val)
+                return LowestCommonAncestor(root.right, p, q);
+            
+            return root;
+        }
+    }
+}
