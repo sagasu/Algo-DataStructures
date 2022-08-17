70581114dbbbe4113189e676111439346ca8a18c
Mateusz Kopij <sagasu@gmail.com>
2022-08-17 19:11:42 +0100

    Unique_Morse_Code_Words

---
+++ AlgoTest/LeetCode/Unique Morse Code Words/Unique Morse Code Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Unique_Morse_Code_Words
+{
+    internal class Unique_Morse_Code_Words
+    {
+        public int UniqueMorseRepresentations(string[] words) => words
+            .Select(w => string.Concat(w.Select(c =>
+                ".-|-...|-.-.|-..|.|..-.|--.|....|..|.---|-.-|.-..|--|-.|---|.--.|--.-|.-.|...|-|..-|...-|.--|-..-|-.--|--.."
+                    .Split('|')[c - 'a']
+            )))
+            .ToHashSet()
+            .Count;
+    }
+}
