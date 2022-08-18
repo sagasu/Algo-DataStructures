9dc2ab33b93ca9f703c96aab42f2d47f92786178
Mateusz Kopij <sagasu@gmail.com>
2022-08-18 20:23:32 +0100

    Reduce Array Size to The Half

---
+++ AlgoTest/LeetCode/Reduce Array Size to The Half/Reduce Array Size to The Half.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Reduce_Array_Size_to_The_Half
+{
+    internal class Reduce_Array_Size_to_The_Half
+    {
+        public int MinSetSize(int[] arr)
+        {
+            var freq = arr
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            var counts = 0;
+            return freq
+                .Keys
+                .OrderByDescending(x => freq[x])
+                .TakeWhile(x => {
+                    counts += freq[x];
+                    return counts < arr.Length / 2;
+                })
+                .Count() + 1;
+        }
+    }
+}
