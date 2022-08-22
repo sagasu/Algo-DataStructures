8825764a12c88af4358761d93779a6a9ae76181e
Mateusz Kopij <sagasu@gmail.com>
2022-08-22 08:27:18 +0100

    Power of Four

--- AlgoTest/LeetCode/PowerOfFour/PowerOfFour2.cs
+++ AlgoTest/LeetCode/PowerOfFour/PowerOfFour2.cs
@@ -6,9 +6,7 @@ namespace AlgoTest.LeetCode.PowerOfFour
 {
     class PowerOfFour2
     {
-        public bool IsPowerOfFour(int num)
-        {
-            return num > 0 && (Math.Log10(num) / Math.Log10(4)) % 1 == 0;
-        }
+        public bool IsPowerOfFour(int num) => num > 0 && (Math.Log10(num) / Math.Log10(4)) % 1 == 0;
+        
     }
 }
