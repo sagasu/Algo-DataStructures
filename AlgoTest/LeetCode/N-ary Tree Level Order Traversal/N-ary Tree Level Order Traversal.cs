85642519aa8af4c8b928de5284e3e6f1c4c2205d
Mateusz Kopij <sagasu@gmail.com>
2022-09-05 20:49:10 +0100

    N-ary Tree Level Order Traversal

---
+++ AlgoTest/LeetCode/N-ary Tree Level Order Traversal/N-ary Tree Level Order Traversal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AlgoTest.LeetCode.N_ary_Tree_Preorder_Traversal;
+
+namespace AlgoTest.LeetCode.N_ary_Tree_Level_Order_Traversal
+{
+    internal class N_ary_Tree_Level_Order_Traversal
+    {
+        public IList<IList<int>> LevelOrder(Node root) {
+            if(root == null) return new List<IList<int>>();
+
+            var queue = new Queue<Node>();
+            var res = new List<IList<int>>();
+            queue.Enqueue(root);
+
+            while(queue.Any()){
+                var n = queue.Count;
+                var temp = new List<int>();
+                for(var i = 0; i< n; i++){
+                    var cur = queue.Dequeue();
+                    temp.Add(cur.val);
+                    foreach(var item in cur.children)
+                        if(item != null) queue.Enqueue(item);
+                }
+                res.Add(temp);
+            }
+
+            return res;
+        }
+    }
+}
