d967e164ad373397fcfd64144f5fcee60ff8fe8e
Mateusz Kopij <sagasu@gmail.com>
2022-09-05 18:50:40 +0100

    extra spaces

--- AlgoTest/LeetCode/Backspace String Compare/Backspace String Compare.cs
+++ AlgoTest/LeetCode/Backspace String Compare/Backspace String Compare.cs
@@ -12,7 +12,7 @@ namespace AlgoTest.LeetCode.Backspace_String_Compare
 		public bool BackspaceCompare(string s, string t)
 		{
 			var sStack = new Stack();
-			var tStack = new Stack();
+			var tStack = new Stack();  
 			foreach (var st in s)
 			{
 				if (st.Equals('#') && sStack.Count > 0) sStack.Pop();
