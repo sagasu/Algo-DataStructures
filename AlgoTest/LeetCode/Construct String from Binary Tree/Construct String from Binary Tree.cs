899ba7d2a90dd2a7d0f135da5ee6b591343c8844
Mateusz Kopij <sagasu@gmail.com>
2022-09-07 18:40:39 +0100

    Construct_String_from_Binary_Tree

---
+++ AlgoTest/LeetCode/Construct String from Binary Tree/Construct String from Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Construct_String_from_Binary_Tree
+{
+    internal class Construct_String_from_Binary_Tree
+    {
+        public string Tree2str(TreeNode root)
+        {
+            var str = "" + root.val;
+            if (root.left != null)
+                str += "(" + Tree2str(root.left) + ")";
+            else if (root.right != null)
+                str += "()";
+            if (root.right != null)
+                str += "(" + Tree2str(root.right) + ")";
+            return str;
+        }
+    }
+}
