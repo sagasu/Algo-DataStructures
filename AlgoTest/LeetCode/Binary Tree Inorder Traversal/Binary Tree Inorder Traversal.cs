d37905adfa62775f67bcd1d7eef45ccb67f3f69e
Mateusz Kopij <sagasu@gmail.com>
2022-09-08 18:08:55 +0100

    Binary_Tree_Inorder_Traversal

---
+++ AlgoTest/LeetCode/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Binary_Tree_Inorder_Traversal
+{
+    internal class Binary_Tree_Inorder_Traversal
+    {
+
+        List<int> orderPrint = new List<int>();
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return orderPrint;
+
+            if (root.left != null)
+                InorderTraversal(root.left);
+
+            orderPrint.Add(root.val);
+
+            if (root.right != null)
+                InorderTraversal(root.right);
+
+            return orderPrint;
+        }
+    }
+
+}
