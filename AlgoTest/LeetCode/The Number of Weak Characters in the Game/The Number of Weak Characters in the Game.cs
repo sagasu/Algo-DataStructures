89db8a4663e23d1bee3db7c63e56b14741b5e13c
Mateusz Kopij <sagasu@gmail.com>
2022-09-09 19:37:59 +0100

    Construct_String_from_Binary_Tree

---
+++ AlgoTest/LeetCode/The Number of Weak Characters in the Game/The Number of Weak Characters in the Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.The_Number_of_Weak_Characters_in_the_Game
+{
+    internal class The_Number_of_Weak_Characters_in_the_Game
+    {
+        HashSet<int> weakChars = new();
+        public int NumberOfWeakCharacters(int[][] properties)
+        {
+            var props = properties.OrderByDescending(p => p[0]).ToList();
+            var maxDefense = 0;
+            var previousOffense = props[0][0];
+            var previousMaxDefense = props[0][1];
+            var weak = 0;
+            for (var i = 0; i < props.Count; i++)
+            {
+                if (previousOffense != props[i][0])
+                {
+                    if (maxDefense < previousMaxDefense)
+                        maxDefense = previousMaxDefense;
+                    previousMaxDefense = props[i][1];
+                    previousOffense = props[i][0];
+                }
+                else if (previousMaxDefense < props[i][1])
+                    previousMaxDefense = props[i][1];
+
+                if (maxDefense > props[i][1])
+                    weak++;
+            }
+            return weak;
+        }
+    }
+}
