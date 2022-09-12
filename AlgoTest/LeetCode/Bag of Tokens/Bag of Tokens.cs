c36e5ba306c87be02f562350b62f50e8696525b2
Mateusz Kopij <sagasu@gmail.com>
2022-09-12 19:01:35 +0100

    Bag of Tokens

---
+++ AlgoTest/LeetCode/Bag of Tokens/Bag of Tokens.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Bag_of_Tokens
+{
+    internal class Bag_of_Tokens
+    {
+        public int BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+            if (tokens.Length == 0 || power < tokens[0]) return 0;
+            var points = 0;
+            var ans = 0;
+            int left = 0, right = tokens.Length - 1;
+            while (left <= right)
+            {
+                if (power >= tokens[left])
+                {
+                    points++;
+                    power -= tokens[left];
+                    left++;
+                }
+                else if (right - left > 1)
+                {
+                    power += tokens[right];
+                    right--;
+                    points--;
+                }
+                else break;
+            }
+            return points;
+        }
+    }
+}
