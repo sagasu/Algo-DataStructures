8ae1dc0a52556f8d6a85e94013142fc27548d183
Mateusz Kopij <sagasu@gmail.com>
2022-10-12 13:01:07 +0100

    Largest Perimeter Triangle

---
+++ AlgoTest/LeetCode/Largest Perimeter Triangle/Largest Perimeter Triangle.cs
@@ -0,0 +1,9 @@
+public class LargestPerimeterTriangle {
+    public int LargestPerimeter(int[] nums) {
+        Array.Sort(nums);
+        for (var q=nums.Length-1; q>1; q--) {
+            if (nums[q] < nums[q-1] + nums[q-2]) return nums[q] + nums[q-1] + nums[q-2];
+        }
+        return 0;
+    }
+}
