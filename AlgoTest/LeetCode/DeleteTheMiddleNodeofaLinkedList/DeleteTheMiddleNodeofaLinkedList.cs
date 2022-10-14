d3502fdceb547ea69d6f17b6d87ab3807e42b503
Mateusz Kopij <sagasu@gmail.com>
2022-10-14 17:18:37 +0100

    Delete the Middle Node of a Linked List

---
+++ AlgoTest/LeetCode/DeleteTheMiddleNodeofaLinkedList/DeleteTheMiddleNodeofaLinkedList.cs
@@ -0,0 +1,24 @@
+public class DeleteTheMiddleNodeofaLinkedList
+{
+    public ListNode DeleteMiddle(ListNode head) 
+    {
+        var c=0;
+        var t = head;
+        while(t!=null)
+        {
+            t=t.next;
+            c++;
+        }
+        if(c==1) return null;
+        
+        c=c/2;
+        var i=0;
+        t=head;
+        
+        while(i++<c-1) t = t.next; 
+        
+        if(t.next!=null) t.next = t.next.next; 
+        
+        return head;
+    }
+}
