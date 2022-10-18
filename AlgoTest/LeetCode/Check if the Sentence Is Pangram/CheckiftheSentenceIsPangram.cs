d112048094924fb9fac5fa8d97ad2b039d3d83c6
Mateusz Kopij <sagasu@gmail.com>
2022-10-18 09:40:48 +0100

    Check if the Sentence Is Pangram

---
+++ AlgoTest/LeetCode/Check if the Sentence Is Pangram/CheckiftheSentenceIsPangram.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool CheckIfPangram(string sentence)
+    {
+                HashSet<char> visited = new HashSet<char>();
+                sentence = sentence.ToLower();
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    visited.Add(sentence[i]);
+                    if (visited.Count == 26) return true;
+                }
+                return false;
+    }
+}
