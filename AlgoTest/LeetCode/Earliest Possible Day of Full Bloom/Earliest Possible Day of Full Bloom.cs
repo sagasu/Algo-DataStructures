077f6121ebb6eaec4c2047762e986719e0acdb79
Mateusz Kopij <sagasu@gmail.com>
2022-10-30 17:23:55 +0000

    EarliestFullBloom

--- AlgoTest/LeetCode/Earliest Possible Day of Full Bloom/Earliest Possible Day of Full Bloom.cs
+++ AlgoTest/LeetCode/Earliest Possible Day of Full Bloom/Earliest Possible Day of Full Bloom.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public int EarliestFullBloom(int[] plantTime, int[] growTime) {
-        int n = growTime.Length;
+        var n = growTime.Length;
         int[][] days = new int[n][];
         
-        for(int i = 0; i < n; i++)
+        for(var i = 0; i < n; i++)
         {
             days[i] = new int[2] { plantTime[i], growTime[i] };
         }
@@ -12,7 +12,7 @@ public class Solution {
         
         int max = 0, plantSum = 0;
         
-        for(int i = 0; i < n; i++)
+        for(var i = 0; i < n; i++)
         {
             int plant = days[i][0], grow = days[i][1];
             plantSum += plant;
