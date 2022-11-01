926c0f3d600ec9c34d6441a6f7401ba9e88d822d
Mateusz Kopij <sagasu@gmail.com>
2022-11-01 22:47:40 +0000

    Where Will the Ball Fall

---
+++ AlgoTest/LeetCode/Where Will the Ball Fall/Where Will the Ball Fall.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int[] FindBall(int[][] grid)
+    {
+
+        int row = grid.Length;
+        int col = grid[0].Length;
+        bool ballstuck = false;
+        List<int> res = new List<int>();
+
+        for (int i = 0; i < col; i++)
+        {
+            int k = i;
+            for (int j = 0; j < row; j++)
+            {
+                // check right
+                if (k + 1 <= col - 1 && grid[j][k] == 1 && grid[j][k + 1] == 1)
+                { ballstuck = false; k++; continue; }
+                // check left
+                else if (k - 1 >= 0 && grid[j][k] == -1 && grid[j][k - 1] == -1)
+                { ballstuck = false; k--; continue; }
+                else
+                { ballstuck = true; res.Add(-1); break; }
+            }
+            if (!ballstuck)
+                res.Add(k);
+            ballstuck = false;
+        }
+        return res.ToArray();
+    }
+}
