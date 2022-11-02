5779832f142b8adb3ade3c9885511980e88ebe2a
Mateusz Kopij <sagasu@gmail.com>
2022-11-02 19:01:19 +0000

    Minimum Genetic Mutation.

---
+++ AlgoTest/LeetCode/Minimum Genetic Mutation/Minimum Genetic Mutation.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int MinMutation(string start, string end, string[] bank) {
+        var queue = new Queue<string>();
+        var visited = new HashSet<string>();
+        
+        queue.Enqueue(start);
+        var mutationsCount = 0;
+        
+        while(queue.Count > 0)
+        {
+            var count = queue.Count;
+            for(var i = 0; i < count; i++)
+            {
+                var gene = queue.Dequeue();
+                if(gene == end)
+                {
+                    return mutationsCount;
+                }
+
+                foreach(var mutation in bank.Where(x => !visited.Contains(x)))
+                {
+                    var diff = CountDiff(gene, mutation);
+                    if(diff != 1)
+                    {
+                        continue;
+                    }
+                    visited.Add(mutation);
+                    queue.Enqueue(mutation);
+                }
+            }
+            mutationsCount++;
+        }
+        return -1;
+        
+        int CountDiff(string first, string second) => first.Zip(second, (x, y) => x != y ? 1 : 0).Sum();
+    }
+}
