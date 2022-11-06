93afcad3ff44f84d556da221d6a3030dc0c27b5f
Mateusz Kopij <sagasu@gmail.com>
2022-11-06 18:06:06 +0000

    Reverse Vowels of a String

---
+++ AlgoTest/LeetCode/Reverse Vowels of a String/Reverse Vowels of a String.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string ReverseVowels(string s) {
+            var ss = s.ToCharArray();
+
+            int a = 0, b = s.Length - 1;
+
+            while (a < b)
+            {
+                while ("aeiouAEIOU".IndexOf(ss[a]) == -1 && a <= b - 1) a++;
+
+                while ("aeiouAEIOU".IndexOf(ss[b]) == -1 && b >= a + 1) b--;
+
+                if (a == b) return new string(ss);
+
+                var h = ss[a];
+
+                ss[a++] = ss[b];
+
+                ss[b--] = h;
+            }
+
+            return new string(ss);
+    }
+}
