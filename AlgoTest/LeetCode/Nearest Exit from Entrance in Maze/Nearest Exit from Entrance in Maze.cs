8146324c69209c4f50b953534f35cb209936d7a6
Mateusz Kopij <sagasu@gmail.com>
2022-11-21 13:19:29 +0000

    Nearest Exit from Entrance in Maze

---
+++ AlgoTest/LeetCode/Nearest Exit from Entrance in Maze/Nearest Exit from Entrance in Maze.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int NearestExit(char[][] maze, int[] entrance) {
+        int ROWS = maze.Length;
+        int COLS = maze[0].Length;
+        Queue<(int,int,int)> mQue = new();
+        mQue.Enqueue((entrance[0],entrance[1],0));
+                
+        while(mQue.Count > 0){
+            (var r, var c, var s) = mQue.Dequeue();
+            if(r < 0 || r >= ROWS || c < 0 || c >= COLS || maze[r][c] == '+'){
+                continue;
+            }
+            if((r != entrance[0] || c != entrance[1]) && (r == 0 || r == ROWS - 1 || c == 0 || c == COLS - 1)){
+                return s;
+            }
+            //Leave visited breadcrumb
+            maze[r][c] = '+';
+
+            (int,int)[] nextSteps = new[]{(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)};
+            foreach((var nextr, var nextc) in nextSteps){
+                mQue.Enqueue((nextr, nextc, s + 1));
+            }
+        }
+
+        return -1;
+    }
+}
