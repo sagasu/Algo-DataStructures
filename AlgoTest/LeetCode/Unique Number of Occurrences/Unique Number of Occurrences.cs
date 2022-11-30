fe35d503415e022c5468c5c011ef3607911c79da
Mateusz Kopij <sagasu@gmail.com>
2022-11-30 21:07:32 +0000

    Unique Number of Occurrences

---
+++ AlgoTest/LeetCode/Unique Number of Occurrences/Unique Number of Occurrences.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		if (dic.ContainsKey(arr[i]))
+			dic[arr[i]]++;
+		else
+			dic.Add(arr[i], 1);
+	}
+
+	HashSet<int> set = new HashSet<int>();
+
+	foreach (KeyValuePair<int, int> pair in dic)
+	{
+		if (set.Contains(pair.Value))
+			return false;
+		else
+			set.Add(pair.Value);
+	}
+
+	return true;
+    }
+}
