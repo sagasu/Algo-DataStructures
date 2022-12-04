e5b1f79fec7f13d3463a53e9cf4bfef6579ee78a
Mateusz Kopij <sagasu@gmail.com>
2022-12-04 19:15:25 +0000

    MinimumAverageDifference

---
+++ AlgoTest/LeetCode/Minimum Average Difference/Minimum Average Difference.cs
@@ -0,0 +1,33 @@
+public class MinimumAverageDifferenceSolution {
+    public int MinimumAverageDifference(int[] nums) {
+                if (nums.Length == 1) return 0;
+
+        long leftSum = 0;
+        long rightSum = 0;
+
+        foreach (var num in nums)
+        {
+            rightSum += num;
+        }
+
+        long minDiff = long.MaxValue;
+        int result = -1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+
+            var diff = Math.Abs(leftSum / (i + 1) - (rightSum / Math.Max(nums.Length - i - 1, 1)));
+
+            if (diff < minDiff)
+            {
+                minDiff = diff;
+                result = i;
+            }
+
+        }
+
+        return result;
+    }
+}
