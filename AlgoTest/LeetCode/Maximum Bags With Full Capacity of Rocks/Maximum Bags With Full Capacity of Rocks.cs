aea1a0ea26dc9744064db3fb0d22dc0aa31a6e8c
Mateusz Kopij <sagasu@gmail.com>
2022-12-27 19:32:15 +0000

    Maximum Bags With Full Capacity of Rocks

---
+++ AlgoTest/LeetCode/Maximum Bags With Full Capacity of Rocks/Maximum Bags With Full Capacity of Rocks.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int MaximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        return capacity
+            .Zip(rocks, (cap, roc) => cap - roc )
+            .OrderBy(x => x)
+            .TakeWhile(x => (additionalRocks -= x) >= 0)
+            .Count();
+    }
+}
