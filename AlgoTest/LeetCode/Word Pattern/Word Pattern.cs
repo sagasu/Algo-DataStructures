e71fb42fbc17d7641090ec7fd1cf6cff554ddb6c
Mateusz Kopij <sagasu@gmail.com>
2023-01-01 22:31:54 +0000

    Word Pattern

---
+++ AlgoTest/LeetCode/Word Pattern/Word Pattern.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+        
+        string[] arr = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        if(pattern.Length != arr.Length)
+            return false;
+        Dictionary<char,string> dic = new Dictionary<char,string>();
+        for(int i = 0; i < pattern.Length; i++)
+        {
+            if(dic.ContainsKey(pattern[i]))
+            {
+                if(dic[pattern[i]] != arr[i])
+                    return false;
+            }
+            else
+            {
+                if(dic.ContainsValue(arr[i]))
+                    return false;
+                else
+                    dic.Add(pattern[i],arr[i]);
+            }
+        }
+        return true;
+    }
+}
