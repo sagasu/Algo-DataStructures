619db54c42610c54bdd102c58cc8bd1369c5898d
Mateusz Kopij <sagasu@gmail.com>
2023-01-16 19:17:05 +0000

    Insert Interval

---
+++ AlgoTest/LeetCode/Insert Interval/Insert Interval.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        
+        if(intervals == null || intervals.Length == 0)
+            return new int[][]{newInterval};
+        
+        List<int[]> list = new List<int[]>();      
+        int i = 0;
+        
+        while(i < intervals.Length && intervals[i][1] < newInterval[0])
+        {
+            list.Add(intervals[i]); 
+            i++;
+        }   
+        
+        while(i < intervals.Length && intervals[i][0] <= newInterval[1])
+        {
+            newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        list.Add(newInterval);
+        
+        while(i < intervals.Length)
+        {
+            list.Add(intervals[i]);
+            i++;
+        }
+        
+        return list.ToArray();
+    }
+}
