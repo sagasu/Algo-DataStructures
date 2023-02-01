fb24440ebb8b1195fe0b59b354d834a2ae0e298a
Mateusz Kopij <sagasu@gmail.com>
2023-02-01 21:49:49 +0000

    Greatest Common Divisor of Strings

---
+++ AlgoTest/LeetCode/Greatest Common Divisor of Strings/Greatest Common Divisor of Strings.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public string GcdOfStrings(string str1, string str2)  => 
+        str1 + str2 == str2 + str1 ? str1.Substring(0, Gcd(str1.Length, str2.Length)) : string.Empty;
+
+    private static int Gcd(int x, int y) => y == 0 ? x : Gcd(y, x % y);
+}
