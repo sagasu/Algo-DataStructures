ea3a6197e09c103bed0f02eb96ba119c089430c7
Mateusz Kopij <sagasu@gmail.com>
2023-02-02 23:21:17 +0000

    improving

--- AlgoTest/LeetCode/Best Team With No Conflicts/Best Team With No Conflicts.cs
+++ AlgoTest/LeetCode/Best Team With No Conflicts/Best Team With No Conflicts.cs
@@ -1,17 +1,17 @@
 public class Solution {
     List<Player> teamList=new List<Player>();
     public int BestTeamScore(int[] scores, int[] ages) {
-        int length=ages.Length;
-        int result=0;
-        int [] dp=new int[length];
+        var length=ages.Length;
+        var result=0;
+        var dp=new int[length];
         for(int i=0;i<length;i++){
             var item=new Player{score=scores[i],age=ages[i]};
             teamList.Add(item);
         }
         teamList=teamList.OrderBy(team=>team.age).ThenBy(team=>team.score).ToList();
-        for(int i=0;i<length;i++){
+        for(var i=0;i<length;i++){
             dp[i] = teamList[i].score;
-            for(int j = 0; j < i; j++) {
+            for(var j = 0; j < i; j++) {
                 if(teamList[j].age==teamList[i].age){
                     dp[i]=Math.Max(dp[i],dp[j]+teamList[i].score);
                 }
