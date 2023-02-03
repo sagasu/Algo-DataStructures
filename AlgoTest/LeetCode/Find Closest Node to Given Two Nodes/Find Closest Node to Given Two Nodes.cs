1935f5c500f476a4906ab1cb492d7968e49ab81c
Mateusz Kopij <sagasu@gmail.com>
2023-02-03 19:28:37 +0000

    improvements

--- AlgoTest/LeetCode/Find Closest Node to Given Two Nodes/Find Closest Node to Given Two Nodes.cs
+++ AlgoTest/LeetCode/Find Closest Node to Given Two Nodes/Find Closest Node to Given Two Nodes.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 public class ClosestMeetingNodeSolution
 {
     public int ClosestMeetingNode(int[] edges, int node1, int node2) {
-        Dictionary<int, int> dict1 = new Dictionary<int, int>();
-        Dictionary<int, int> dict2 = new Dictionary<int, int>();
+        var dict1 = new Dictionary<int, int>();
+        var dict2 = new Dictionary<int, int>();
         int cur = node1, counter = 0, min = int.MaxValue, result = int.MaxValue;
 
         while (cur != -1) {
-            if (dict1.ContainsKey(cur))
-                break;
+            if (dict1.ContainsKey(cur)) break;
             
             dict1.Add(cur, counter++);
             cur = edges[cur];
@@ -20,8 +19,7 @@ public class ClosestMeetingNodeSolution
         counter = 0;
 
         while (cur != -1) {
-            if (dict2.ContainsKey(cur))
-                break;
+            if (dict2.ContainsKey(cur)) break;
 
             dict2.Add(cur, counter++);
             cur = edges[cur];
@@ -29,7 +27,7 @@ public class ClosestMeetingNodeSolution
 
         foreach (int key in dict1.Keys) {
             if (dict2.ContainsKey(key)) {
-                int tempMin = Math.Max(dict1[key], dict2[key]);
+                var tempMin = Math.Max(dict1[key], dict2[key]);
                 
                 if (tempMin <= min) {
                     result = (tempMin < min) ? key : Math.Min(result, key);
