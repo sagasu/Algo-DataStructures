87be1b3b51e4a96e7cf5fee6e4aa97fd6e3a6fa3
Mateusz Kopij <sagasu@gmail.com>
2023-02-06 16:28:11 +0000

    Shuffle the Array

---
+++ AlgoTest/LeetCode/Shuffle the Array/Shuffle the Array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        var ret = new int[nums.Length];
+        for(var i = 0; i < n;i++){
+            ret[i*2] = nums[i];
+            ret[i*2+1] = nums[n+i];
+        }
+        return ret;
+    }
+}
