fe59867fb58126cd3cfe0ef3ca06fc1953b0a8e3
Mateusz Kopij <sagasu@gmail.com>
2023-02-19 16:06:48 +0000

    Count Odd Numbers in an Interval Range

---
+++ AlgoTest/LeetCode/Count Odd Numbers in an Interval Range/Count Odd Numbers in an Interval Range.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Count_Odd_Numbers_in_an_Interval_Range
+{
+    internal class Count_Odd_Numbers_in_an_Interval_Range
+    {
+        public int CountOdds(int low, int high)
+        {
+            var count = 0;
+            for (var i = low; i <= high; i++)
+                if (i % 2 != 0) count += 1;
+            
+            return count;
+        }
+    }
+}
