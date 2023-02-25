7e3a7139c480b9a0c5f10a558b3ab90f53eb09cb
Mateusz Kopij <sagasu@gmail.com>
2023-02-25 19:36:37 +0000

    Best Time to Buy and Sell Stock

---
+++ AlgoTest/LeetCode/BestTimeToBuyStock/Best Time to Buy and Sell Stock TimelimitExceeded.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.BestTimeToBuyStock
+{
+    //brute force solution, time limit exceeded 
+    public class Best_Time_to_Buy_and_Sell_Stock_timelimit
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var max = 0;
+            for (var i = 0; i < prices.Length; i++)
+            for (var j = i + 1; j < prices.Length; j++)
+                max = Math.Max(max, prices[j] - prices[i]);
+
+            return max;
+        }
+    }
+}
---
+++ AlgoTest/LeetCode/BestTimeToBuyStock/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.BestTimeToBuyStock
+{
+    public class Best_Time_to_Buy_and_Sell_Stock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var maxProfit = 0;
+            var bestSell = 0;
+            // We start from the end so we will know the best selling price (the max one) and we move to beginning comparing the profit. 
+            for (var i = prices.Length-1; i >= 0; i--)
+            {
+                maxProfit = Math.Max(maxProfit, bestSell - prices[i]);
+                bestSell = Math.Max(bestSell, prices[i]);
+            }
+
+
+            return maxProfit;
+        }
+    }
+}
