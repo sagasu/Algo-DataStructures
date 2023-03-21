68d47e89753cad220fceafd6aba6feb5aefcc244
Mateusz Kopij <sagasu@gmail.com>
2023-03-21 20:29:17 +0000

    Number of Zero-Filled Subarrays

---
+++ AlgoTest/LeetCode/Number of Zero-Filled Subarrays/Number of Zero-Filled Subarrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Number_of_Zero_Filled_Subarrays
+{
+    internal class Number_of_Zero_Filled_Subarrays
+    {
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            long res = 0;
+            long curr = 0;
+
+            foreach (var n in nums)
+                if (n == 0) res += curr + 1;
+                else curr = 0;
+
+            return res;
+        }
+    }
+}
