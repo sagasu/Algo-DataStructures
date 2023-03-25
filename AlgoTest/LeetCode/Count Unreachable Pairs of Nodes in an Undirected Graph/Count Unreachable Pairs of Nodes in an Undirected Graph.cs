56697a62a469d8ca009819a04b9f4f2fcaa7e8d9
Mateusz Kopij <sagasu@gmail.com>
2023-03-25 19:39:26 +0000

    Count Unreachable Pairs of Nodes in an Undirected Graph

---
+++ AlgoTest/LeetCode/Count Unreachable Pairs of Nodes in an Undirected Graph/Count Unreachable Pairs of Nodes in an Undirected Graph.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public long CountPairs(int n, int[][] edges) {
+        var dsu = new UnionFindSet(n);
+        foreach (var edge in edges) dsu.Union(edge[0], edge[1]);
+        
+        var componentSize = new Dictionary<int, int>();
+        for (var i= 0; i<n; i++) {
+            var parent = dsu.Find(i);
+            componentSize[parent] = componentSize.GetValueOrDefault(parent, 0) + 1;
+        }
+        
+        long numberOfPaths = 0;
+        long remainingNodes = n;
+
+        foreach (int size in componentSize.Values) {
+            numberOfPaths += size * (remainingNodes - size);
+            remainingNodes -= size;
+        }
+
+        return numberOfPaths;        
+    }
+
+    public class UnionFindSet {
+        private int[] parent;
+
+        public UnionFindSet(int size) {
+            parent = new int[size];
+            for (var i = 0; i < parent.Length; ++i) 
+                parent[i] = i;
+        }
+
+        public void Union(int x, int y) => parent[Find(x)] = parent[Find(y)];
+        
+        public int Find(int x) {
+            if (parent[x] != x) {
+                parent[x] = Find(parent[x]);
+            }
+            return parent[x];
+        }
+    }    
+}
