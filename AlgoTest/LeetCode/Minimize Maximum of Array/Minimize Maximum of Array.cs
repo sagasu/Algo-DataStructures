4092e928132d6741912d632a6cd92906b9e6c369
Mateusz Kopij <sagasu@gmail.com>
2023-04-05 13:50:15 +0100

    Minimize Maximum of Array

---
+++ AlgoTest/LeetCode/Minimize Maximum of Array/Minimize Maximum of Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Minimize_Maximum_of_Array
+{
+    public class Minimize_Maximum_of_Array
+    {
+        public int MinimizeArrayValue(int[] nums)
+        {
+            long answer = 0, prefixSum = 0;
+            
+            for (var i = 0; i < nums.Length; i++)
+            {
+                prefixSum += nums[i];
+                answer = Math.Max(answer, (prefixSum + i) / (i + 1));
+            }
+
+            return (int)answer;
+        }
+    }
+}
