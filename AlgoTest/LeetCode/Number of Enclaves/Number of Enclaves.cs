5a320b0787e12b038d7178abdd9363b76a4423fa
Mateusz Kopij <sagasu@gmail.com>
2023-04-07 21:08:07 +0100

    Number of Enclaves

---
+++ AlgoTest/LeetCode/Number of Enclaves/Number of Enclaves.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Number_of_Enclaves
+{
+    public class Number_of_Enclaves
+    {
+        public int NumEnclaves(int[][] grid)
+        {
+            var ni = grid.Length-1;
+            var nj = grid[0].Length-1;
+            var nrOfEnclaves = 0;
+
+            bool IsEnclave(int i, int j)
+            {
+
+            }
+
+            for(var i =1;i<ni;i++)
+                for(var j = 1;j<nj;j++)
+                    if (grid[i][j] == 1 && IsEnclave(i,j)) nrOfEnclaves++;
+
+            return nrOfEnclaves;
+        }
+    }
+}
