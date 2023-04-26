7dae26faf97a9fe877487a2cf55c9005aea64938
Mateusz Kopij <sagasu@gmail.com>
2023-04-26 13:20:27 +0100

    Add_Digits

---
+++ AlgoTest/LeetCode/Add Digits/Add Digits.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Add_Digits
+{
+    [TestClass]
+    public class Add_Digits
+    {
+        [TestMethod]
+        public void Test() => Assert.AreEqual(2, AddDigits(38));
+        
+        [TestMethod]
+        public void Test2() => Assert.AreEqual(0, AddDigits(0));
+        
+        [TestMethod]
+        public void Test3() => Assert.AreEqual(4, AddDigits(13));
+        
+        [TestMethod]
+        public void Test4() => Assert.AreEqual(2, AddDigits(2));
+
+        public int AddDigits(int num)
+        {
+            var sum = num;
+            while (sum >= 10)
+            {
+                num = sum;
+                sum = 0;
+                foreach (var n in num.ToString())
+                {
+                    sum += int.Parse(n.ToString());
+                }
+            }
+            return sum;
+        }
+    }
+}
