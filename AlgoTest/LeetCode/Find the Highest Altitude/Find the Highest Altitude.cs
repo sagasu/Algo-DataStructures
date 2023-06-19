da8f22f1118c5433a020568b7acc1d73d0809614
Mateusz Kopij <sagasu@gmail.com>
2023-06-19 11:28:35 +0100

    Find the Highest Altitude

---
+++ AlgoTest/LeetCode/Find the Highest Altitude/Find the Highest Altitude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Find_the_Highest_Altitude
+{
+    [TestClass]
+    public class Find_the_Highest_Altitude
+    {
+        [TestMethod]
+        public void Test() => Assert.AreEqual(1, LargestAltitude(new[] { -5, 1, 5, 0, -7 }));
+        
+        [TestMethod]
+        public void Test1() => Assert.AreEqual(0, LargestAltitude(new[] { -4, -3, -2, -1, 4, 3, 2 }));
+
+        public int LargestAltitude(int[] gain)
+        {
+            var max = 0;
+            var curr = 0;
+            foreach (var g in gain)
+            {
+                curr += g;
+                max = Math.Max(curr, max);
+            }
+            return max;
+        }
+    }
+}
