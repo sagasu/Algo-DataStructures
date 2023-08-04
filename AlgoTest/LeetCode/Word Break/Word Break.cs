d8e80166d2493434eee6a9c4617278c932d51c6d
Mateusz Kopij <sagasu@gmail.com>
2023-08-04 04:12:33 +0100

    Word Break

---
+++ AlgoTest/LeetCode/Word Break/Word Break.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTest.LeetCode.Word_Break
+{
+    internal class Word_Break
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            var n = s.Length;
+            var dp = new bool[n + 1];
+
+            dp[0] = true;
+            for (var i = 1; i <= n; i++)
+            {
+                var sIndex = i - 1;
+                foreach (var word in wordDict)
+                {
+                    var wordLen = word.Length;
+                    var lastChar = word.Last();
+
+                    if (lastChar == s[sIndex] && sIndex + 1 >= wordLen)
+                        if (s.Substring(sIndex - wordLen + 1, wordLen) == word)
+                            dp[i] |= dp[i - wordLen];
+                        
+                    
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
