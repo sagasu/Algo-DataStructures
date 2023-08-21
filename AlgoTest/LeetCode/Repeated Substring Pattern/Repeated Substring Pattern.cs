3db22c44f5800a2c90abefdf8352c28b28e7f9da
Mateusz Kopij <sagasu@gmail.com>
2023-08-21 21:48:49 +0100

    Repeated Substring Pattern

---
+++ AlgoTest/LeetCode/Repeated Substring Pattern/Repeated Substring Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Repeated_Substring_Pattern;
+
+public class Repeated_Substring_Pattern
+{
+    public bool RepeatedSubstringPattern(string s) {
+        return Enumerable
+            .Range(1, s.Length / 2)
+            .Any(l => s.Length % l == 0 &&
+                      Enumerable
+                          .Range(0, s.Length)
+                          .All(i => s[i] == s[i % l])
+            );
+    }
+}
