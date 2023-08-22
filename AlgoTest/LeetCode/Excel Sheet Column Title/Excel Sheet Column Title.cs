e8b460d7445c2320087271467f95ffc8eb0b8f5e
Mateusz Kopij <sagasu@gmail.com>
2023-08-22 21:36:39 +0100

    Excel Sheet Column Title

---
+++ AlgoTest/LeetCode/Excel Sheet Column Title/Excel Sheet Column Title.cs
@@ -0,0 +1,7 @@
+﻿namespace AlgoTest.LeetCode.Excel_Sheet_Column_Title;
+
+public class Excel_Sheet_Column_Title
+{
+    public string ConvertToTitle(int c) =>
+        (--c < 26 ? "" : ConvertToTitle(c / 26)) + (char)(c % 26 + 'A');
+}
