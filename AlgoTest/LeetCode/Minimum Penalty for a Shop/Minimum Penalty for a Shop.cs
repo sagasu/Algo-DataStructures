dc13098043171ac53e60ed80e4010aa9702c0ec0
Mateusz Kopij <sagasu@gmail.com>
2023-08-29 20:57:55 +0100

    Minimum Penalty for a Shop

---
+++ AlgoTest/LeetCode/Minimum Penalty for a Shop/Minimum Penalty for a Shop.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Penalty_for_a_Shop;
+
+public class Minimum_Penalty_for_a_Shop
+{
+    public int BestClosingTime(string customers) {
+        var current = customers.Count(c => c == 'N');
+        
+        var best = current;
+        var result = customers.Length;
+
+        for (var i = customers.Length - 1; i >= 0; --i) {
+            current += customers[i] == 'Y' ? 1 : -1; 
+
+            if (current <= best) {
+                current = best;
+
+                result = i;
+            }        
+        }
+
+        return result;
+    }
+}
