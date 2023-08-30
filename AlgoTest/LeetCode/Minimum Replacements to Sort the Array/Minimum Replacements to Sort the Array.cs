acefad22ee8d1d302cd61cdf1850e7a4a2751d7c
Mateusz Kopij <sagasu@gmail.com>
2023-08-30 21:18:57 +0100

    Minimum Replacements to Sort the Array

---
+++ AlgoTest/LeetCode/Minimum Replacements to Sort the Array/Minimum Replacements to Sort the Array.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Replacements_to_Sort_the_Array;
+
+public class Minimum_Replacements_to_Sort_the_Array
+{
+    public long MinimumReplacement(int[] nums) {
+        long result = 0;
+        
+        for (var i = nums.Length - 2; i >= 0; ) 
+            if (nums[i] <= nums[i + 1]) 
+                i -= 1;
+            else {
+                var breakCount = ((nums[i] - 1) / nums[i + 1]) + 1;
+            
+                nums[i] /= breakCount;
+            
+                result += breakCount - 1;
+            }
+           
+        return result;
+    }
+}
