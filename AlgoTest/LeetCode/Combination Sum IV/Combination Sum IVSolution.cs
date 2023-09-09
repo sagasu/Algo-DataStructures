8208422c34b7d0e820022286b122d8d2a849a0fb
Mateusz Kopij <sagasu@gmail.com>
2023-09-09 07:58:39 +0100

    Combination Sum IV

---
+++ AlgoTest/LeetCode/Combination Sum IV/Combination Sum IVSolution.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgoTest.LeetCode.Combination_Sum_IV;
+
+public class Combination_Sum_IVSolution
+{
+    public int CombinationSum4(int[] nums, int target) {
+        var comb = new int[target + 1];
+        comb[0] = 1;
+        
+        for(var i = 1; i <= target; i++)
+            foreach (var num in nums)
+                if(i - num >= 0)
+                    comb[i] += comb[i - num];
+
+        return comb[target];
+    }
+}
