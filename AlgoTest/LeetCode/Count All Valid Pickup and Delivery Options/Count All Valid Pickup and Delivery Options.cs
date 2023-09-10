48e1d2375bcc41ecb29b6177d0211e8db3ceb576
Mateusz Kopij <sagasu@gmail.com>
2023-09-10 21:16:00 +0100

    Count All Valid Pickup and Delivery Options

---
+++ AlgoTest/LeetCode/Count All Valid Pickup and Delivery Options/Count All Valid Pickup and Delivery Options.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgoTest.LeetCode.Count_All_Valid_Pickup_and_Delivery_Options;
+
+public class Count_All_Valid_Pickup_and_Delivery_Options
+{
+    public int CountOrders(int n)
+    {
+        const int modulo = 1_000_000_007;
+        var res = 1L;
+
+        for (var i = 1; i <= n; ++i)
+            res = res * i * (2 * i - 1) % modulo;
+        
+        return (int)res;
+    }
+}
