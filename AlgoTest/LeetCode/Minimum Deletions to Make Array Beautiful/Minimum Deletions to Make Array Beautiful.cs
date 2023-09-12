9ca7c094f801b3bf202dfc819419e3ceeea0d83e
Mateusz Kopij <sagasu@gmail.com>
2023-09-12 21:08:52 +0100

    Minimum Deletions to Make Array Beautiful

---
+++ AlgoTest/LeetCode/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Deletions_to_Make_Array_Beautiful;
+
+public class Minimum_Deletions_to_Make_Array_Beautiful
+{
+    public int MinDeletion(int[] nums) {
+        var count = 0;
+        var i = 0;
+
+        while(i<nums.Length-1){
+            if(nums[i] == nums[i+1]){
+                i++;
+                count++;
+            }
+            else i = i+2;
+            
+        }
+
+        return (nums.Length-count)%2 == 0 ? count : count+1;
+    }
+}
