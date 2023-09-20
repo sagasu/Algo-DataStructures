ac65a938e49be16e971afae8f1ac0a330b467d6e
Mateusz Kopij <sagasu@gmail.com>
2023-09-20 13:31:43 +0100

    Removing Minimum Number of Magic Beans

---
+++ AlgoTest/LeetCode/Removing Minimum Number of Magic Beans/Removing Minimum Number of Magic Beans.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Removing_Minimum_Number_of_Magic_Beans;
+
+public class Removing_Minimum_Number_of_Magic_Beans
+{
+    public long MinimumRemoval(int[] beans) 
+        => beans.Sum(x => (long) x) - beans.OrderBy(x => x).Select((bean, i) => ((long) bean) * (beans.Length - i)).Max();
+}
