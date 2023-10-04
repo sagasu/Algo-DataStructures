00d069d64f29afae5b1c9eaf09fc5e36d1f57b02
Mateusz Kopij <sagasu@gmail.com>
2023-10-04 19:57:08 +0100

    Skiplist

---
+++ AlgoTest/LeetCode/Design Skiplist/Design Skiplist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Design_Skiplist;
+
+public class Skiplist
+{
+    SortedDictionary<int,int> sortedDict = new();
+    public Skiplist() {
+    
+    }
+
+    public bool Search(int target) => sortedDict.ContainsKey(target);
+
+    public void Add(int num) {
+        if(!sortedDict.TryAdd(num,1))
+            sortedDict[num]+=1;
+    }
+
+    public bool Erase(int num) {
+        if(sortedDict.ContainsKey(num))
+        {
+            if(sortedDict[num]>1)
+                sortedDict[num]-=1;
+            else
+                sortedDict.Remove(num);
+            
+            return true;
+        }
+        return false;
+    }
+}
