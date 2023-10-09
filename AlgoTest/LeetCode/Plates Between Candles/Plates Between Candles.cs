c936a94480e176fdfd0022b6a3b528a2bf4da9e9
Mateusz Kopij <sagasu@gmail.com>
2023-10-09 17:05:06 +0100

    Plates Between Candles

---
+++ AlgoTest/LeetCode/Plates Between Candles/Plates Between Candles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Plates_Between_Candles;
+
+public class Plates_Between_Candles
+{
+    public int[] PlatesBetweenCandles(string s, int[][] queries) {
+        var candles = new List<int>(s.Length);
+
+        for (var i = 0; i < s.Length; ++i)
+            if (s[i] == '|') 
+                candles.Add(i);
+
+        var result = new int[queries.Length];
+
+        for (var i = 0; i < queries.Length; ++i) {
+            var left = candles.BinarySearch(queries[i][0]);
+
+            if (left < 0)
+                left = ~left;
+
+            var right = candles.BinarySearch(queries[i][1]);
+
+            if (right < 0)
+                right = ~right - 1;
+
+            if (left < right)
+                result[i] = candles[right] - candles[left] - (right - left);
+        }
+
+        return result;
+    }
+}
