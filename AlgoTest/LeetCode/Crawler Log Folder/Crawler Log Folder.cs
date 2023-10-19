05f6daf79bf51aca672b183d889448ca6a3cbe1a
Mateusz Kopij <sagasu@gmail.com>
2023-10-19 18:50:36 +0100

    Crawler Log Folder

---
+++ AlgoTest/LeetCode/Crawler Log Folder/Crawler Log Folder.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgoTest.LeetCode.Crawler_Log_Folder;
+
+public class Crawler_Log_Folder
+{
+    public int MinOperations(string[] logs) 
+    {
+        var level = 0;       
+        foreach(var l in logs)
+        {
+            if (l == "../") {             
+                if(level > 0) level--;
+            } else if(l != "./")
+                level++;
+        }
+        
+        return level;
+    }
+}
