f847da3e154d8792ce06fd28ed40d1ceb9a5499e
Mateusz Kopij <sagasu@gmail.com>
2023-11-19 22:04:20 +0000

    Minimum_Size_Subarray_Sum

---
+++ AlgoTest/LeetCode/Minimum Size Subarray Sum/Minimum Size Subarray Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Minimum_Size_Subarray_Sum;
+
+[TestClass]
+public class Minimum_Size_Subarray_Sum
+{
+    [TestMethod]
+    public void TesT() => Assert.AreEqual(2, MinSubArrayLen(7, new[] { 2, 3, 1, 2, 4, 3 }));
+    
+    public int MinSubArrayLen(int target, int[] nums) {
+        var n = nums.Length;
+        var ans = int.MaxValue;
+        var left = 0;
+        var sum = 0;
+        for (var i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum >= target) {
+                ans = Math.Min(ans, i + 1 - left);
+                sum -= nums[left++];
+            }
+        }
+        
+        return (ans != int.MaxValue) ? ans : 0;
+    }
+}
