557586c5e47e575b201c104cdfe5f45fadf86396
Mateusz Kopij <sagasu@gmail.com>
2023-12-07 09:07:49 +0000

    Advent of code day 7 part 2

--- AlgoTest/AOC2023/Day7.cs
+++ AlgoTest/AOC2023/Day7.cs
@@ -50,7 +50,48 @@ public class Day7
 
     }
 
-    class HandComparer : IComparer<Hand>
+    [TestMethod]
+    public void Test2()
+    {
+        var lines = data
+            .Split("\r\n", StringSplitOptions.TrimEntries);
+
+        var hands = lines
+            .Select(line => line.Split(" "))
+            .Select(lineParts => (
+                hand: lineParts[0],
+                bid: int.Parse(lineParts[1]),
+                groups: lineParts[0].GroupBy(c => c).Where(g => g.First() != 'J'),
+                jokers: lineParts[0].Count(c => c is 'J')
+            ))
+            .Select(hand => new Hand(
+                hand.hand,
+                hand.bid,
+                type: hand.groups.Count() is 0 ? 7
+                : hand.groups.Any(g => g.Count() >= 5 - hand.jokers) ? 7
+                : hand.groups.Any(g => g.Count() >= 4 - hand.jokers) ? 6
+                : hand.groups.FirstOrDefault(g => g.Count() >= 3 - hand.jokers) is { } larger && hand.groups.Any(g => g.First() != larger.First() && g.Count() >= 2) ? 5
+                : hand.groups.Any(g => g.Count() >= 3 - hand.jokers) ? 4
+                : hand.groups.FirstOrDefault(g => g.Count() >= 2) is { } firstPair && hand.groups.Any(g => g.First() != firstPair.First() && g.Count() >= 2 - hand.jokers) ? 3
+                : hand.groups.Any(g => g.Count() >= 2 - hand.jokers) ? 2
+                : 1
+            ))
+            .ToArray();
+
+        var orderedHands = hands
+            .OrderBy(hand => hand, new HandComparer())
+            .ToArray();
+
+        int winnings = 0;
+        for (int i = 0; i < orderedHands.Length; i++)
+        {
+            winnings += orderedHands[i].bid * (i + 1);
+        }
+
+        Console.WriteLine(winnings);
+    }
+
+    public class HandComparer : IComparer<Hand>
     {
         public int Compare(Hand x, Hand y)
         {
@@ -73,20 +114,21 @@ public class Day7
             'A' => 13,
             'K' => 12,
             'Q' => 11,
-            'J' => 10,
-            'T' => 9,
-            '9' => 8,
-            '8' => 7,
-            '7' => 6,
-            '6' => 5,
-            '5' => 4,
-            '4' => 3,
-            '3' => 2,
+            'T' => 10,
+            '9' => 9,
+            '8' => 8,
+            '7' => 7,
+            '6' => 6,
+            '5' => 5,
+            '4' => 4,
+            '3' => 3,
+            '2' => 2,
             _ => 1
         };
     }
+
+    public record struct Hand(string hand, int bid, int type);
     
-    record struct Hand(string hand, int bid, int type);
     string data = """
                             AT777 727
                             7QT8A 998
