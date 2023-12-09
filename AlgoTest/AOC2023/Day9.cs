e20acbf3384a0db41ac5284009bacfce9fa56f84
Mateusz Kopij <sagasu@gmail.com>
2023-12-09 18:52:56 +0000

    Advent of code day 9 part 2

--- AlgoTest/AOC2023/Day9.cs
+++ AlgoTest/AOC2023/Day9.cs
@@ -30,6 +30,32 @@ public class Day9
         Console.WriteLine(result);
     }
     
+    [TestMethod]
+    public void Test2()
+    {
+        var lines = data.ReplaceLineEndings("\n").Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var result = 0;
+                foreach(var line in lines)
+                {
+                    List<int[]> sequences = new(){line.Split(' ').Select(int.Parse).ToArray()};
+                    var currentSequence = sequences[0];
+                    while(currentSequence.Any(x => x != 0))
+                    {
+                        var diff = currentSequence[..^1].Select((number, index) => currentSequence[index + 1] - number).ToArray();
+                        sequences.Add(diff);
+                        currentSequence = diff;
+                    }
+        
+                    var tempresult = 0;
+                    for(int i = sequences.Count - 1; i >= 0; i--)
+                        tempresult = tempresult * -1 + sequences[i][0];
+        
+                    result += tempresult;
+                }
+        
+                Console.WriteLine(result);
+    }
+    
     string data = """
                     -5 0 14 35 64 120 276 742 2049 5429 13539 31751 70366 148419 300461 589269 1130567 2141638 4035732 7600527 14324252
                     12 32 65 127 242 435 720 1091 1536 2105 3067 5179 10050 20500 40670 75413 128164 196022 260149 268767 108978
