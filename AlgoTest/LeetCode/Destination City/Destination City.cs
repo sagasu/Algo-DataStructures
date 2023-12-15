a52c5649d7cc6fe9bdfbb4480ee5efcb1c7d874d
Mateusz Kopij <sagasu@gmail.com>
2023-12-15 20:32:40 +0000

    Destination City

---
+++ AlgoTest/LeetCode/Destination City/Destination City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Destination_City;
+
+public class Destination_City
+{
+    public string DestCity(IList<IList<string>> paths)
+    {
+        var dic = new HashSet<string>();
+        foreach (var path in paths)
+            dic.Add(path[0]);
+
+        foreach (var path in paths)
+            if (dic.Add(path[1])) return path[1];
+
+        throw new InvalidDataException("not found");
+    }
+}
