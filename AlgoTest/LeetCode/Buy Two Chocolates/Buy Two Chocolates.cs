a8ae33cfe19b65d63461cfb0eb297a2bb3342ac0
Mateusz Kopij <sagasu@gmail.com>
2023-12-20 19:54:03 +0000

    Buy Two Chocolates

---
+++ AlgoTest/LeetCode/Buy Two Chocolates/Buy Two Chocolates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Buy_Two_Chocolates;
+
+public class Buy_Two_Chocolates
+{
+    public int BuyChoco(int[] prices, int money) {
+        Array.Sort(prices);
+        var cur = prices[0] + prices[1];
+        return cur > money ? money : money - cur;
+    }
+}
