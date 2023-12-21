7356736a0f6b134b27e914f7f4451bda85317a69
Mateusz Kopij <sagasu@gmail.com>
2023-12-21 20:04:14 +0000

    Widest Vertical Area Between Two Points Containing No Points

---
+++ AlgoTest/LeetCode/Widest Vertical Area Between Two Points Containing No Points/Widest Vertical Area Between Two Points Containing No Points.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Widest_Vertical_Area_Between_Two_Points_Containing_No_Points;
+
+public class Widest_Vertical_Area_Between_Two_Points_Containing_No_Points
+{
+    public int MaxWidthOfVerticalArea(int[][] points) {
+        var ordered = points
+            .Select(p => p[0])
+            .OrderBy(p => p);
+        return ordered
+            .SkipLast(1)
+            .Zip(ordered.Skip(1), (a, b) => b - a)
+            .Max();
+    }
+}
