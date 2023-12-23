fb140b48a261abcd6b5acfdef20400e116f3b0cc
Mateusz Kopij <sagasu@gmail.com>
2023-12-23 19:40:16 +0000

    Path_Crossing

---
+++ AlgoTest/LeetCode/Path Crossing/Path Crossing.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Path_Crossing;
+
+public class Path_Crossing
+{
+    public bool IsPathCrossing(string P, int x = 0, int y = 0) => P
+        .Select(c => c switch {
+            'N' => (x, ++y),
+            'S' => (x, --y),
+            'E' => (++x, y),
+            _  => (--x, y),
+        })
+        .Append((0, 0))
+        .Distinct()
+        .Count() <= P.Length;
+}
