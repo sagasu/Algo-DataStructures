b1ecad908517c5c68ad64f016c439caea5f9435c
Mateusz Kopij <sagasu@gmail.com>
2023-12-24 13:21:23 +0000

    Minimum Changes To Make Alternating Binary String

---
+++ AlgoTest/LeetCode/Minimum Changes To Make Alternating Binary String/Minimum Changes To Make Alternating Binary String.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Changes_To_Make_Alternating_Binary_String;
+
+public class Minimum_Changes_To_Make_Alternating_Binary_String
+{
+    public int MinOperations(string s) => Math.Min(
+        s.Select((c, i) => (c + i) % 2).Sum(),
+        s.Select((c, i) => (c + i + 1) % 2).Sum());
+}
