9f9bf3b1cb078f476a44f5490e3a7c7e93c794d4
Mateusz Kopij <sagasu@gmail.com>
2024-01-10 19:14:09 +0000

    Amount of Time for Binary Tree to Be Infected

--- AlgoTest/LeetCode/Amount of Time for Binary Tree to Be Infected/Amount of Time for Binary Tree to Be Infected.cs
+++ AlgoTest/LeetCode/Amount of Time for Binary Tree to Be Infected/Amount of Time for Binary Tree to Be Infected.cs
@@ -1,52 +1,54 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgoTest.LeetCode.Amount_of_Time_for_Binary_Tree_to_Be_Infected;
 
 public class Amount_of_Time_for_Binary_Tree_to_Be_Infected
 {
-    public int AmountOfTime(TreeNode root, int start) {
-        var g = new Dictionary<int, List<int>>();
-        buildGraph(root, g);
-        
-        var visited = new HashSet<int>();
-        var queue = new Queue<int>();
-        queue.Enqueue(start);
-        visited.Add(start);
-        var res = 0;
-        while(queue.Count > 0){
-            var size = queue.Count;
-            for(var i = 0; i < size; i++){
-                var curr = queue.Dequeue();
-                
-                foreach(var next in g[curr]){
-                    if(!visited.Contains(next)){
-                        queue.Enqueue(next);
-                        visited.Add(next);
-                    }
-                }
+    public int AmountOfTime(TreeNode root, int start)
+    {
+        var adjDict = GetAdjacencyDictionary(root);
+        var infected = new HashSet<int>();
+        var toInfect = new Queue<int>();
+        var time = -1;
+
+        toInfect.Enqueue(start);
+
+        while (toInfect.Any())
+        {
+            time += 1;
+            var count = toInfect.Count;
+            for (var i = 0; i < count; i++)
+            {
+                var curr = toInfect.Dequeue();
+                if (infected.Add(curr))
+                    foreach (var next in adjDict[curr])
+                        if (!infected.Contains(next))
+                            toInfect.Enqueue(next);
             }
-            
-            res++;
         }
-        return res-1;
+
+        return time;
     }
-    
-    private void buildGraph(TreeNode root, Dictionary<int, List<int>> g){
-        if(!g.ContainsKey(root.val))
-            g.Add(root.val, new List<int>());
-        
-        if(root.left != null && !g.ContainsKey(root.left.val)){
-            g.Add(root.left.val, new List<int>());
-            g[root.val].Add(root.left.val);
-            g[root.left.val].Add(root.val);
-            buildGraph(root.left, g);
-        }
-        
-        if(root.right != null && !g.ContainsKey(root.right.val)){
-            g.Add(root.right.val, new List<int>());
-            g[root.val].Add(root.right.val);
-            g[root.right.val].Add(root.val);
-            buildGraph(root.right, g);
+
+    private Dictionary<int, List<int>> GetAdjacencyDictionary(TreeNode root)
+    {
+        var adjDict = new Dictionary<int, List<int>>();
+        adjDict[root.val] = new();
+        TraverseDfs(root.left, root);
+        TraverseDfs(root.right, root);
+        return adjDict;
+
+        void TraverseDfs(TreeNode node, TreeNode parent)
+        {
+            if (node != null)
+            {
+                adjDict[node.val] = new();
+                adjDict[node.val].Add(parent.val);
+                adjDict[parent.val].Add(node.val);
+                TraverseDfs(node.left, node);
+                TraverseDfs(node.right, node);
+            }
         }
     }
 }
