d5c9682c0397fc94196b09b4bbe15f8d62f69b01
Mateusz Kopij <sagasu@gmail.com>
2024-01-27 19:54:08 +0000

    Maximum Earnings From Taxi

---
+++ AlgoTest/LeetCode/Maximum Earnings From Taxi/Maximum Earnings From Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Maximum_Earnings_From_Taxi;
+
+public class MaximumEarningsFromTaxi
+{
+    public long MaxTaxiEarnings(int n, int[][] rides) {
+        var dp = new long[n + 1];
+        var starts = rides
+            .GroupBy(x => x[0])
+            .ToDictionary(x => x.Key, x => x.ToHashSet());
+        
+        for (var i = 1; i <= n; i++) {
+            dp[i] = Math.Max(dp[i - 1], dp[i]);
+            
+            if (!starts.ContainsKey(i)) {
+                continue;
+            }
+            
+            foreach (var ride in starts[i]) {
+                var start = i; 
+                var end = ride[1];
+                var tip = ride[2];
+                dp[end] = Math.Max(dp[end], end - start + tip + dp[i]);
+            }
+        }
+        
+        return dp[n];
+    }
+}
