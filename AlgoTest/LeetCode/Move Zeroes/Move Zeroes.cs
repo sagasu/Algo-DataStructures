781290be8d5a6deb2fa27fb25f942b968c440859
Mateusz Kopij <sagasu@gmail.com>
2024-01-30 19:07:01 +0000

    Move Zeroes

---
+++ AlgoTest/LeetCode/Move Zeroes/Move Zeroes.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoTest.LeetCode.Move_Zeroes;
+
+public class Move_Zeroes
+{
+    public void MoveZeroes(int[] nums) {
+        var count = 0;
+        for(var i = nums.Length-1;i>=0;i--){
+            if(nums[i]==0)
+            {
+                for(var j = i;j<nums.Length-1-count;j++)
+                {
+                    nums[j] = nums[j+1];
+                    nums[j+1] = 0;
+                }  
+                count ++;
+            }
+        }
+    }
+}
