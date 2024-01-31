0b3b60a519681edcfea5aafbfbef28f854f07f6e
Mateusz Kopij <sagasu@gmail.com>
2024-01-31 21:34:16 +0000

    Count_and_Say

--- AlgoTest/LeetCode/Count and Say/Count and Say.cs
+++ AlgoTest/LeetCode/Count and Say/Count and Say.cs
@@ -11,12 +11,14 @@ namespace AlgoTest.LeetCode.Count_and_Say
         public string CountAndSay(int n)
         {
             if (n == 1) return "1";
+            
             var prev = CountAndSay(n - 1);
             var sb = new StringBuilder();
             var count = 1;
             for (var i = 0; i < prev.Length; i++)
             {
                 if (i + 1 < prev.Length && prev[i] == prev[i + 1]) count++;
+                
                 else
                 {
                     sb.Append(count + "" + prev[i]);
