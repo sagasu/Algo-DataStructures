d21235a0ea86d42f1efd8a2f46b0acad07e05ac5
Mateusz Kopij <sagasu@gmail.com>
2024-02-16 17:40:07 +0000

    Least Number of Unique Integers after K Removals

---
+++ AlgoTest/LeetCode/Least Number of Unique Integers after K Removals/Least Number of Unique Integers after K Removals.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Least_Number_of_Unique_Integers_after_K_Removals;
+
+public class Least_Number_of_Unique_Integers_after_K_Removals
+{
+    public int FindLeastNumOfUniqueInts(int[] arr, int k) {
+        var map = new Dictionary<int, int>();
+        foreach (var i in arr)
+        {
+            map.TryAdd(i, 0);
+            map[i] += 1;
+        }
+
+        var frequencies = map.Values.ToList();
+
+        frequencies.Sort();
+
+        var elementsRemoved = 0;
+
+        for (var i = 0; i < frequencies.Count; i++) {
+            elementsRemoved += frequencies[i];
+            
+            if (elementsRemoved > k)
+                return frequencies.Count - i;
+        }
+        
+        return 0;
+    }
+}
