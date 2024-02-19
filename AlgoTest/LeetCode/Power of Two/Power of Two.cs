84c4d35b1afd8f77f00cd4fabf669c1fa3441925
Mateusz Kopij <sagasu@gmail.com>
2024-02-19 20:36:24 +0000

    Power of Two

---
+++ AlgoTest/LeetCode/Power of Two/Power of Two.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgoTest.LeetCode.Power_of_Two;
+
+public class Power_of_Two
+{
+    public bool IsPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        var result = 1;
+        
+        for (var i = 0; i < 31; i++) 
+            if (n == (result << i)) 
+                return true;
+        return false;
+    }
+}
