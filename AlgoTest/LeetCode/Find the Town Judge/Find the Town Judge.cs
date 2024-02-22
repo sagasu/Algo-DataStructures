1b5200c4ea744d3d295748afe1a08fa0f7d317fa
Mateusz Kopij <sagasu@gmail.com>
2024-02-22 20:24:08 +0000

    Find the Town Judge

---
+++ AlgoTest/LeetCode/Find the Town Judge/Find the Town Judge.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_the_Town_Judge;
+
+public class Find_the_Town_Judge
+{
+    public int FindJudge(int n, int[][] trust) {
+        return trust.GroupBy(t => t[1])
+            .Where(g => g.Count() == n - 1)
+            .Select(g => g.Key)
+            .Except(trust.Select(t => t[0]))
+            .SingleOrDefault(n == 1 ? 1 : -1);
+    }
+}
