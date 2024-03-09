8144d486544f6c79d5224b86510899b7b4c8fe4c
Mateusz Kopij <sagasu@gmail.com>
2024-03-09 19:24:48 +0100

    Create Minimum Common Value.cs

---
+++ AlgoTest/LeetCode/Minimum Common Value.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        HashSet<int> set = new HashSet<int>(nums1);
+
+        foreach (int num in nums2) {
+            if (set.Contains(num)) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
