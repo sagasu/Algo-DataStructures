226827cd00c826c020b9493a13f54fbd5fadc96f
Mateusz Kopij <sagasu@gmail.com>
2024-03-19 19:39:41 +0000

    Task Scheduler

---
+++ AlgoTest/LeetCode/Task Scheduler/Task Scheduler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Task_Scheduler;
+
+public class Task_Scheduler
+{
+    public int LeastInterval(char[] tasks, int n) {
+        var freq = new int[26];
+        foreach(char ch in tasks)
+            freq[ch-'A']++;
+			
+        int max = 0, maxCount = 0;
+        foreach (var t in freq)
+        {
+            if(t > max)
+            {
+                max = t;
+                maxCount = 1;
+            } 
+            else if(t == max)
+                maxCount++;
+        }
+        
+        return Math.Max(max+n*(max-1)+maxCount-1, tasks.Length);
+    }
+}
