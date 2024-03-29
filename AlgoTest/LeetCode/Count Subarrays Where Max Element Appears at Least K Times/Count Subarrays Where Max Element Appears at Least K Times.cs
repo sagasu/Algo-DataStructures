79e545449ef2ce56871146eef9f06ee800165d2f
Mateusz Kopij <sagasu@gmail.com>
2024-03-29 21:30:31 +0000

    Count Subarrays Where Max Element Appears at Least K Times

---
+++ AlgoTest/LeetCode/Count Subarrays Where Max Element Appears at Least K Times/Count Subarrays Where Max Element Appears at Least K Times.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times;
+
+public class Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times
+{
+    public long CountSubarrays(int[] nums, int k) {
+        var maxElement = nums.Max();
+        long count = 0;
+        var windowStart = 0;
+        var maxCount = 0;
+
+        foreach (var t in nums)
+        {
+            if (t == maxElement) {
+                maxCount++;
+            }
+
+            while (maxCount >= k) {
+                if (nums[windowStart] == maxElement) {
+                    maxCount--;
+                }
+                windowStart++;
+            }
+
+            count += windowStart;
+        }
+
+        return count;
+    }
+}
