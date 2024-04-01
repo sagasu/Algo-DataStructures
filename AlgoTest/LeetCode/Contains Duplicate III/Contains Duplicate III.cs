35b0601ee4da57bb38c9c24d455c4fc41800bc9d
Mateusz Kopij <sagasu@gmail.com>
2024-04-01 21:05:31 +0100

    Contains_Duplicate_III

---
+++ AlgoTest/LeetCode/Contains Duplicate III/Contains Duplicate III.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Contains_Duplicate_III;
+
+public class Contains_Duplicate_III
+{
+    public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (t < 0) return false;
+        var n = nums.Length;
+        var sortedSet = new SortedSet<long>();
+        for (var i = 0; i < n; i++) {
+            if (sortedSet.GetViewBetween((long)nums[i] - t, (long)nums[i] + t).Count > 0) return true;
+            sortedSet.Add(nums[i]);
+            if (i >= k) sortedSet.Remove(nums[i - k]);
+        }
+        return false;
+    }
+}
