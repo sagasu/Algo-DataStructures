e31fde3cf28cc4901a5d2fd902f6b13eb304d800
Mateusz Kopij <sagasu@gmail.com>
2024-04-10 19:22:15 +0100

    Reveal Cards In Increasing Order

---
+++ AlgoTest/LeetCode/Reveal Cards In Increasing Order/Reveal Cards In Increasing Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Reveal_Cards_In_Increasing_Order;
+
+public class Reveal_Cards_In_Increasing_Order
+{
+    public int[] DeckRevealedIncreasing(int[] deck) {
+        var n = deck.Length;
+        Array.Sort(deck);
+
+        LinkedList<int> list = new();
+        list.AddLast(deck[n - 1]);
+
+        for(var i = n - 2; i >= 0; i--)
+        {
+            list.AddFirst(list.Last.Value);
+            list.RemoveLast();
+            list.AddFirst(deck[i]);
+        }
+
+        return list.ToArray();
+    }
+}
