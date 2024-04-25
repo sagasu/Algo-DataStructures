fd4fa83c2163e1df50bba62fab51fca39e48c0da
Mateusz Kopij <sagasu@gmail.com>
2024-04-25 20:48:20 +0100

    Longest_Ideal_Subsequence

---
+++ AlgoTest/LeetCode/Longest Ideal Subsequence/Longest Ideal Subsequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Longest_Ideal_Subsequence;
+
+public class Longest_Ideal_Subsequence
+{
+    public int LongestIdealString(string s, int k) {
+        var lasts = new int[26];
+        
+        var result = 0;
+        
+        foreach (var c in s)
+        {
+            var current = 1;
+            
+            for (var d = -k; d <= k; ++d) {
+                var index = (c - 'a' + d);
+
+                if (index < 0 || index > 25)
+                    continue;
+                
+                current = Math.Max(lasts[index] + 1, current);
+            }
+            
+            result = Math.Max(lasts[c - 'a'] = current, result);
+        }
+        
+        return result;
+    }
+}
