d9c4a10f92686fcd9c34d35bee5c45a5e1cda17c
Mateusz Kopij <sagasu@gmail.com>
2024-04-29 21:41:58 +0100

    Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K

---
+++ AlgoTest/LeetCode/Minimum Number of Operations to Make Array XOR Equal to K/Minimum Number of Operations to Make Array XOR Equal to K.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace AlgoTest.LeetCode.Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K;
+
+public class Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K
+{
+    public int MinOperations(int[] nums, int k) {
+        return BitOperations.PopCount((uint) nums.Aggregate(k, (current, num) => current ^ num));
+    }
+}
