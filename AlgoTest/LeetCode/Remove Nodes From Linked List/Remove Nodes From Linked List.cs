dfef1f01b7675644d2573bd2aac210eea53bb7db
Mateusz Kopij <sagasu@gmail.com>
2024-05-05 22:49:25 +0100

    Remove_Nodes_From_Linked_List

---
+++ AlgoTest/LeetCode/Remove Nodes From Linked List/Remove Nodes From Linked List.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Remove_Nodes_From_Linked_List;
+
+public class Remove_Nodes_From_Linked_List
+{
+    public ListNode RemoveNodes(ListNode head) {
+        var l = new List<ListNode>();
+        var curr = head;
+        
+        while(curr!=null){
+            if(l.Count!=0)
+                while(l.Count!=0 && l[l.Count-1].val<curr.val)
+                    l.RemoveAt(l.Count-1);
+            
+            l.Add(curr);
+            curr=curr.next;
+        }
+        
+        var dummyhead=new ListNode();
+        var trav=dummyhead;
+        
+        foreach (var t in l)
+        {
+            trav.next=t;
+            trav=trav.next;
+        }
+        
+        return dummyhead.next;
+    }
+}
