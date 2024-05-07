774d3729b70d3d5685c7ee7f60bbe6d333eaa5c5
Mateusz Kopij <sagasu@gmail.com>
2024-05-07 22:22:49 +0100

    Double a Number Represented as a Linked List

---
+++ AlgoTest/LeetCode/Double a Number Represented as a Linked List/Double a Number Represented as a Linked List.cs
@@ -0,0 +1,19 @@
+﻿using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Double_a_Number_Represented_as_a_Linked_List;
+
+public class Double_a_Number_Represented_as_a_Linked_List
+{
+    public ListNode DoubleIt(ListNode head) {
+        head = head.val >= 5 ? new ListNode(0, head) : head;
+
+        for (var node = head; node != null; node = node.next)
+        {
+            var carry = node.next is { val: >= 5 } ? 1 : 0;
+
+            node.val = node.val * 2 % 10 + carry;
+        }
+
+        return head;
+    }
+}
