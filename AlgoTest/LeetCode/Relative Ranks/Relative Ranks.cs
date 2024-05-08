b2716c8f51191d88bb04196f27fb180c1286a12f
Mateusz Kopij <sagasu@gmail.com>
2024-05-08 20:22:58 +0100

    Relative Ranks

---
+++ AlgoTest/LeetCode/Relative Ranks/Relative Ranks.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Relative_Ranks;
+
+public class Relative_Ranks
+{
+    public string[] FindRelativeRanks(int[] score)
+        => score
+            .Select((n, i) => new{n, i})
+            .OrderBy(x => -x.n)
+            .Select((x, i) => 
+                new
+                {
+                    i = x.i, 
+                    p = i <= 2 ? (i == 0 ? "Gold" : i == 1 ? "Silver" : "Bronze") + " Medal" : 
+                        $"{i+1}"
+                })
+            .OrderBy(x => x.i)
+            .Select(x => x.p).ToArray(); 
+}
