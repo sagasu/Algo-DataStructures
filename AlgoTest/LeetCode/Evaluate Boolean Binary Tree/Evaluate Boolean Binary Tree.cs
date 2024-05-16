2d4233126e576fb775c54fbd68a528c0ba756742
Mateusz Kopij <sagasu@gmail.com>
2024-05-16 22:01:05 +0100

    Evaluate Boolean Binary Tree

---
+++ AlgoTest/LeetCode/Evaluate Boolean Binary Tree/Evaluate Boolean Binary Tree.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgoTest.LeetCode.Evaluate_Boolean_Binary_Tree;
+
+public class Evaluate_Boolean_Binary_Tree
+{
+    public bool EvaluateTree(TreeNode node) => node.val switch
+    {
+        0 => false,
+        1 => true,
+        2 => EvaluateTree(node.left) || EvaluateTree(node.right),
+        3 => EvaluateTree(node.left) && EvaluateTree(node.right),
+    };
+}
