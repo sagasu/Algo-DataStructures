a1941c8028dd385c277afe11651b0a43771d9552
Mateusz Kopij <sagasu@gmail.com>
2024-05-20 16:50:52 +0100

    sum of All Subset XOR Totals

---
+++ AlgoTest/LeetCode/Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoTest.LeetCode.Sum_of_All_Subset_XOR_Totals;
+
+public class Sum_of_All_Subset_XOR_Totals
+{
+    public int SubsetXORSum(int[] nums) {
+        var xorSum = 0;
+        Backtracking(nums, ref xorSum, 0, 0);
+        return xorSum;
+    }
+    
+    void Backtracking(int[] nums, ref int xorSum, int xor, int index) {
+        for (int i=index; i<nums.Length; i++) {
+            xor = xor^nums[i];
+            xorSum += xor;
+            Backtracking(nums, ref xorSum, xor, i+1);
+            xor = xor^nums[i];
+        }
+    }
+}
