f1a256b9ecbe1a0da4052cfca42ee1a74b52e5fc
Mateusz Kopij <sagasu@gmail.com>
2024-05-23 21:04:24 +0100

    The Number of Beautiful Subsets

---
+++ AlgoTest/LeetCode/The Number of Beautiful Subsets/The Number of Beautiful Subsets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.The_Number_of_Beautiful_Subsets;
+
+public class The_Number_of_Beautiful_Subsets
+{
+    public int BeautifulSubsets(int[] nums, int k) {
+        var res = 0;
+        DFS(nums, 0, new List<int>(), ref res, k);
+        return res;
+    }
+
+    void DFS(int[] nums, int index, List<int> list, ref int res, int k) {
+        if (index == nums.Length) {
+            if (list.Count > 0) {
+                res++;
+            }
+            return;
+        }
+
+        DFS(nums, index + 1, list, ref res, k);
+        if (list.Count == 0 || (!list.Contains(nums[index] - k) && !list.Contains(nums[index] + k))) {
+            list.Add(nums[index]);
+            DFS(nums, index + 1, list, ref res, k);
+            list.RemoveAt(list.Count - 1);
+        }
+    }
+}
