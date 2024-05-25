fa154f88d785ae6f1df512078d41e30e9ae398dc
Mateusz Kopij <sagasu@gmail.com>
2024-05-25 13:43:12 +0100

    Word Break II

---
+++ AlgoTest/LeetCode/Word Break II/Word Break II.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Word_Break_II;
+
+public class Word_Break_II
+{
+    public IList<string> WordBreak(string s, IList<string> wordDict) => Helper(s, wordDict, 0, new List<string>[s.Length]);
+
+    private List<string> Helper(string s, IList<string> dict, int i, List<string>[] cache)
+    {
+        if (cache[i] != null)
+            return cache[i];
+        
+        var cur = new List<string>();
+        
+        for (var j = i; j < s.Length; j++)
+            if (dict.Contains(s.Substring(i, j - i + 1)))
+            {
+                if (j == s.Length - 1)
+                    cur.Add(s.Substring(i, j - i + 1));
+                else
+                    foreach (var str in Helper(s, dict, j + 1, cache))
+                        cur.Add(s.Substring(i, j - i + 1) + " " + str);
+            }
+        
+        cache[i] = cur;
+        
+        return cache[i];
+    }
+}
