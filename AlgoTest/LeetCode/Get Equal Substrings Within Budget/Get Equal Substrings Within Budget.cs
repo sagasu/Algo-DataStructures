1fceb90cd908a533d761e25b683c67f746ac02f1
Mateusz Kopij <sagasu@gmail.com>
2024-05-28 21:09:14 +0100

    Get Equal Substrings Within Budget

---
+++ AlgoTest/LeetCode/Get Equal Substrings Within Budget/Get Equal Substrings Within Budget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Get_Equal_Substrings_Within_Budget;
+
+public class Get_Equal_Substrings_Within_Budget
+{
+    public int EqualSubstring(string s, string t, int maxCost) 
+    {
+        var st=0;
+        var end=0;
+        var maxLength = 0;
+        var cost = 0;
+        
+        while(end<s.Length)
+        {
+            var diff = Math.Abs(s[end]-t[end]);
+            cost+=diff;
+            
+            while(cost>maxCost)
+            {
+                cost-=Math.Abs(s[st]-t[st]);
+                st++;
+            }
+            
+            maxLength = Math.Max(maxLength,end-st+1);
+            end++;
+        }
+        
+        return maxLength;
+    }
+}
