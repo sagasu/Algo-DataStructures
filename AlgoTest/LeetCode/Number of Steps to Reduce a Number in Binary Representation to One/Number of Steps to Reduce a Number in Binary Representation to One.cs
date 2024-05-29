585545400faa3246a033d6b6e0848894b466536a
Mateusz Kopij <sagasu@gmail.com>
2024-05-29 19:15:10 +0100

    Number of Steps to Reduce a Number in Binary Representation to One

---
+++ AlgoTest/LeetCode/Number of Steps to Reduce a Number in Binary Representation to One/Number of Steps to Reduce a Number in Binary Representation to One.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoTest.LeetCode.Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One;
+
+public class Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One
+{
+    public int NumSteps(string s)
+    {
+        var res = 0;
+        var carry = 0;
+        for (var i = s.Length - 1; i > 0; i--)
+        {
+            if (s[i] - '0' + carry == 1)
+            {
+                res++;
+                carry = 1;
+            }
+            res++;
+        }
+
+        return res + carry;
+    }
+}
