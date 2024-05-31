29321b56d4b7c95f25103a16337a0d7dd1494cfb
Mateusz Kopij <sagasu@gmail.com>
2024-05-31 15:13:00 +0100

    Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR

--- AlgoTest/LeetCode/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.cs
+++ AlgoTest/LeetCode/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.cs
@@ -8,13 +8,16 @@ public class Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR
         for(var i=0;i<len-1;i++)
         {
             var a=0;
+            
             for(var j=i;j<len-1;j++)
             {
                 a^=arr[j];
                 var b=0;
+                
                 for(var k=j+1;k<len;k++)
                 {
                     b^=arr[k];
+                    
                     if(a==b)
                         count++;
                 }
