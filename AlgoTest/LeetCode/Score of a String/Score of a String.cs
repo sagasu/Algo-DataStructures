3278891dbea8332da3e5abf49c7dcf22ecfdece0
Mateusz Kopij <sagasu@gmail.com>
2024-06-01 21:08:48 +0100

    Score_of_a_String

---
+++ AlgoTest/LeetCode/Score of a String/Score of a String.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Score_of_a_String;
+
+public class Score_of_a_String
+{
+    public int ScoreOfString(string s) => Enumerable
+        .Range(0, s.Length - 1)
+        .Sum(i => Math.Abs(s[i] - s[i + 1]));   
+}
