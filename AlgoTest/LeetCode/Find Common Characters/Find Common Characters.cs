5e3974ce4ac477fa57476dd41af9ff943bdbe82e
Mateusz Kopij <sagasu@gmail.com>
2024-06-05 21:37:55 +0100

    Find Common Characters

---
+++ AlgoTest/LeetCode/Find Common Characters/Find Common Characters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_Common_Characters;
+
+public class Find_Common_Characters
+{
+    public IList<string> CommonChars(string[] words) 
+    {
+        var common = words[0].ToCharArray();
+
+        for(int i=1; i<words.Length; i++)
+            common = Intersect(common, words[i].ToCharArray()).ToArray();
+
+        return common.Select(x=>x.ToString()).ToArray();
+    }
+
+    private IEnumerable<T> Intersect<T>(IEnumerable<T> source, IEnumerable<T> target)
+    {
+        var list = target.ToList();
+        foreach (T item in source)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                yield return item;
+            }
+        }
+    }
+}
