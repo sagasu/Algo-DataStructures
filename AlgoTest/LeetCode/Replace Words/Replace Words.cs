6e117e87572424c7e0015dfe68d899b764537707
Mateusz Kopij <sagasu@gmail.com>
2024-06-07 07:08:39 +0100

    Replace Words

---
+++ AlgoTest/LeetCode/Replace Words/Replace Words.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Replace_Words;
+
+public class Replace_Words
+{
+    public string ReplaceWords(IList<string> dictionary, string sentence) {
+        HashSet<string> set = new();
+        List<string> ans = new();
+
+        foreach(var word in dictionary)
+            set.Add(word);
+        
+
+        foreach(var word in sentence.Split(' '))
+        {
+            var prefix = "";
+            foreach (var t in word)
+            {
+                prefix += t;
+                if(set.Contains(prefix))
+                    break;
+            }
+
+            ans.Add(prefix);
+        }
+
+        return string.Join(' ', ans);
+    }
+}
