97368e0f1907e4b2f6cad4ccc4c353e282dbc474
Mateusz Kopij <sagasu@gmail.com>
2024-06-09 20:14:51 +0100

    Count_Number_of_Bad_Pairs

---
+++ AlgoTest/LeetCode/Count Number of Bad Pairs/Count Number of Bad Pairs.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Count_Number_of_Bad_Pairs;
+
+public class Count_Number_of_Bad_Pairs
+{
+    public long CountBadPairs(int[] nums) => nums.Length * (nums.Length - 1L) / 2 - nums
+        .Select((value, index) => value - index)
+        .GroupBy(item => item)
+        .Sum(group => group.Count() * (group.Count() - 1L) / 2);
+}
