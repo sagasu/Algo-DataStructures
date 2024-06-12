e987199662af466672557f36df0fbf7456c1c175
Mateusz Kopij <sagasu@gmail.com>
2024-06-12 13:51:34 +0100

    Wiggle Sort II

---
+++ AlgoTest/LeetCode/Wiggle Sort II/Wiggle Sort II.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Wiggle_Sort_II;
+
+public class Wiggle_Sort_II
+{
+    public void WiggleSort(int[] nums) 
+    {
+        var n = nums.Length;
+        var pq = new PriorityQueue<int, int>();
+        for(int i = 0; i < n; ++i)
+            pq.Enqueue(nums[i], -nums[i]);
+        
+        for(int i = 1; i < n; i = i + 2)
+            nums[i] = pq.Dequeue();
+        
+        for(int i = 0; i < n; i = i + 2)
+            nums[i] = pq.Dequeue();
+    }
+}
