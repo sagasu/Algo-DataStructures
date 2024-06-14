6352f93b2e329f284a5570a4aeb49b013498e643
Mateusz Kopij <sagasu@gmail.com>
2024-06-14 07:25:00 +0100

    Minimum_Increment_to_Make_Array_Unique

---
+++ AlgoTest/LeetCode/Minimum Increment to Make Array Unique/Minimum Increment to Make Array Unique.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Increment_to_Make_Array_Unique;
+
+public class Minimum_Increment_to_Make_Array_Unique
+{
+    public int MinIncrementForUnique(int[] nums, int prev = int.MinValue) =>
+        nums.Order().Sum(num => (prev = Math.Max(num, prev + 1)) - num);
+}
