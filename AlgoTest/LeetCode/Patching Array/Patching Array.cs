5312e17bc36b509ab91a6dc897e490f81072c8e0
Mateusz Kopij <sagasu@gmail.com>
2024-06-16 07:15:15 +0100

    Patching Array

---
+++ AlgoTest/LeetCode/Patching Array/Patching Array.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgoTest.LeetCode.Patching_Array;
+
+public class Patching_Array
+{
+    public int MinPatches(int[] nums, int n) 
+    {
+        var miss = 1l;
+        var result = 0;
+        var i = 0;
+
+        while(miss <= n)
+        {
+            if (i < nums.Length && nums[i] <= miss)
+            {
+                miss += nums[i];
+                i++;
+            }
+            else
+            {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
