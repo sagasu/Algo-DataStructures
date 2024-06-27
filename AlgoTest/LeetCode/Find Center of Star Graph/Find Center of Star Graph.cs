5700c4deb5432f3712d74406b205af9fa9061644
Mateusz Kopij <sagasu@gmail.com>
2024-06-27 18:55:36 +0100

    Find Center of Star Graph

---
+++ AlgoTest/LeetCode/Find Center of Star Graph/Find Center of Star Graph.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgoTest.LeetCode.Find_Center_of_Star_Graph;
+
+public class Find_Center_of_Star_Graph
+{
+    public int FindCenter(int[][] edges)
+    {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+
+        return edges[0][1];
+    }
+}
