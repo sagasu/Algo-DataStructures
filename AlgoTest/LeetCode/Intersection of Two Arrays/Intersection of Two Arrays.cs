c064c6d491feacbd773f9d46672c2ded6e45882a
Mateusz Kopij <sagasu@gmail.com>
2024-07-02 20:07:15 +0100

    Intersection of Two Arrays

---
+++ AlgoTest/LeetCode/Intersection of Two Arrays/Intersection of Two Arrays.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Intersection_of_Two_Arrays;
+
+public class Intersection_of_Two_Arrays
+{
+    public int[] Intersection(int[] nums1, int[] nums2) => nums1.Intersect(nums2).Distinct().ToArray();
+}
