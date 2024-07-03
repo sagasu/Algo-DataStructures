c673135de27a74c6619e31ff277bde975611b1b7
Mateusz Kopij <sagasu@gmail.com>
2024-07-03 16:54:54 +0100

    Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ AlgoTest/LeetCode/Minimum Difference Between Largest and Smallest Value in Three Moves/Minimum Difference Between Largest and Smallest Value in Three Moves.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves;
+
+public class Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves
+{
+    public int MinDifference(int[] nums) {
+        if (nums.Length < 5) return 0;
+
+        Array.Sort(nums);
+
+        return Enumerable.Range(0, 4)
+            .Select(x => nums.Skip(x).Take(nums.Length - 3).ToArray())
+            .Select(x => x[nums.Length - 4] - x[0])
+            .Min();
+    }
+}
