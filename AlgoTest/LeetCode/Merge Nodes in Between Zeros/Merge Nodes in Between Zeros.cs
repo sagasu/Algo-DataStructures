8f802c4a81b2fbce8cd38e1b32e3ec255f6b198d
Mateusz Kopij <sagasu@gmail.com>
2024-07-04 20:07:42 +0100

    Merge_Nodes_in_Between_Zeros

---
+++ AlgoTest/LeetCode/Merge Nodes in Between Zeros/Merge Nodes in Between Zeros.cs
@@ -0,0 +1,28 @@
+﻿using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Merge_Nodes_in_Between_Zeros;
+
+public class Merge_Nodes_in_Between_Zeros
+{
+    public ListNode MergeNodes(ListNode head)
+    {
+        var sum = 0;
+        var tail = head;
+        var curr = head.next;
+
+        while (curr != null)
+        {
+            if (curr.val == 0)
+            {
+                tail.next = new(sum);
+                tail = tail.next;
+                sum = 0;
+            }
+
+            sum += curr.val;
+            curr = curr.next;
+        }
+
+        return head.next;
+    }
+}
