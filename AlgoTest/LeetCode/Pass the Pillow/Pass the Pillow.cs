b8b319a34f674da3aacfd788272a77f2c1876105
Mateusz Kopij <sagasu@gmail.com>
2024-07-06 18:48:31 +0100

    Pass the Pillow

---
+++ AlgoTest/LeetCode/Pass the Pillow/Pass the Pillow.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoTest.LeetCode.Pass_the_Pillow;
+
+public class Pass_the_Pillow
+{
+    public int PassThePillow(int n, int time) {
+        var div = time/(n-1);
+        var mod = time%(n-1);
+        return div%2 == 0 ? 1+mod : n-mod;
+    }
+}
