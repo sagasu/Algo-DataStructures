6deaf94d5f792b6dd960aa6c76fecdd951cfd2ec
Mateusz Kopij <sagasu@gmail.com>
2024-07-07 09:44:25 +0100

    Water Bottles

---
+++ AlgoTest/LeetCode/Water Bottles/Water Bottles.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgoTest.LeetCode.Water_Bottles;
+
+public class Water_Bottles
+{
+    public int NumWaterBottles(int numBottles, int numExchange) {
+        var result = numBottles;
+        
+        while(numBottles >= numExchange)
+        {
+            var tmp = numBottles / numExchange;
+            result += tmp ;
+            numBottles = tmp + numBottles % numExchange;
+        }
+        return result;
+    }
+}
