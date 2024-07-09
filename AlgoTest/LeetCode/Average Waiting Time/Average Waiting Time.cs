a4fec9024d21d70d11f38139082a495d2d9b4827
Mateusz Kopij <sagasu@gmail.com>
2024-07-09 19:44:06 +0100

    Average Waiting Time

---
+++ AlgoTest/LeetCode/Average Waiting Time/Average Waiting Time.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Average_Waiting_Time;
+
+public class Average_Waiting_Time
+{
+    public double AverageWaitingTime(int[][] customers) => customers
+        .Aggregate(
+            (currTime: 0.0, totalWait: 0.0),
+            (acc, next) => (
+                acc.currTime = Math.Max(acc.currTime, next[0]) + next[1],
+                acc.totalWait + acc.currTime - next[0]
+            ),
+            acc => acc.totalWait / customers.Length
+        );
+}
