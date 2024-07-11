4223d271cd5d6a29487a9aee8d9d333925bd6025
Mateusz Kopij <sagasu@gmail.com>
2024-07-11 19:24:32 +0100

    Reverse Substrings Between Each Pair of Parentheses

---
+++ AlgoTest/LeetCode/Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Reverse_Substrings_Between_Each_Pair_of_Parentheses;
+
+public class Reverse_Substrings_Between_Each_Pair_of_Parentheses
+{
+    public string ReverseParentheses(string s) {
+        var result = "";
+        var stack = new Stack<int>();
+        
+        foreach (var current in s)
+        {
+            if (current != '(' && current != ')')
+                result += current;
+            else switch (current)
+            {
+                case '(':
+                    stack.Push(result.Length);
+                    break;
+                case ')':
+                {
+                    var start = stack.Pop();
+                    var length = ((result.Length) - 1) - start + 1;
+                    var reversed = new string(result.Substring(start, length).Reverse().ToArray());
+                    result = result.Remove(start, length);
+                    result += reversed;
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
