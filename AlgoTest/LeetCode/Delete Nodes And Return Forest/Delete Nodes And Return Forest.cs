250808f755d59f0fed69816de6ded9235d415aef
Mateusz Kopij <sagasu@gmail.com>
2024-07-17 22:07:27 +0100

    Delete_Nodes_And_Return_Forest

---
+++ AlgoTest/LeetCode/Delete Nodes And Return Forest/Delete Nodes And Return Forest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Delete_Nodes_And_Return_Forest;
+
+public class Delete_Nodes_And_Return_Forest
+{
+    public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete) {
+        var h = new HashSet<int>(to_delete);        
+        var ret = new List<TreeNode>();
+        root = Del(root, h, ret);
+        if(root!=null)
+            ret.Add(root);
+        return ret;
+    }
+    
+    TreeNode Del(TreeNode root, HashSet<int> h, List<TreeNode> ret){
+        if(root==null) return null;
+            
+        root.left = Del(root.left, h, ret);
+        root.right = Del(root.right, h, ret);
+        
+        if(h.Contains(root.val)){
+            if(root.left!=null) ret.Add(root.left);
+            if(root.right!=null) ret.Add(root.right);
+            return null;
+        }
+        
+        return root;
+    }
+}
