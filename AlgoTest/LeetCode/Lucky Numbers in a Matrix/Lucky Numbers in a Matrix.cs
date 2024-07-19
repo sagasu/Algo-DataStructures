a702417803be8eb341ef85a7b6ae77ec492f7b57
Mateusz Kopij <sagasu@gmail.com>
2024-07-19 15:47:32 +0100

    Lucky Numbers in a Matrix

---
+++ AlgoTest/LeetCode/Lucky Numbers in a Matrix/Lucky Numbers in a Matrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Lucky_Numbers_in_a_Matrix;
+
+public class Lucky_Numbers_in_a_Matrix
+{
+    public IList<int> LuckyNumbers (int[][] matrix) {
+        List<int> result = new List<int>();
+        int column = matrix.Length;
+        int row = matrix[0].Length;
+
+        int[] rowsMinIndexes = new int[column];
+        for (int i = 0; i < column; i++)
+        {
+            int min = Int32.MaxValue;
+            for (int j = 0; j < row; j++)
+            {
+                int val = matrix[i][j];
+                if (val < min) 
+                { 
+                    rowsMinIndexes[i] = j;
+                    min = val;
+                }
+            }
+        }
+
+        for (int i = 0; i < rowsMinIndexes.Length; i++)
+        {
+            int minIndex = rowsMinIndexes[i];
+            int maxVal = matrix[i][minIndex];
+            for (int j = 0; j < column; j++)
+            {
+                if (matrix[j][minIndex] >= maxVal)
+                {
+                    maxVal = matrix[j][minIndex];
+                }
+            }
+            if (maxVal == matrix[i][minIndex]) { result.Add(maxVal); }
+        }
+        return result;
+    }
+}
