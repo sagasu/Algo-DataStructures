5e76e47944eb704df3528f6ead3f6e612583d3ad
Mateusz Kopij <sagasu@gmail.com>
2024-07-24 16:57:10 +0100

    Sort the Jumbled Numbers

---
+++ AlgoTest/LeetCode/Sort the Jumbled Numbers/Sort the Jumbled Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Sort_the_Jumbled_Numbers;
+
+public class Sort_the_Jumbled_Numbers
+{
+    public int[] SortJumbled(int[] mapping, int[] nums) => nums
+        .Select((int original) => {
+            if (original < 10)
+                return (original, mapping[original]);
+            
+            var mapped = 0;
+            for (int pow = 1, temp = original; temp > 0; pow *= 10, temp /= 10)
+                mapped += pow * mapping[(temp % 10)];
+            
+            return (original, mapped);
+        })
+        .OrderBy(((int original, int mapped) s) => s.mapped)
+        .Select(((int original, int mapped) s) => s.original)
+        .ToArray();
+}
