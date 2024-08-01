fa80143cc2571e02db0cf08734e94e374e1ce611
Mateusz Kopij <sagasu@gmail.com>
2024-08-01 18:47:14 +0100

    Number of Senior Citizens

---
+++ AlgoTest/LeetCode/Number of Senior Citizens/Number of Senior Citizens.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Number_of_Senior_Citizens;
+
+public class Number_of_Senior_Citizens
+{
+    public int CountSeniors(string[] details) => details.Count(detail => int.Parse(detail[^4..^2]) > 60);
+}
