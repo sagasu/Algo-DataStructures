bc20ee0f513e900544e7351fcca4c33d346287ad
Mateusz Kopij <sagasu@gmail.com>
2024-08-04 20:07:47 +0100

    Make_Two_Arrays_Equal_by_Reversing_Subarrays

---
+++ AlgoTest/LeetCode/Make Two Arrays Equal by Reversing Subarrays/Make Two Arrays Equal by Reversing Subarrays.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Make_Two_Arrays_Equal_by_Reversing_Subarrays;
+
+public class Make_Two_Arrays_Equal_by_Reversing_Subarrays
+{
+    public bool CanBeEqual(int[] target, int[] arr) {
+        Array.Sort(target);
+        Array.Sort(arr);
+        return !target.Where((t, i) => t != arr[i]).Any();
+    }
+}
