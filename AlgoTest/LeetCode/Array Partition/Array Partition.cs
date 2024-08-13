fde272caa521d0b286643d6fc158164c5866507c
Mateusz Kopij <sagasu@gmail.com>
2024-08-13 18:42:59 +0100

    Array Partition

---
+++ AlgoTest/LeetCode/Array Partition/Array Partition.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Array_Partition;
+
+public class Array_Partition
+{
+    public int ArrayPairSum(int[] nums)
+    {
+        var result = 0;
+        var sortednums = nums.OrderBy(x => x).ToArray();
+        for(var i = 0; i < sortednums.Length; i += 2)
+            result += sortednums[i];
+        
+        return result;
+    }
+}
