bbf79e2ff7ded0339e5bbf37ee0938d6dc848ef1
Mateusz Kopij <sagasu@gmail.com>
2024-08-16 16:24:46 +0100

    Maximum Distance in Arrays

---
+++ AlgoTest/LeetCode/Maximum Distance in Arrays/Maximum Distance in Arrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Maximum_Distance_in_Arrays;
+
+public class Maximum_Distance_in_Arrays
+{
+    public int MaxDistance(IList<IList<int>> arrays) 
+    {
+        var minMaxes = arrays.Select((x, i) => (i, x.Min(), x.Max())).ToArray();
+
+        var min0 = minMaxes.MinBy(x => x.Item2);
+        var max0 = minMaxes.Where(x => x.Item1 != min0.Item1).MaxBy(x => x.Item3);
+        var d0 = max0.Item3 - min0.Item2;
+
+        var max1 = minMaxes.MaxBy(x => x.Item3);
+        var min1 = minMaxes.Where(x => x.Item1 != max1.Item1).MinBy(x => x.Item2);
+        var d1 = max1.Item3 - min1.Item2;
+        
+        return Math.Max(d0, d1);
+    }
+}
