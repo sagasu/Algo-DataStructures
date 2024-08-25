5863558a8b80dfa7dd4c2860c11e37ede4e69de3
Mateusz Kopij <sagasu@gmail.com>
2024-08-25 18:47:13 +0100

    N_ary_Tree_Postorder_Traversal

---
+++ AlgoTest/LeetCode/N-ary Tree Postorder Traversal/N-ary Tree Postorder Traversal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.N_ary_Tree_Postorder_Traversal;
+
+public class N_ary_Tree_Postorder_Traversal
+{
+    public class Node {
+        public int val;
+        public IList<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public IList<int> Postorder(Node root) {
+        if (root == null) return new List<int>();
+        var result = new List<int> { root.val };
+        return root.children == null
+            ? result
+            : root.children.SelectMany(Postorder).Concat(result).ToList();
+    }
+}
