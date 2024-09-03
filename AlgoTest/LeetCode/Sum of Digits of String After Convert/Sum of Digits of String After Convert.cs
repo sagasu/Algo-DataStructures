b2586673ca6b74fd4a2a97fea79beac1e9ec83f9
Mateusz Kopij <sagasu@gmail.com>
2024-09-03 20:05:26 +0100

    Sum of Digits of String After Convert

---
+++ AlgoTest/LeetCode/Sum of Digits of String After Convert/Sum of Digits of String After Convert.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Sum_of_Digits_of_String_After_Convert;
+
+public class Sum_of_Digits_of_String_After_Convert
+{
+    public int GetLucky(string s, int k) => s
+        .Aggregate(
+            0,
+            (acc, next) => char.IsLetter(next) ?
+                acc + (next - 'a' + 1) / 10 + (next - 'a' + 1) % 10 :
+                acc + next - '0',
+            acc => k == 1 ? acc : GetLucky($"{acc}", k - 1)
+        );
+}
