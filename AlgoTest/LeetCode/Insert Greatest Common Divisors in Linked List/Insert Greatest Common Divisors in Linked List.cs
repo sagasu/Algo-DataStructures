ee6e2d5279511831dcd9fa5163e5f451f3cc73e9
Mateusz Kopij <sagasu@gmail.com>
2024-09-10 20:21:41 +0100

    Insert Greatest Common Divisors in Linked List

---
+++ AlgoTest/LeetCode/Insert Greatest Common Divisors in Linked List/Insert Greatest Common Divisors in Linked List.cs
@@ -0,0 +1,19 @@
+﻿using AlgoTest.LeetCode.RotateList;
+
+namespace AlgoTest.LeetCode.Insert_Greatest_Common_Divisors_in_Linked_List;
+
+public class Insert_Greatest_Common_Divisors_in_Linked_List
+{
+    public ListNode InsertGreatestCommonDivisors(ListNode head) {
+        var cur=head;
+        while(cur.next!=null){
+            var newNode=new ListNode(GCD(cur.val,cur.next.val));
+            newNode.next=cur.next;
+            cur.next=newNode;
+            cur=cur.next.next;
+        }
+        return head;
+    }
+    int GCD(int a,int b) => b==0 ? a : GCD(b,a%b);
+    
+}
