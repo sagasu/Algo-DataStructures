787b36ff92e113c9482ffd83bf37afc8226fdfe3
Mateusz Kopij <sagasu@gmail.com>
2024-09-11 20:21:10 +0100

    Minimum_Bit_Flips_to_Convert_Number

---
+++ AlgoTest/LeetCode/Minimum Bit Flips to Convert Number/Minimum Bit Flips to Convert Number.cs
@@ -0,0 +1,6 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Bit_Flips_to_Convert_Number;
+
+public class Minimum_Bit_Flips_to_Convert_Number
+{
+    public int MinBitFlips(int x, int y) => int.PopCount(x ^ y);
+}
