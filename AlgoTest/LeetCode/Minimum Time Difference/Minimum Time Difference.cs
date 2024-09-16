f01f6b612d859f2ffeb95e256780204538e86ac4
Mateusz Kopij <sagasu@gmail.com>
2024-09-16 15:20:28 +0100

    Minimum Time Difference

---
+++ AlgoTest/LeetCode/Minimum Time Difference/Minimum Time Difference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Time_Difference;
+
+public class Minimum_Time_Difference
+{
+    public int FindMinDifference(IList<string> timePoints)
+    {
+        var times = timePoints.Select(TimeOnly.Parse).Order().ToArray();
+        var differences = times.Prepend(times[^1]).Zip(times, (a, b) => b - a);
+        return (int)differences.Min().TotalMinutes;
+    }
+}
