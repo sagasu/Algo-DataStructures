5bdd53bc17706200167bec4fbc23f39d13691d04
Mateusz Kopij <sagasu@gmail.com>
2024-09-17 21:11:33 +0100

    Uncommon Words from Two Sentences

---
+++ AlgoTest/LeetCode/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest.LeetCode.Uncommon_Words_from_Two_Sentences;
+
+[TestClass]
+public class Uncommon_Words_from_Two_Sentences
+{
+    [TestMethod]
+    public void Test() => CollectionAssert.AreEqual(new[] { "sweet", "sour" },
+        UncommonFromSentences("this apple is sweet", "this apple is sour"));
+    
+    public string[] UncommonFromSentences(string s, string t) => $"{s} {t}"
+        .Split(' ')
+        .GroupBy(w => w)
+        .Where(g => g.Count() == 1)
+        .Select(g => g.Key)
+        .ToArray();
+}
