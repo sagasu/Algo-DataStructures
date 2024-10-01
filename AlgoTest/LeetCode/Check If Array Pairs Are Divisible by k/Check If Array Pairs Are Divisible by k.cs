7ff019127bb707e0babb25f3e9542c7946934188
Mateusz Kopij <sagasu@gmail.com>
2024-10-01 19:18:41 +0100

    Check If Array Pairs Are Divisible by k

---
+++ AlgoTest/LeetCode/Check If Array Pairs Are Divisible by k/Check If Array Pairs Are Divisible by k.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgoTest.LeetCode.Check_If_Array_Pairs_Are_Divisible_by_k;
+
+public class Check_If_Array_Pairs_Are_Divisible_by_k
+{
+    public bool CanArrange(int[] numbers, int k) 
+    {
+        var remainderCount = new int[k];
+
+        foreach (var num in numbers)
+        {
+            var remainder = num % k;
+            if(remainder < 0)
+                remainder += k;
+
+            remainderCount[remainder]++;
+        }
+
+        if(remainderCount[0] % 2 != 0)
+            return false;
+
+        for(var i = 1; i < k; i++)
+            if(remainderCount[i] != remainderCount[k - i])
+                return false;
+        
+        return true;
+    }
+}
