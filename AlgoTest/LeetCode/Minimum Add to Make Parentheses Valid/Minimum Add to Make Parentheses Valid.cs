ab103925da74271762014d677129c84067d3eb66
Mateusz Kopij <sagasu@gmail.com>
2024-10-09 23:43:35 +0100

    Minimum Add to Make Parentheses Valid

---
+++ AlgoTest/LeetCode/Minimum Add to Make Parentheses Valid/Minimum Add to Make Parentheses Valid.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Add_to_Make_Parentheses_Valid;
+
+public class Minimum_Add_to_Make_Parentheses_Valid
+{
+    public int MinAddToMakeValid(string s) {
+        if(string.IsNullOrEmpty(s))
+            return 0;
+        
+        int open = 0, close = 0;
+        foreach(var c in s)
+        {
+            if(c == '(')
+                close++;
+            else if(close > 0)
+                close--;
+            else
+                open++; 
+        }
+        
+        return open + close;
+    }
+}
