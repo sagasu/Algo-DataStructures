65623027a12a40b861b58766cf5b83fe42f808ac
Mateusz Kopij <sagasu@gmail.com>
2024-10-18 23:05:36 +0100

    Count Number of Maximum Bitwise-OR Subsets

---
+++ AlgoTest/LeetCode/Count Number of Maximum Bitwise-OR Subsets/Count Number of Maximum Bitwise-OR Subsets.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoTest.LeetCode.Count_Number_of_Maximum_Bitwise_OR_Subsets;
+
+public class Count_Number_of_Maximum_Bitwise_OR_Subsets
+{
+    int sum;
+    int count;
+
+    public void Rec(int[] nums, int cur, int index){
+        cur |= nums[index];
+        if (cur == sum) count++;
+        for (int i = index+1; i < nums.Length; i++){
+            Rec(nums, cur, i);
+        }
+    }
+
+    public int CountMaxOrSubsets(int[] nums) {
+        sum = 0;
+        count = 0;
+        foreach (int num in nums) sum |= num;
+        for (int i = 0; i < nums.Length; i++) Rec(nums, 0, i);
+        return count;
+    }
+}
