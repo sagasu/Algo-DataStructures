b4eaf975eb0f8462e16eaa325553418d34a7fac4
Mateusz Kopij <sagasu@gmail.com>
2024-10-22 18:57:29 +0100

    Kth Largest Sum in a Binary Tree

---
+++ AlgoTest/LeetCode/Kth Largest Sum in a Binary Tree/Kth Largest Sum in a Binary Tree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Kth_Largest_Sum_in_a_Binary_Tree;
+
+public class Kth_Largest_Sum_in_a_Binary_Tree
+{
+    public long KthLargestLevelSum(TreeNode root, int k) {
+        var list = new List<long>();
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+
+        while(queue.Count > 0) {
+            long sum = 0;
+            var count = queue.Count;
+            for(int i = 0; i < count; i++) {
+                var node = queue.Dequeue();
+                sum += node.val;
+
+                if(node.left != null)
+                    queue.Enqueue(node.left);
+
+                if(node.right != null)
+                    queue.Enqueue(node.right);
+            }
+
+            list.Add(sum);
+        }
+
+        list.Sort();
+        return list.Count < k ? -1 : list[^k];
+    }
+}
