9f89fc09771a9a3b9d6404dec35e1b066e613c44
Mateusz Kopij <sagasu@gmail.com>
2024-11-01 22:51:19 +0000

    Delete Characters to Make Fancy String

---
+++ AlgoTest/LeetCode/Delete Characters to Make Fancy String/Delete Characters to Make Fancy String.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace AlgoTest.LeetCode.Delete_Characters_to_Make_Fancy_String;
+
+public class Delete_Characters_to_Make_Fancy_String
+{
+    public string MakeFancyString(string s)
+    {
+        StringBuilder stringBuilder = new();
+        for (int index = 0; index < s.Length; index++)
+            if (index < 2 || s[index - 1] != s[index] || s[index - 2] != s[index])
+                stringBuilder.Append(s[index]);
+
+        return stringBuilder.ToString();
+    }
+}
