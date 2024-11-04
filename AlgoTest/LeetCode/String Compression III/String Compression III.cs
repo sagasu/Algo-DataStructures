259f59063e6a060d4c38a36094e1a0a17dd7b21e
Mateusz Kopij <sagasu@gmail.com>
2024-11-04 19:22:11 +0000

    String Compression III

---
+++ AlgoTest/LeetCode/String Compression III/String Compression III.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace AlgoTest.LeetCode.String_Compression_III;
+
+public class String_Compression_III
+{
+    public string CompressedString(string s) => s
+        .Aggregate(
+            new StringBuilder(),
+            (acc, next) => acc.Length == 0 || acc[^1] != next || acc[^2] == '9' ?
+                acc.Append(1).Append(next) :
+                acc.Replace(acc[^2], (char)(acc[^2] + 1), acc.Length - 2, 1),
+            acc => acc.ToString()
+        );
+}
