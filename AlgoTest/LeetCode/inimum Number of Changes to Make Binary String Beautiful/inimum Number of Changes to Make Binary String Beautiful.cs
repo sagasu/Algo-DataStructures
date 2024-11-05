3350ccc0773b0752b594c9b65782750412c501c0
Mateusz Kopij <sagasu@gmail.com>
2024-11-05 19:22:17 +0000

    Minimum Number of Changes to Make Binary String Beautiful

---
+++ AlgoTest/LeetCode/inimum Number of Changes to Make Binary String Beautiful/inimum Number of Changes to Make Binary String Beautiful.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.inimum_Number_of_Changes_to_Make_Binary_String_Beautiful;
+
+public class inimum_Number_of_Changes_to_Make_Binary_String_Beautiful
+{
+    public int MinChanges(string s) => s.Chunk(2).Sum(x => x[0] ^ x[1]);
+}
