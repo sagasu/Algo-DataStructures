237b73953266991e79708ee1db6c699333d7a8d5
Mateusz Kopij <sagasu@gmail.com>
2024-11-14 18:22:35 +0000

    Minimized_Maximum_of_Products_Distributed_to_Any_Store

---
+++ AlgoTest/LeetCode/Minimized Maximum of Products Distributed to Any Store/Minimized Maximum of Products Distributed to Any Store.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimized_Maximum_of_Products_Distributed_to_Any_Store;
+
+public class Minimized_Maximum_of_Products_Distributed_to_Any_Store
+{
+    public int MinimizedMaximum(int n, int[] quantities)
+        {
+            var left = 1;
+            var right = quantities.Max();
+    
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                var stores = quantities.Sum(q => (q + mid - 1) / mid);
+                if (stores <= n)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+    
+            return left;
+        }
+}
