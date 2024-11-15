5ec00eb5e601ec2821ebed1fc37b6f8fb472d04e
Mateusz Kopij <sagasu@gmail.com>
2024-11-15 19:26:51 +0000

    Shortest Subarray to be Removed to Make Array Sorted

---
+++ AlgoTest/LeetCode/Shortest Subarray to be Removed to Make Array Sorted/Shortest Subarray to be Removed to Make Array Sorted.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Shortest_Subarray_to_be_Removed_to_Make_Array_Sorted;
+
+public class Shortest_Subarray_to_be_Removed_to_Make_Array_Sorted
+{
+    public int FindLengthOfShortestSubarray(int[] arr) {
+        var n=arr.Length;
+        var r=n-1;
+        
+        while(r>0&&arr[r-1]<=arr[r])
+            r--;
+        
+        var ans=r;
+        var l=0;
+        
+        while(l<r&&(l==0 || arr[l-1]<=arr[l]))
+        {
+            while(r<n&&arr[r]<arr[l]) r++;
+            ans=Math.Min(ans,r-l-1);
+            l++;
+        }
+        return ans;
+    }
+}
