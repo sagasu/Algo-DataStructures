1ded9536e4e3d8c1c0fd12bf20469e7cf3e39b0a
Mateusz Kopij <sagasu@gmail.com>
2024-11-20 19:25:12 +0000

    Minimum_Array_End

--- AlgoTest/LeetCode/Minimum Array End/Minimum Array End.cs
+++ AlgoTest/LeetCode/Minimum Array End/Minimum Array End.cs
@@ -5,7 +5,7 @@ public class Minimum_Array_End
     public long MinEnd(int n, int x) {
         n--;
         long result=x, mask=1;
-        for(int nbit=0; nbit<30; mask<<=1)
+        for(var nbit=0; nbit<30; mask<<=1)
             if((result & mask)==0 && (n & 1<<nbit++)!=0)
                 result |= mask;
         return result;
