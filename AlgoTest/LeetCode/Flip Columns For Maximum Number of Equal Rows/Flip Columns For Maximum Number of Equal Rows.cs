34bfacc101e5f50138c667956fc162794622f282
Mateusz Kopij <sagasu@gmail.com>
2024-11-22 20:19:40 +0000

    Flip Columns For Maximum Number of Equal Rows

---
+++ AlgoTest/LeetCode/Flip Columns For Maximum Number of Equal Rows/Flip Columns For Maximum Number of Equal Rows.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Flip_Columns_For_Maximum_Number_of_Equal_Rows;
+
+public class Flip_Columns_For_Maximum_Number_of_Equal_Rows
+{
+    public int MaxEqualRowsAfterFlips(int[][] M) => M
+        .GroupBy(r => string.Concat(
+            r.Select(c => r[0] ^ c)
+        ))
+        .Max(g => g.Count());
+}
