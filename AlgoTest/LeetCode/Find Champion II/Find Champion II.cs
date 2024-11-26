60a9872fd35a6a8071deb5c979582cb625250d20
Mateusz Kopij <sagasu@gmail.com>
2024-11-26 23:12:03 +0000

    Find Champion II

---
+++ AlgoTest/LeetCode/Find Champion II/Find Champion II.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_Champion_II;
+
+public class Find_Champion_II
+{
+    public int FindChampion(int n, int[][] edges) {
+        var result = Enumerable
+            .Range(0, n)
+            .Except(edges.Select(edge => edge[1]))
+            .ToList();
+
+        return result.Count == 1 ? result[0] : -1;            
+    }
+}
