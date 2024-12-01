b89008d1dc0f598297cb953f5862bbd1dc3bc81b
Mateusz Kopij <sagasu@gmail.com>
2024-12-01 21:38:17 +0000

    Check If N and Its Double Exist

---
+++ AlgoTest/LeetCode/Check If N and Its Double Exist/Check If N and Its Double Exist.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Check_If_N_and_Its_Double_Exist;
+
+public class Check_If_N_and_Its_Double_Exist
+{
+    public bool CheckIfExist(int[] arr) => arr.Count(i => i == 0) > 1 || arr.Where(i => i != 0).Any(x => arr.Contains(x * 2));
+}
