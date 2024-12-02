ea6f346e8db167a6a50c81fd19a380c5ba3f83b1
Mateusz Kopij <sagasu@gmail.com>
2024-12-02 18:34:46 +0000

    Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ AlgoTest/LeetCode/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Check If a Word Occurs As a Prefix of Any Word in a Sentence.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence;
+
+public class Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence
+{
+    public int IsPrefixOfWord(string sentence, string searchWord) 
+    {
+        var words = sentence.Split(' ').ToList();
+        for(var i = 0; i < words.Count; i++)
+            if(words[i].StartsWith(searchWord))
+                return i + 1;
+        return -1;
+    }
+}
