0fe5e01d8955ea331199953bc6feb459f062a65a
Mateusz Kopij <sagasu@gmail.com>
2024-12-05 20:37:22 +0000

    Move Pieces to Obtain a String

---
+++ AlgoTest/LeetCode/Move Pieces to Obtain a String/Move Pieces to Obtain a String.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoTest.LeetCode.Move_Pieces_to_Obtain_a_String;
+
+public class Move_Pieces_to_Obtain_a_String
+{
+    public bool CanChange(string start, string target) {
+        int l = 0, r = 0, n = start.Length;
+        while(l < n || r < n){
+            while(l < n && start[l] == '_')l++;
+            while(r < n && target[r] == '_')r++;
+            if(l == n || r == n)break;
+            if( start[l] != target[r] || 
+                (l > r && start[l] == 'R') || (r > l && start[l] == 'L')) 
+                return false;
+            l++;
+            r++;
+        }
+        while(l < n && start[l] == '_')l++;
+        while(r < n && target[r] == '_')r++;
+        return l == n && r == n;
+    }
+}
