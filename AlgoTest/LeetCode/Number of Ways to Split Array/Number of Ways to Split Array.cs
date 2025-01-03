478f1098577caf699ba0eee13b823e151b6cddbc
Mateusz Kopij <sagasu@gmail.com>
2025-01-03 15:44:12 +0000

    Number of Ways to Split Array

---
+++ AlgoTest/LeetCode/Number of Ways to Split Array/Number of Ways to Split Array.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Number_of_Ways_to_Split_Array;
+
+public class Number_of_Ways_to_Split_Array
+{
+    public int WaysToSplitArray(int[] nums) {
+        long leftSum = 0;
+        var rightSum = nums.Sum(x => (long)x);
+        var count = 0;
+
+        for(int i = 0; i < nums.Length-1; i++)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            if(leftSum >= rightSum)
+                ++count;
+        }
+
+        return count;
+    }
+}
