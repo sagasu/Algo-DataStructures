6c1890359e9b76029ff2ca4de37e62602add65a0
Mateusz Kopij <sagasu@gmail.com>
2025-01-07 21:22:45 +0000

    String Matching in an Array

---
+++ AlgoTest/LeetCode/String Matching in an Array/String Matching in an Array.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.String_Matching_in_an_Array;
+
+public class String_Matching_in_an_Array
+{
+    public IList<string> StringMatching(string[] words) => words.Where(w => words.Any(a => a.Contains(w) && a.Length > w.Length)).ToList();
+    
+}
