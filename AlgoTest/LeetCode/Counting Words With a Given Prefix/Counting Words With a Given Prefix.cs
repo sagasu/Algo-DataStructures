1b25b995b08ae3ec8d049e2db0c8b6368d0276f5
Mateusz Kopij <sagasu@gmail.com>
2025-01-09 23:28:29 +0000

    Counting Words With a Given Prefix

---
+++ AlgoTest/LeetCode/Counting Words With a Given Prefix/Counting Words With a Given Prefix.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Counting_Words_With_a_Given_Prefix;
+
+public class Counting_Words_With_a_Given_Prefix
+{
+    public class Solution
+    {
+        public int PrefixCount(string[] words, string pref) => words.Count(word => IsPrefix(pref, word));
+
+        static bool IsPrefix(string w1, string w2)
+        {
+            if (w1.Length > w2.Length) return false;
+
+            return !w1.Where((t, i) => t != w2[i]).Any();
+        }
+    }
+}
