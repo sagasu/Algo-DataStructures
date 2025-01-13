47862e805f195be050761e3642ef2ee93ec0b846
Mateusz Kopij <sagasu@gmail.com>
2025-01-13 16:30:01 +0000

    Minimum Length of String After Operations

---
+++ AlgoTest/LeetCode/Minimum Length of String After Operations/Minimum Length of String After Operations.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Length_of_String_After_Operations;
+
+public class Minimum_Length_of_String_After_Operations
+{
+    public int MinimumLength(string s) {
+        var charFrequency = new int[26];
+        var totalLength = 0;
+
+        foreach(char currentChar in s.ToCharArray()) 
+            charFrequency[currentChar - 'a']++;
+
+        foreach (int frequency in charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) 
+                totalLength += 2;
+            else 
+                totalLength += 1;
+        }
+
+        return totalLength;
+    }
+}
