450d17fd93c66b57f1bbe246b022770eb08c6cad
Mateusz Kopij <sagasu@gmail.com>
2025-01-14 19:49:57 +0000

    Find the Prefix Common Array of Two Arrays

---
+++ AlgoTest/LeetCode/Find the Prefix Common Array of Two Arrays/Find the Prefix Common Array of Two Arrays.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoTest.LeetCode.Find_the_Prefix_Common_Array_of_Two_Arrays;
+
+public class Find_the_Prefix_Common_Array_of_Two_Arrays
+{
+    public int[] FindThePrefixCommonArray(int[] A, int[] B) {
+        var n = A.Length;
+        var prefixCommonArray = new int[n];
+        var frequency = new int[n + 1];
+        var commonCount = 0;
+
+        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
+            frequency[A[currentIndex]] += 1;
+            if (frequency[A[currentIndex]] == 2) ++commonCount;
+
+            frequency[B[currentIndex]] += 1;
+            if (frequency[B[currentIndex]] == 2) ++commonCount;
+
+            prefixCommonArray[currentIndex] = commonCount;
+        }
+
+        return prefixCommonArray;
+    }
+}
