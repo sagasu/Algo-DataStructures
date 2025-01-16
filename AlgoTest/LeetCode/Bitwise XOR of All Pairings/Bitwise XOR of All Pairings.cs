ee238a87ed6938f2949cfcd5311eeab6f27e1a89
Mateusz Kopij <sagasu@gmail.com>
2025-01-16 20:34:24 +0000

    Bitwise XOR of All Pairings

---
+++ AlgoTest/LeetCode/Bitwise XOR of All Pairings/Bitwise XOR of All Pairings.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Bitwise_XOR_of_All_Pairings;
+
+public class Bitwise_XOR_of_All_Pairings
+{
+    public int XorAllNums(int[] nums1, int[] nums2) {
+        var n1 = nums1.Length;
+        var n2 = nums2.Length;
+
+        var n1even = (n1%2==0);
+        var n2even = (n2%2==0);
+
+        var answer = 0;
+
+        if (!n1even)
+        {
+            answer = nums2.Aggregate(answer, (current, val) => current ^ val);
+        }
+
+        if (!n2even)
+        {
+            answer = nums1.Aggregate(answer, (current, val) => current ^ val);
+        }
+
+        return answer;
+    }
+}
