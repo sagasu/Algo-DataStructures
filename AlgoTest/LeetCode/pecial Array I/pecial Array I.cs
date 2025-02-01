54bd79bf2134aab51f40f11db9493209f81f1f1d
Mateusz Kopij <sagasu@gmail.com>
2025-02-01 20:43:57 +0000

    pecial_Array_I

---
+++ AlgoTest/LeetCode/pecial Array I/pecial Array I.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.pecial_Array_I;
+
+public class pecial_Array_I
+{
+    public bool IsArraySpecial(int[] nums) => nums.Zip(nums.Skip(1), (a, b) => (a % 2) != (b % 2)).All(x => x);
+}
