0db0fc4e01a290a6d5424821f23aa3114ad36364
Mateusz Kopij <sagasu@gmail.com>
2025-02-02 19:45:33 +0000

    Check_if_Array_Is_Sorted_and_Rotated

---
+++ AlgoTest/LeetCode/Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Check_if_Array_Is_Sorted_and_Rotated;
+
+public class Check_if_Array_Is_Sorted_and_Rotated
+{
+    public bool Check(int[] nums) => nums.Where((t, i) => t > nums[(i + 1) % nums.Length]).Count() <= 1;
+}
