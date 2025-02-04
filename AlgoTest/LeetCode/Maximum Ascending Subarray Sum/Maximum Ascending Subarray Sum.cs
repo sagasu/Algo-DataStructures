1430052870537f7dd8e082ac2b70baa7b77ee78c
Mateusz Kopij <sagasu@gmail.com>
2025-02-04 20:23:38 +0000

    Maximum Ascending Subarray Sum

---
+++ AlgoTest/LeetCode/Maximum Ascending Subarray Sum/Maximum Ascending Subarray Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Maximum_Ascending_Subarray_Sum;
+
+public class Maximum_Ascending_Subarray_Sum
+{
+    public int MaxAscendingSum(int[] nums) =>
+        nums.Aggregate(
+            (Max: 0, Sum: 0, Previous: 0),
+            (a, n) => n > a.Previous
+                ? (Math.Max(a.Max, a.Sum + n), a.Sum + n, n)
+                : (Math.Max(a.Max, n), n, n),
+            a => a.Max
+        );
+}
