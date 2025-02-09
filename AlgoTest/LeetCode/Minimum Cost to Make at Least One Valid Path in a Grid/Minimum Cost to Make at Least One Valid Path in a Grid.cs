026fd86c53f3d43882a70852ea4a2acc80528da1
Mateusz Kopij <sagasu@gmail.com>
2025-02-09 21:19:22 +0000

    Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid

--- AlgoTest/LeetCode/Minimum Cost to Make at Least One Valid Path in a Grid/Minimum Cost to Make at Least One Valid Path in a Grid.cs
+++ AlgoTest/LeetCode/Minimum Cost to Make at Least One Valid Path in a Grid/Minimum Cost to Make at Least One Valid Path in a Grid.cs
@@ -6,19 +6,18 @@ public class Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid
 {
     public int MinCost(int[][] grid)
         {
-            int rows = grid.Length;
-            int cols = grid[0].Length;
+            var rows = grid.Length;
+            var cols = grid[0].Length;
 
             int[][] directions = { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
 
-            bool[][] visited = new bool[rows][];
+            var visited = new bool[rows][];
             for (int row = 0; row < rows; row++)
-            {
                 visited[row] = new bool[cols];
-            }
+            
 
-            Queue<(int row, int col, int cost)> q = new Queue<(int row, int col, int cost)>();
-            Queue<(int row, int col, int cost)> stack = new Queue<(int row, int col, int cost)>();
+            var q = new Queue<(int row, int col, int cost)>();
+            var stack = new Queue<(int row, int col, int cost)>();
             q.Enqueue((0, 0, 0));
 
             while (q.Count > 0)
@@ -34,13 +33,9 @@ public class Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid
                     for (int i = 0; i < directions.Length; i++)
                     {
                         if (i == dir)
-                        {
                             addInQ(q, visited, dq.row + directions[dir][0], dq.col + directions[dir][1], dq.cost);
-                        }
                         else
-                        {
                             addInQ(stack, visited, dq.row + directions[i][0], dq.col + directions[i][1], dq.cost + 1);
-                        }
                     }
                 }
 
