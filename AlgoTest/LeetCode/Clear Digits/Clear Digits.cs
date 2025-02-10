f5df64939ca67ee952b30192ebb593426fe38f09
Mateusz Kopij <sagasu@gmail.com>
2025-02-10 20:14:06 +0000

    Clear_Digits

---
+++ AlgoTest/LeetCode/Clear Digits/Clear Digits.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Clear_Digits;
+
+public class Clear_Digits
+{
+    public string ClearDigits(string s) => s.Aggregate("", (prev, c) => Char.IsDigit(c) ? prev[..^1] : (prev + c));
+}
