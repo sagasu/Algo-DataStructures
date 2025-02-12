449779c4b20e760cb5497b479731caac62b9dadd
Mateusz Kopij <sagasu@gmail.com>
2025-02-12 21:01:59 +0000

    Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits

---
+++ AlgoTest/LeetCode/Max Sum of a Pair With Equal Sum of Digits/Max Sum of a Pair With Equal Sum of Digits.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits;
+
+public class Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits
+{
+    public int MaximumSum(int[] nums)
+    {
+        return nums.GroupBy(DigSums)
+                .Where(x => x.Count() > 1)
+                .Select(x => x.OrderDescending().Take(2).Sum())
+                .Union([-1]).Max();
+    }
+
+    public int DigSums(int num)
+    {
+        int ans = 0;
+        while (num > 0)
+        {
+            ans += num % 10;
+            num /= 10;
+        }
+        return ans;
+    }
+}
