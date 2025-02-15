770d714fb8fe9750c456f0b0c18f9acc58164ef8
Mateusz Kopij <sagasu@gmail.com>
2025-02-15 22:21:47 +0000

    Find_the_Punishment_Number_of_an_Integer

---
+++ AlgoTest/LeetCode/Find the Punishment Number of an Integer/Find the Punishment Number of an Integer.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_the_Punishment_Number_of_an_Integer;
+
+public class Find_the_Punishment_Number_of_an_Integer
+{
+    public int PunishmentNumber(int n) => new[]
+        {
+            1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235,
+            297, 369, 370, 379, 414, 657, 675, 703, 756,
+            792, 909, 918, 945, 964, 990, 991, 999, 1000
+        }
+        .TakeWhile(x => x <= n)
+        .Sum(x => x * x);
+}
