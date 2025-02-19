6b0e424edbbb3e371877a7a15ff5b1abc7c9a7f0
Mateusz Kopij <sagasu@gmail.com>
2025-02-19 20:24:19 +0000

    Letter Tile Possibilities

---
+++ AlgoTest/LeetCode/Letter Tile Possibilities/Letter Tile Possibilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Letter_Tile_Possibilities;
+
+public class Letter_Tile_Possibilities
+{
+    public int NumTilePossibilities(string tiles) {
+        var len = tiles.Length;
+        var seq = new HashSet<string>();
+
+        for (int i = 0; i < len; i++) {
+            var c = tiles[i];
+            var temp = new HashSet<string>(seq);
+            temp.Add(c + "");
+
+            if (i != 0) {
+                foreach (string s in seq) {
+                    var subLen = s.Length;
+
+                    for (int j = 0; j <= subLen; j++)
+                        temp.Add(s.Insert(j, c + ""));
+                }
+            }
+
+            seq.UnionWith(temp);
+        }
+
+        return seq.Count;
+    }
+}
