51a85f26fd6ee4a609c27702c75bb841d1af1b55
Mateusz Kopij <sagasu@gmail.com>
2025-02-20 20:48:23 +0000

    FindDifferentBinaryString

---
+++ AlgoTest/LeetCode/FindDifferentBinaryString/FindDifferentBinaryString.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.FindDifferentBinaryString;
+
+public class FindDifferentBinaryStringSolution
+{
+    public string FindDifferentBinaryString(string[] nums) =>
+        string.Concat(nums.Select((n, i) => n[i] == '0' ? '1' : '0'));
+}
