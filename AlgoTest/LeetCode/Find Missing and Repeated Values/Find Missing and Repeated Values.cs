7551a296d03891ade6845b7d8f5859e50b8d3ffa
Mateusz Kopij <sagasu@gmail.com>
2025-03-06 22:22:00 +0000

    Find Missing and Repeated Values

---
+++ AlgoTest/LeetCode/Find Missing and Repeated Values/Find Missing and Repeated Values.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Find_Missing_and_Repeated_Values;
+
+public class Find_Missing_and_Repeated_Values
+{
+    public int[] FindMissingAndRepeatedValues(int[][] grid) {
+        var set = new HashSet<int>();
+        var res = new int[2];
+        int rows = grid.Length, cols = rows;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(set.Contains(grid[r][c])){
+                    res[0] = grid[r][c];
+                }
+                set.Add(grid[r][c]);
+            }
+        }
+        for(int i=1;i<=rows*rows;i++){
+            if(!set.Contains(i)){
+                res[1]=i;
+                break;
+            }
+        }
+        return res;
+    }
+}
