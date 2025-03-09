529dc2c3ddb3f8f6b0fa3bdd2bc5569fcd5a1583
Mateusz Kopij <sagasu@gmail.com>
2025-03-09 20:54:06 +0000

    NumberOfAlternatingGroups

---
+++ AlgoTest/LeetCode/Alternating Groups II/Alternating Groups II.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Alternating_Groups_II;
+
+public class Alternating_Groups_II
+{
+    public int NumberOfAlternatingGroups(int[] colors, int k) {
+        var temp = new List<int>(colors);
+        temp.AddRange(colors[..(k - 1)]);
+        
+        int count = 0;
+        int left = 0;
+        
+        for (int right = 0; right < temp.Count; right++) {
+            if (right > 0 && temp[right] == temp[right - 1]) {
+                left = right;  
+            }
+            
+            if (right - left + 1 >= k) {
+                count++; 
+            }
+        }
+        
+        return count;
+    }
+}
