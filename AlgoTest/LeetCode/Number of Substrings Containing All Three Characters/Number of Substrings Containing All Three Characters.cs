7e729f48c5957dbbc3b5ff2872c8a6aa2fa11370
Mateusz Kopij <sagasu@gmail.com>
2025-03-11 20:51:56 +0000

    Number_of_Substrings_Containing_All_Three_Characters

---
+++ AlgoTest/LeetCode/Number of Substrings Containing All Three Characters/Number of Substrings Containing All Three Characters.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoTest.LeetCode.Number_of_Substrings_Containing_All_Three_Characters;
+
+public class Number_of_Substrings_Containing_All_Three_Characters
+{
+    public int NumberOfSubstrings(string s) {
+        int[] vis = new int[3];
+        int l = 0 , r = 0 , ans = 0;
+        while(r < s.Length)
+        {
+            vis[s[r]-'a']++;
+            while(vis[0] > 0 && vis[1]> 0 && vis[2]> 0)
+            {
+                ans+= (s.Length - r);
+                vis[s[l++]-'a']--;
+            }
+            r++;
+        }
+        return ans;
+        
+    }
+}
