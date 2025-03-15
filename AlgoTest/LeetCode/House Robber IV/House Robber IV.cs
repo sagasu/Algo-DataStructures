9d9c68fd74491cab4b9e2392f6ba9143a463a7c1
Mateusz Kopij <sagasu@gmail.com>
2025-03-15 07:17:08 +0000

    House_Robber_IV

---
+++ AlgoTest/LeetCode/House Robber IV/House Robber IV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.House_Robber_IV;
+
+public class House_Robber_IV
+{
+    public int MinCapability(int[] nums, int k) {
+        int left = int.MaxValue, right = int.MinValue;
+
+        foreach (int num in nums) {
+            left = Math.Min(left, num);
+            right = Math.Max(right, num);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (CanRobAtLeastK(nums, k, mid)) {
+                right = mid; 
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return left;
+    }
+    private bool CanRobAtLeastK(int[] nums, int k, int cap) {
+        int count = 0;
+        int i = 0;
+
+        while (i < nums.Length) {
+            if (nums[i] <= cap) {
+                count++;
+                i++; 
+            }
+            i++; 
+        }
+
+        return count >= k;
+    }
+}
