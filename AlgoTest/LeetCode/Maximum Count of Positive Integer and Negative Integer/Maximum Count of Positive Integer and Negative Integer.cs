d7b64fc36be112f3b7f933ff7a1195e18c689b59
Mateusz Kopij <sagasu@gmail.com>
2025-03-17 15:53:14 +0000

    Maximum_Count_of_Positive_Integer_and_Negative_Integer

---
+++ AlgoTest/LeetCode/Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Maximum_Count_of_Positive_Integer_and_Negative_Integer;
+
+public class Maximum_Count_of_Positive_Integer_and_Negative_Integer
+{
+    public int MaximumCount(int[] nums)
+    {
+        var countNegative = 0;
+        var countPositive = 0;
+        foreach (var num in nums)
+        {
+            if (num > 0) countPositive++;
+            if (num < 0) countNegative++;
+        }
+
+        return Math.Max(countNegative, countPositive);
+    }
+}
