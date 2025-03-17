589a2bb8ca1de56b9ac19623da990de7593e8932
Mateusz Kopij <sagasu@gmail.com>
2025-03-17 16:15:38 +0000

    Maximum_Count_of_Positive_Integer_and_Negative_Integer adding unit tests

--- AlgoTest/LeetCode/Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.cs
+++ AlgoTest/LeetCode/Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgoTest.LeetCode.Maximum_Count_of_Positive_Integer_and_Negative_Integer;
 
+[TestClass]
 public class Maximum_Count_of_Positive_Integer_and_Negative_Integer
 {
+    [TestMethod]
+    public void Test() => Assert.IsTrue(MaximumCount(new int[] { -2, -1, -1, 1, 2, 3 }) == 3);
+    
+    [TestMethod]
+    public void Test1() => Assert.IsTrue(MaximumCount(new int[] { -3,-2,-1,0,0,1,2 }) == 3);
+    
+    [TestMethod]
+    public void Test2() => Assert.IsTrue(MaximumCount(new int[] { 5,20,66,1314 }) == 4);
+    
     public int MaximumCount(int[] nums)
     {
         var countNegative = 0;
