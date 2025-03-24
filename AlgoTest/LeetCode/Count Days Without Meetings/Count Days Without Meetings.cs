416fc74a92baddb91717da4abdc4d715cf01647d
Mateusz Kopij <sagasu@gmail.com>
2025-03-24 19:29:45 +0000

    Count_Days_Without_Meetings

---
+++ AlgoTest/LeetCode/Count Days Without Meetings/Count Days Without Meetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Count_Days_Without_Meetings;
+
+public class Count_Days_Without_Meetings
+{
+    public int CountDays(int days, int[][] meetings) {
+        Array.Sort(meetings, (a, b) => a[0].CompareTo(b[0]));
+
+        var mergedMeetings = new List<int[]>();
+        foreach (var meeting in meetings) {
+            if (mergedMeetings.Count == 0 || mergedMeetings[mergedMeetings.Count - 1][1] < meeting[0]) {
+                mergedMeetings.Add(meeting);
+            } else {
+                mergedMeetings[mergedMeetings.Count - 1][1] = Math.Max(mergedMeetings[mergedMeetings.Count - 1][1], meeting[1]);
+            }
+        }
+
+        long busyDays = 0;
+        foreach (var meeting in mergedMeetings) {
+            busyDays += (long)(meeting[1] - meeting[0] + 1);
+        }
+        return (int)(days - busyDays);
+    }
+}
