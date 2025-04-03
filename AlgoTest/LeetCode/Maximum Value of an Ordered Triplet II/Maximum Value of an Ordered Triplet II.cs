5d9c2b13ba35c21f572c237861b4c29c2bcaa1ea
Mateusz Kopij <sagasu@gmail.com>
2025-04-03 22:07:21 +0100

    Maximum Value of an Ordered Triplet II

---
+++ AlgoTest/LeetCode/Maximum Value of an Ordered Triplet II/Maximum Value of an Ordered Triplet II.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Maximum_Value_of_an_Ordered_Triplet_II;
+
+public class Maximum_Value_of_an_Ordered_Triplet_II
+{
+    public long MaximumTripletValue(int[] nums)
+    {
+        long max = 0;
+        long min = 0;
+        long result = 0;
+        int n = nums.Length;
+        for (int i = 0; i < n ; i++)
+        {
+            result = Math.Max(result, min * nums[i]);
+            min    = Math.Max(min, max - nums[i]);
+            max = Math.Max(max, nums[i]);
+        }
+        return result;
+    }
+}
