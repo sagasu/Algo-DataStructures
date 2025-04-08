1b648237ed1b9342af06040baa041e19edaa7edf
Mateusz Kopij <sagasu@gmail.com>
2025-04-08 19:57:36 +0100

    MinimumOperations

---
+++ AlgoTest/LeetCode/Minimum Number of Operations to Make Elements in Array Distinct/Minimum Number of Operations to Make Elements in Array Distinct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct;
+
+public class Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct
+{
+    public int MinimumOperations(int[] nums) {
+        int operation = 0;
+        HashSet<int> set = new ();
+        for(int j = nums.Length - 1; j >= 0; j--)
+        {
+            if(!set.Contains(nums[j]))
+                set.Add(nums[j]);
+            else
+                return (j + 3) / 3; 
+        }
+        
+        return 0;
+    }
+}
