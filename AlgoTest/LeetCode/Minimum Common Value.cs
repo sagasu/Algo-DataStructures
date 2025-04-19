ce06280a3e8f3e91ba9ff9bf973d2df1a672baab
Mateusz Kopij <sagasu@gmail.com>
2025-04-19 13:44:09 +0100

    GetCommon

--- AlgoTest/LeetCode/Minimum Common Value.cs
+++ AlgoTest/LeetCode/Minimum Common Value.cs
@@ -1,12 +1,19 @@
-public class Solution {
-    public int GetCommon(int[] nums1, int[] nums2) {
+using System.Collections.Generic;
+
+public class Solution2323
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
         HashSet<int> set = new HashSet<int>(nums1);
 
-        foreach (int num in nums2) {
-            if (set.Contains(num)) {
+        foreach (int num in nums2)
+        {
+            if (set.Contains(num))
+            {
                 return num;
             }
         }
 
         return -1;
     }
+}
