6d44cb2d52e145cec3494eaf9ae944be029e4ad0
Mateusz Kopij <sagasu@gmail.com>
2025-04-20 20:55:33 +0100

    Rabbits_in_Forest

---
+++ AlgoTest/LeetCode/Rabbits in Forest/Rabbits in Forest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Rabbits_in_Forest;
+
+public class Rabbits_in_Forest
+{
+    public int NumRabbits(int[] answers) {
+        var count = new Dictionary<int, int>();
+        foreach (int a in answers) {
+            if (!count.ContainsKey(a)) count[a] = 0;
+            count[a]++;
+        }
+
+        int res = 0;
+        foreach (var kv in count)
+            res += ((kv.Value + kv.Key) / (kv.Key + 1)) * (kv.Key + 1);
+
+        return res;
+    }
+}
