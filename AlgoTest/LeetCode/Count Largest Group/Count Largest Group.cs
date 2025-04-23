8a6d042bac51db5155729345ec08751602b8bfd9
Mateusz Kopij <sagasu@gmail.com>
2025-04-23 19:22:17 +0100

    Count Largest Group

---
+++ AlgoTest/LeetCode/Count Largest Group/Count Largest Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Count_Largest_Group;
+
+public class Count_Largest_Group
+{
+    public int CountLargestGroup(int n) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0, x = i;
+            while (x > 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+            if (!map.ContainsKey(sum)) map[sum] = 0;
+            map[sum]++;
+            max = Math.Max(max, map[sum]);
+        }
+        return map.Values.Count(v => v == max);
+    }
+}
