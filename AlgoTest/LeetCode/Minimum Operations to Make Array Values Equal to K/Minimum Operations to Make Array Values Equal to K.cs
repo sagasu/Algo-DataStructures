4026a6d84b9d4759c2b6141c0bac3c09bfaee4ce
Mateusz Kopij <sagasu@gmail.com>
2025-04-26 19:30:39 +0100

    Minimum_Operations_to_Make_Array_Values_Equal_to_K

---
+++ AlgoTest/LeetCode/Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Minimum_Operations_to_Make_Array_Values_Equal_to_K;
+
+public class Minimum_Operations_to_Make_Array_Values_Equal_to_K
+{
+    public int MinOperations(int[] nums, int k) {
+        var st = new HashSet<int>();
+        foreach (int x in nums) {
+            if (x < k) {
+                return -1;
+            } else if (x > k) {
+                st.Add(x);
+            }
+        }
+        return st.Count;
+    }
+}
