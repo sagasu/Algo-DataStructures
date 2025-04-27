a5f1bb8e8071c9009de1911d562f90bdc26c4566
Mateusz Kopij <sagasu@gmail.com>
2025-04-27 08:30:35 +0100

    CountSubarrays

---
+++ AlgoTest/LeetCode/Count Subarrays of Length Three With a Condition/Count Subarrays of Length Three With a Condition.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgoTest.LeetCode.Count_Subarrays_of_Length_Three_With_a_Condition;
+
+public class Count_Subarrays_of_Length_Three_With_a_Condition
+{
+    public int CountSubarrays(int[] nums) {
+        int n = nums.Length;
+        int ans = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            if (nums[i] == (nums[i - 1] + nums[i + 1]) * 2) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
