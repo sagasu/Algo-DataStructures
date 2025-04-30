78adb8e2cd3364a91c15d077c7f6dd477b46244e
Mateusz Kopij <sagasu@gmail.com>
2025-04-30 22:42:53 +0100

    Find_Numbers_with_Even_Number_of_Digits

---
+++ AlgoTest/LeetCode/Find Numbers with Even Number of Digits/Find Numbers with Even Number of Digits.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgoTest.LeetCode.Find_Numbers_with_Even_Number_of_Digits;
+
+public class Find_Numbers_with_Even_Number_of_Digits
+{
+    public int FindNumbers(int[] nums) {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = Math.Abs(nums[i]).ToString().Length;
+                if ((num & 1) == 0) count++;
+            }
+            return count;
+        }
+}
