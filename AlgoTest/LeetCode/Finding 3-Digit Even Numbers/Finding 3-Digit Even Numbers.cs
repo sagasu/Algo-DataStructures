e965d4f0a06189729adf2e6ca58830b403b92f05
Mateusz Kopij <sagasu@gmail.com>
2025-05-12 20:20:08 +0100

    Finding_3_Digit_Even_Numbers

---
+++ AlgoTest/LeetCode/Finding 3-Digit Even Numbers/Finding 3-Digit Even Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Finding_3_Digit_Even_Numbers;
+
+public class Finding_3_Digit_Even_Numbers
+{
+    public int[] FindEvenNumbers(int[] digits)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = 100; i < 1000; i += 2)
+        {
+            int three = i / 100;
+            int two = (i / 10) % 10;
+            int one = i % 10;
+
+            List<int> temp = new List<int>(digits);
+
+            if (temp.Contains(three))
+            {
+                temp.Remove(three);
+                if (temp.Contains(two))
+                {
+                    temp.Remove(two);
+                    if (temp.Contains(one))
+                    {
+                        result.Add(i);
+                    }
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
