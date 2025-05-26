4022215845992de7977e817d042977aecf8b4298
Mateusz Kopij <sagasu@gmail.com>
2025-05-26 20:27:33 +0100

    Evaluate_the_Bracket_Pairs_of_a_String

---
+++ AlgoTest/LeetCode/Evaluate the Bracket Pairs of a String/Evaluate the Bracket Pairs of a String.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoTest.LeetCode.Evaluate_the_Bracket_Pairs_of_a_String;
+
+public class Evaluate_the_Bracket_Pairs_of_a_String
+{
+    public string Evaluate(string s, IList<IList<string>> knowledge) {
+        var d = new Dictionary<string, string>();
+        foreach (var pair in knowledge)
+        {
+            d[pair[0]] = pair[1];
+        }
+
+        var result = new StringBuilder();     
+        var k = new StringBuilder();
+        bool start = false;
+        foreach(char c in s) {
+            if (c == '(') {
+                start = true;
+                k.Clear();
+            }
+            else if (c == ')' && start) {
+                start = false;
+                string key = k.ToString();
+                if (d.TryGetValue(key, out string v))
+                {
+                    result.Append(v);
+                }
+                else
+                {
+                    result.Append('?');
+                }
+            }
+            else if (!start) {
+                result.Append(c);
+            }
+            else {
+                k.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+}
