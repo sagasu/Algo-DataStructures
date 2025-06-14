59668d1de6f46ac70e0d5407949e4c8095c211e3
Mateusz Kopij <sagasu@gmail.com>
2025-06-14 22:20:37 +0100

    Maximum_Difference_by_Remapping_a_Digit

---
+++ AlgoTest/LeetCode/Maximum Difference by Remapping a Digit/Maximum Difference by Remapping a Digit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Maximum_Difference_by_Remapping_a_Digit;
+
+public class Maximum_Difference_by_Remapping_a_Digit
+{
+    public int MinMaxDifference(int num) {
+        string s = num.ToString();
+
+        int maxVal = int.MinValue;
+        int minVal = int.MaxValue;
+
+        for (char digit = '0'; digit <= '9'; digit++) {
+            if (!s.Contains(digit)) continue;
+
+            string maxCandidate = s.Replace(digit, '9');
+            int maxNum = int.Parse(maxCandidate);
+            maxVal = Math.Max(maxVal, maxNum);
+
+            string minCandidate = s.Replace(digit, '0');
+            int minNum = int.Parse(minCandidate);
+            minVal = Math.Min(minVal, minNum);
+        }
+
+        return maxVal - minVal;
+    }
+}
