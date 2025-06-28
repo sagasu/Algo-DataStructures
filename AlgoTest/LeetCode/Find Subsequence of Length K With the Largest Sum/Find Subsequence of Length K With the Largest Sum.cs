6fa84316ff00f3db9b97106cd2ab5eadd68a59cc
Mateusz Kopij <sagasu@gmail.com>
2025-06-28 19:51:36 +0100

    MaxSubsequence

---
+++ AlgoTest/LeetCode/Find Subsequence of Length K With the Largest Sum/Find Subsequence of Length K With the Largest Sum.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_Subsequence_of_Length_K_With_the_Largest_Sum;
+
+public class Find_Subsequence_of_Length_K_With_the_Largest_Sum
+{
+    public int[] MaxSubsequence(int[] nums, int k) => nums.
+        Select((m, i) => new { m, i }).
+        OrderByDescending(m => m.m).
+        Take(k).
+        OrderBy(m => m.i).
+        Select(m => m.m).
+        ToArray();
+}
