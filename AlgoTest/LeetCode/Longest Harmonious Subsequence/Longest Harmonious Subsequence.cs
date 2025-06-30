b85919d6ba69ad010f50a6c954ee3fbbde75e9b4
Mateusz Kopij <sagasu@gmail.com>
2025-06-30 20:09:11 +0100

    Longest_Harmonious_Subsequence

---
+++ AlgoTest/LeetCode/Longest Harmonious Subsequence/Longest Harmonious Subsequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Longest_Harmonious_Subsequence;
+
+public class Longest_Harmonious_Subsequence
+{
+    public int FindLHS(int[] nums) {
+        Array.Sort(nums);
+        int max = 0;
+        int start = 0;
+ 
+        for (int end = 0; end < nums.Length; end++) {
+            while (nums[end] - nums[start] > 1) {
+                start++;
+            }
+ 
+            if (nums[end] - nums[start] == 1) {
+                int len = end - start + 1;
+                if (len > max) {
+                    max = len;
+                }
+            }
+        }
+ 
+        return max;
+    }
+}
