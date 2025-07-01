74dc71ff5221024570174ba3a0ae74b0c6670614
Mateusz Kopij <sagasu@gmail.com>
2025-07-01 20:12:08 +0100

    Find_the_Original_Typed_String_I

---
+++ AlgoTest/LeetCode/Find the Original Typed String I/Find the Original Typed String I.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_the_Original_Typed_String_I;
+
+public class Find_the_Original_Typed_String_I
+{
+    public int PossibleStringCount(string word) => Enumerable.
+        Range(0, word.Length - 1).
+        Sum(m => word[m] == word[m + 1] ? 1 : 0) + 1;
+}
