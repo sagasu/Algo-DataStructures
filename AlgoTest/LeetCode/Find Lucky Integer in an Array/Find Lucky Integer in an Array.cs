ef3e80c066a7d3b860ae0bf7d05b1eb328d16527
Mateusz Kopij <sagasu@gmail.com>
2025-07-05 23:02:47 +0100

    Find_Lucky_Integer_in_an_Array

---
+++ AlgoTest/LeetCode/Find Lucky Integer in an Array/Find Lucky Integer in an Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Find_Lucky_Integer_in_an_Array;
+
+public class Find_Lucky_Integer_in_an_Array
+{
+    public int FindLucky(int[] arr) {
+        var freq= new Dictionary<int,int>();
+        foreach (var t in arr)
+        {
+            if(freq.ContainsKey(t))
+                freq[t]+=1;
+            else
+                freq[t]=1;
+        }
+
+        return (from item in freq where item.Key == item.Value select item.Key).Prepend(-1).Max();
+    }
+}
