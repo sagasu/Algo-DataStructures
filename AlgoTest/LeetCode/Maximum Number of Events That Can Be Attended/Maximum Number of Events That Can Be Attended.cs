c463e3993e7ca794972e678ac8986d9ebf7e71a3
Mateusz Kopij <sagasu@gmail.com>
2025-07-07 21:08:41 +0100

    Maximum_Number_of_Events_That_Can_Be_Attended

---
+++ AlgoTest/LeetCode/Maximum Number of Events That Can Be Attended/Maximum Number of Events That Can Be Attended.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Maximum_Number_of_Events_That_Can_Be_Attended;
+
+public class Maximum_Number_of_Events_That_Can_Be_Attended
+{
+    public int MaxEvents(int[][] events) {
+        int n = events.Length;
+        Array.Sort(events, (a, b) => a[0].CompareTo(b[0]));
+
+        var pq = new PriorityQueue<int, int>(); 
+        int day = 0, result = 0, i = 0;
+
+        while (i < n || pq.Count > 0) {
+            if (pq.Count == 0 && day < events[i][0]) {
+                day = events[i][0];
+            }
+
+            while (i < n && events[i][0] == day) {
+                pq.Enqueue(events[i][1], events[i][1]);
+                i++;
+            }
+
+            while (pq.Count > 0 && pq.Peek() < day) {
+                pq.Dequeue();
+            }
+
+            if (pq.Count > 0) {
+                pq.Dequeue();
+                result++;
+            }
+
+            day++;
+        }
+
+        return result;
+    }
+}
