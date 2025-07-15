764328f6f36413b2f12350d3c3cb8512006a762f
Mateusz Kopij <sagasu@gmail.com>
2025-07-15 20:04:15 +0100

    Valid_Word

---
+++ AlgoTest/LeetCode/Valid Word/Valid Word.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgoTest.LeetCode.Valid_Word;
+
+public class Valid_Word
+{
+    public bool IsValid(string word) {
+        if(word.Length > 2){
+            string vowel = "aeiouAEIOU";
+            bool isV = false, isC = false;
+            foreach(char w in word){
+                if(!char.IsLetterOrDigit(w)){
+                    return false;
+                }
+                else{
+                    if(vowel.Contains(w)){
+                        isV = true;
+                    }
+                    else if(char.IsLetter(w) && !vowel.Contains(w)){
+                        isC = true;
+                    }
+                }
+            }
+            return isV && isC;
+        }
+        return false;
+    }
+}
