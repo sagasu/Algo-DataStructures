a10298a6ada0ac1b39f21d08db792fe3d129c291
Mateusz Kopij <sagasu@gmail.com>
2025-07-23 22:06:46 +0100

    Count_Words_Obtained_After_Adding_a_Letter

---
+++ AlgoTest/LeetCode/Count Words Obtained After Adding a Letter/Count Words Obtained After Adding a Letter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Count_Words_Obtained_After_Adding_a_Letter;
+
+public class Count_Words_Obtained_After_Adding_a_Letter
+{
+    public int WordCount(string[] startWords, string[] targetWords) {
+        int count = 0;
+        HashSet<string> startSet = new HashSet<string>();
+
+        foreach(string word in startWords){
+            char[] arr = word.ToCharArray();
+            Array.Sort(arr);
+            startSet.Add(new string(arr));
+        }
+
+        foreach(string word in targetWords){
+            char[] arr = word.ToCharArray();
+            Array.Sort(arr);
+            string target = new string(arr);
+
+            for(int i = 0; i < target.Length; i++){
+                string candidate = target.Remove(i,1);
+                if(startSet.Contains(candidate)){
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
