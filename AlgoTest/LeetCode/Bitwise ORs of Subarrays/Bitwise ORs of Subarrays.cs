cc5b495161ce848cdf63bc5d324469fb876561df
Mateusz Kopij <sagasu@gmail.com>
2025-07-31 22:57:08 +0100

    Bitwise_ORs_of_Subarrays

---
+++ AlgoTest/LeetCode/Bitwise ORs of Subarrays/Bitwise ORs of Subarrays.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Bitwise_ORs_of_Subarrays;
+
+public class Bitwise_ORs_of_Subarrays
+{
+    public int SubarrayBitwiseORs(int[] arr) {
+        HashSet<int> res = new(), cur = new(), next = new();
+
+        foreach (int x in arr) {
+            next.Clear();
+            next.Add(x);
+
+            foreach (int y in cur) {
+                next.Add(x | y);
+            }
+
+            foreach (int y in next) {
+                res.Add(y);
+            }
+
+            (cur, next) = (next, cur);
+        }
+
+        return res.Count;
+    }
+}
