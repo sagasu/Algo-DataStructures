dc51efe7f06c4df341ff445b710bd90ac8b22d9f
Mateusz Kopij <sagasu@gmail.com>
2025-08-12 20:28:43 +0100

    NumberOfWays

---
+++ AlgoTest/LeetCode/Ways to Express an Integer as Sum of Powers/Ways to Express an Integer as Sum of Powers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Ways_to_Express_an_Integer_as_Sum_of_Powers;
+
+public class Ways_to_Express_an_Integer_as_Sum_of_Powers
+{
+    public int NumberOfWays(int n, int x) {
+        int[] table = new int[(int) Math.Ceiling(Math.Pow(n, 1.0/x)) + 1];
+        for (int i = 1; i < table.Length; i++) {
+            table[i] = (int) Math.Pow(i, x);
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < table.Length && table[i] <= n; i++) {
+            for (int j = n - table[i]; j > 0; j--) {
+                if (dp[j] == 0) continue;
+                dp[j + table[i]] = (dp[j + table[i]] + dp[j]) % 1_000_000_007;
+            }
+            dp[table[i]]++;
+        }
+        return dp[n];
+    }
+}
