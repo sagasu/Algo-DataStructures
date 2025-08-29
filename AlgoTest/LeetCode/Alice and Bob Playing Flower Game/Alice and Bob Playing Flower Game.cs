4b8f9f49e3d8bf7e34aa1b4249f1e42a7b2e0a5f
Mateusz Kopij <sagasu@gmail.com>
2025-08-29 23:37:59 +0100

    FlowerGame

---
+++ AlgoTest/LeetCode/Alice and Bob Playing Flower Game/Alice and Bob Playing Flower Game.cs
@@ -0,0 +1,8 @@
+﻿namespace AlgoTest.LeetCode.Alice_and_Bob_Playing_Flower_Game;
+
+public class Alice_and_Bob_Playing_Flower_Game
+{
+    public long FlowerGame(int n, int m) {
+        return (long)n*m/2;
+    }
+}
