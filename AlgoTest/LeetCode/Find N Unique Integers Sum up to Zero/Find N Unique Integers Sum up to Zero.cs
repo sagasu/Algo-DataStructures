7a19bdb93dfe65f6091c954e6bf526d4ff67f59f
Mateusz Kopij <sagasu@gmail.com>
2025-09-07 21:21:05 +0100

    Find_N_Unique_Integers_Sum_up_to_Zero

---
+++ AlgoTest/LeetCode/Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Find_N_Unique_Integers_Sum_up_to_Zero;
+
+public class Find_N_Unique_Integers_Sum_up_to_Zero
+{
+    public int[] SumZero(int n) {
+        List<int> l = new List<int>();
+        int pair = n / 2 + 1;
+
+        for (int i = 1; i < pair; i++) {
+            l.Add(i);
+            l.Add(-i);
+        }
+
+        if (n % 2 != 0) {
+            l.Add(0);
+        }
+
+        return l.ToArray();
+    }
+}
