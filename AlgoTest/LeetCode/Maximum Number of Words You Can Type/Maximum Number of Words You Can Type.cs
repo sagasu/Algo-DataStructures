449aa115b468f207625b0b5d8835d14d05a72d36
Mateusz Kopij <sagasu@gmail.com>
2025-09-15 20:35:57 +0100

    Maximum_Number_of_Words_You_Can_Type

---
+++ AlgoTest/LeetCode/Maximum Number of Words You Can Type/Maximum Number of Words You Can Type.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Maximum_Number_of_Words_You_Can_Type;
+
+public class Maximum_Number_of_Words_You_Can_Type
+{
+    public int CanBeTypedWords(string text, string brokenLetters) {
+        int count=0;
+        var hs = new HashSet<char>();
+        foreach(char b in brokenLetters){
+            hs.Add(b);
+        }
+
+        string[] words = text.Split(" ");
+        foreach(string s in words){
+            if(containsNoBroken(s,hs)) count++;
+        }
+
+        return count;
+        
+    }
+    bool containsNoBroken(string s, HashSet<char> hs){
+        foreach(char c in s){
+            if(hs.Contains(c)) return false;
+        }
+        return true;
+    }
+}
