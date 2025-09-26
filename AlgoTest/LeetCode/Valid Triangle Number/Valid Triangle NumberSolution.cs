47c5e9b531ad0d75f73ac1428c5e450e1a052458
Mateusz Kopij <sagasu@gmail.com>
2025-09-26 21:34:07 +0100

    Valid_Triangle_NumberSolution

---
+++ AlgoTest/LeetCode/Valid Triangle Number/Valid Triangle NumberSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Valid_Triangle_Number;
+
+public class Valid_Triangle_NumberSolution
+{
+    public int TriangleNumber(int[] nums) {
+        Array.Sort(nums);
+        int n = nums.Length;
+        int ans = 0;
+        for (int k = n - 1; k >= 2; k--) {
+            int l = 0, r = k - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[k]) {
+                    ans += (r - l);
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+}
