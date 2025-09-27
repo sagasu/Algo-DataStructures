884d82dc3aae032b39dcf7b837ec539902b44543
Mateusz Kopij <sagasu@gmail.com>
2025-09-27 21:38:24 +0100

    Largest_Triangle_Area

---
+++ AlgoTest/LeetCode/Largest Triangle Area/Largest Triangle Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Largest_Triangle_Area;
+
+public class Largest_Triangle_Area
+{
+    public double LargestTriangleArea(int[][] points) {
+        double ans = 0;
+        for (int i = 0; i < points.Length - 2; i++)
+        for (int j = i + 1; j < points.Length - 1; j++)
+        for (int k = j + 1; k < points.Length; k++)
+        {
+            double area = Math.Abs(points[i][0] * (points[j][1] - points[k][1]) +
+                                   points[j][0] * (points[k][1] - points[i][1]) +
+                                   points[k][0] * (points[i][1] - points[j][1])) / 2.0;
+            ans = area > ans ? area : ans;
+        }
+        return ans;
+    }
+}
