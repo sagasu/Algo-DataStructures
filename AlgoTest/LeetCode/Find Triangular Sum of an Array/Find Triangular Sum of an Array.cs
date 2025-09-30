c1408b577139d77a20c65a3fca97745b8ea351dd
Mateusz Kopij <sagasu@gmail.com>
2025-09-30 21:10:31 +0100

    TriangularSum

---
+++ AlgoTest/LeetCode/Find Triangular Sum of an Array/Find Triangular Sum of an Array.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgoTest.LeetCode.Find_Triangular_Sum_of_an_Array;
+
+public class Find_Triangular_Sum_of_an_Array
+{
+    public int TriangularSum(int[] nums) {
+        int n = nums.Length;
+        for (int i = n; i > 1; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+}
