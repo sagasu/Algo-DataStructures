1d3db7d7121d522728aa96c1d35b8e2155dd3681
Mateusz Kopij <sagasu@gmail.com>
2025-10-09 22:05:32 +0100

    Find_the_Minimum_Amount_of_Time_to_Brew_Potions

---
+++ AlgoTest/LeetCode/Find the Minimum Amount of Time to Brew Potions/Find the Minimum Amount of Time to Brew Potions.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgoTest.LeetCode.Find_the_Minimum_Amount_of_Time_to_Brew_Potions;
+
+public class Find_the_Minimum_Amount_of_Time_to_Brew_Potions
+{
+    public long MinTime(int[] skill, int[] mana) {
+        int n = skill.Length;
+        int m = mana.Length;
+        long[] done = new long[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                done[j + 1] = Math.Max(done[j + 1], done[j]) + (long)mana[i] * skill[j];
+
+            for (int k = n - 1; k > 0; k--)
+                done[k] = done[k + 1] - (long)mana[i] * skill[k];
+        }
+
+        return done[n];
+    }
+}
