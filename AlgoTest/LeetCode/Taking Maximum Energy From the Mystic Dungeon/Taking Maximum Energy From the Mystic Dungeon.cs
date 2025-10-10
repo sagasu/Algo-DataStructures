28074e4b03c2f392c3e6576116a7d692cb0a0769
Mateusz Kopij <sagasu@gmail.com>
2025-10-10 07:41:00 +0100

    Taking_Maximum_Energy_From_the_Mystic_Dungeon

---
+++ AlgoTest/LeetCode/Taking Maximum Energy From the Mystic Dungeon/Taking Maximum Energy From the Mystic Dungeon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Taking_Maximum_Energy_From_the_Mystic_Dungeon;
+
+public class Taking_Maximum_Energy_From_the_Mystic_Dungeon
+{
+    public int MaximumEnergy(int[] energy, int k) {
+        int n = energy.Length;
+        int[] dp = new int[n];
+        int result = int.MinValue;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
+            result = Math.Max(result, dp[i]);
+        }
+        return result;
+    }
+}
