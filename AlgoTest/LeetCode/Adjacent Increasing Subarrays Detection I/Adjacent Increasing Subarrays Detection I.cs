17d64020ec3ddd14fc3c8d92832f6a0490cd7c05
Mateusz Kopij <sagasu@gmail.com>
2025-10-14 22:12:15 +0100

    Adjacent_Increasing_Subarrays_Detection_I

---
+++ AlgoTest/LeetCode/Adjacent Increasing Subarrays Detection I/Adjacent Increasing Subarrays Detection I.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Adjacent_Increasing_Subarrays_Detection_I;
+
+public class Adjacent_Increasing_Subarrays_Detection_I
+{
+    public bool HasIncreasingSubarrays(IList<int> nums, int k)
+    {
+        var currentSequenceLength = 1;
+        var previousSequenceLength = 0;
+
+        for (int i = 1; i < nums.Count; i++)
+        {
+            if (nums[i - 1] < nums[i])
+            {
+                currentSequenceLength++;
+            }
+            else
+            {
+                previousSequenceLength = currentSequenceLength;
+                currentSequenceLength = 1;
+            }
+
+            if ((currentSequenceLength >= k * 2) || (currentSequenceLength >= k && previousSequenceLength >= k))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
