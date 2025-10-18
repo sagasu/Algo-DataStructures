2975abd352d8d6e0692174a7bb2ff8facc4a33a5
Mateusz Kopij <sagasu@gmail.com>
2025-10-18 22:08:36 +0100

    Maximum_Number_of_Distinct_Elements_After_Operations

---
+++ AlgoTest/LeetCode/Maximum Number of Distinct Elements After Operations/Maximum Number of Distinct Elements After Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Maximum_Number_of_Distinct_Elements_After_Operations;
+
+public class Maximum_Number_of_Distinct_Elements_After_Operations
+{
+    public int MaxDistinctElements(int[] nums, int k) {
+        Array.Sort(nums);
+        long prevAssigned = long.MinValue / 4; // very small
+        int distinctCount = 0;
+        foreach (int v in nums) {
+            long num = v;
+            long assigned = Math.Max(num - k, prevAssigned + 1);
+            if (assigned <= num + k) {
+                distinctCount++;
+                prevAssigned = assigned;
+            }
+        }
+        return distinctCount;
+    }
+}
