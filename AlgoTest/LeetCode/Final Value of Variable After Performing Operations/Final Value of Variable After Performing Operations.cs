87df1a32fbefe76c9c54e5aad6fd1c0a2a0334ee
Mateusz Kopij <sagasu@gmail.com>
2025-10-20 21:05:52 +0100

    Final_Value_of_Variable_After_Performing_Operations

---
+++ AlgoTest/LeetCode/Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgoTest.LeetCode.Final_Value_of_Variable_After_Performing_Operations;
+
+public class Final_Value_of_Variable_After_Performing_Operations
+{
+    public int FinalValueAfterOperations(string[] operations) {
+        int x = 0;
+        foreach (var op in operations) {
+            if (op[1] == '+') x++;
+            else x--;
+        }
+        return x;
+    }
+}
