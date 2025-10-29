965d1d1d2c739710454537496dd11205197886c2
Mateusz Kopij <sagasu@gmail.com>
2025-10-29 21:37:23 +0000

    Smallest_Number_With_All_Set_Bits

---
+++ AlgoTest/LeetCode/Smallest Number With All Set Bits/Smallest Number With All Set Bits.cs
@@ -0,0 +1,6 @@
+﻿namespace AlgoTest.LeetCode.Smallest_Number_With_All_Set_Bits;
+
+public class Smallest_Number_With_All_Set_Bits
+{
+    public int SmallestNumber(int n) =>  (1 << (32 - System.Numerics.BitOperations.LeadingZeroCount((uint)n))) - 1;
+}
