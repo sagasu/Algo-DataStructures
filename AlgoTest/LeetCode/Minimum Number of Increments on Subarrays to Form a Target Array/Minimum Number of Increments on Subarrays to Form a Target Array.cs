42c20ffa627047687973d2d364c12d5558b31449
Mateusz Kopij <sagasu@gmail.com>
2025-10-30 20:14:39 +0000

    Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array

---
+++ AlgoTest/LeetCode/Minimum Number of Increments on Subarrays to Form a Target Array/Minimum Number of Increments on Subarrays to Form a Target Array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array;
+
+public class Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array
+{
+    public int MinNumberOperations(int[] target)
+    {
+        int N = target.Length;
+        int Ans = target[0];
+
+        for (int i = 1; i < N; ++i)
+            Ans += Math.Max(target[i] - target[i - 1], 0);
+        
+        return Ans;
+    }
+}
