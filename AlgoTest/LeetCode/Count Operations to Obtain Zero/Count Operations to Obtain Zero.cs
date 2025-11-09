8f68d3a425cb4633049cc63673230d2da9ed3336
Mateusz Kopij <sagasu@gmail.com>
2025-11-09 20:20:29 +0000

    Count_Operations_to_Obtain_Zero

---
+++ AlgoTest/LeetCode/Count Operations to Obtain Zero/Count Operations to Obtain Zero.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgoTest.LeetCode.Count_Operations_to_Obtain_Zero;
+
+public class Count_Operations_to_Obtain_Zero
+{
+    public int CountOperations(int num1, int num2) {
+        int res = 0;
+        while (num1 != 0 && num2 != 0) {
+            res += num1 / num2;
+            num1 %= num2;
+            (num1, num2) = (num2, num1);
+        }
+        return res;
+    }
+}
