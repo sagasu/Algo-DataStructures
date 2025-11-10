16e79d197e7e94106bc8aeb85e07a2f42866cddd
Mateusz Kopij <sagasu@gmail.com>
2025-11-10 20:15:47 +0000

    Minimum_Operations_to_Convert_All_Elements_to_Zero

---
+++ AlgoTest/LeetCode/Minimum Operations to Convert All Elements to Zero/Minimum Operations to Convert All Elements to Zero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Minimum_Operations_to_Convert_All_Elements_to_Zero;
+
+public class Minimum_Operations_to_Convert_All_Elements_to_Zero
+{
+    public int MinOperations(int[] nums) {
+        var s = new List<int>();
+        int res = 0;
+        foreach (int a in nums) {
+            while (s.Count > 0 && s[s.Count - 1] > a) s.RemoveAt(s.Count - 1);
+            if (a == 0)
+                continue;
+            if (s.Count == 0 || s[s.Count - 1] < a) {
+                res++;
+                s.Add(a);
+            }
+        }
+        return res;
+    }
+}
