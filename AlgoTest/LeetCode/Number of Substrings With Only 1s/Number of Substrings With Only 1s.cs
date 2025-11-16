bc9d64f697444b4fb4642d17e848ea0af7c1fa21
Mateusz Kopij <sagasu@gmail.com>
2025-11-16 12:59:41 +0000

    Number_of_Substrings_With_Only_1s

---
+++ AlgoTest/LeetCode/Number of Substrings With Only 1s/Number of Substrings With Only 1s.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoTest.LeetCode.Number_of_Substrings_With_Only_1s;
+
+public class Number_of_Substrings_With_Only_1s
+{
+    public int NumSub(string s) {
+        const int MODULO = 1000000007;
+        long total = 0;
+        long consecutive = 0;
+        foreach (char c in s) {
+            if (c == '0') {
+                total += consecutive * (consecutive + 1) / 2;
+                total %= MODULO;
+                consecutive = 0;
+            } else {
+                consecutive++;
+            }
+        }
+        total += consecutive * (consecutive + 1) / 2;
+        total %= MODULO;
+        return (int)total;
+    }
+}
