23fc1e57a3dcdab6cfc4869f990987a7fa5e791c
Mateusz Kopij <sagasu@gmail.com>
2025-11-18 20:25:12 +0000

    IsOneBitCharacter

---
+++ AlgoTest/LeetCode/1-bit and 2-bit Characters/class1bitand2bitCharacters.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoTest.LeetCode._1_bit_and_2_bit_Characters;
+
+public class C1bitand2bitCharacters {
+    public bool IsOneBitCharacter(int[] bits) {
+        var i = 0 ;
+        while(i < bits.Length -1)
+        {
+            if(bits[i] == 0)
+            {
+                i += 1;
+            }    
+            else
+            {
+                i += 2;
+            }
+        }
+        return i ==  bits.Length -1 && bits[i] == 0; 
+    }
+}
