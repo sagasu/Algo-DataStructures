0b194d065db98ea1648afd0ab2f4dba8b7c04867
Mateusz Kopij <sagasu@gmail.com>
2025-11-25 20:38:13 +0000

    SmallestRepunitDivByK

---
+++ AlgoTest/LeetCode/Smallest Integer Divisible by K/Smallest Integer Divisible by K.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoTest.LeetCode.Smallest_Integer_Divisible_by_K;
+
+public class Smallest_Integer_Divisible_by_K
+{
+    public int SmallestRepunitDivByK(int k)
+    {
+        if((k&1) == 0 || k%5 == 0)
+            return -1;
+
+        int len = 1;
+        int rem = 1%k;
+
+        while(rem != 0)
+        {
+            rem = rem*10 + 1;
+            rem = rem%k;
+            len++;
+        }
+
+        return len;
+    }
+}
