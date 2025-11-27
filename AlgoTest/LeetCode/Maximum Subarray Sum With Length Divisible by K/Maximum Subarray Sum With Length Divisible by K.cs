0676aa2ca9815e4c67af275c1ea42d17c71dfac8
Mateusz Kopij <sagasu@gmail.com>
2025-11-27 20:21:20 +0000

    Maximum_Subarray_Sum_With_Length_Divisible_by_K

---
+++ AlgoTest/LeetCode/Maximum Subarray Sum With Length Divisible by K/Maximum Subarray Sum With Length Divisible by K.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Maximum_Subarray_Sum_With_Length_Divisible_by_K;
+
+public class Maximum_Subarray_Sum_With_Length_Divisible_by_K
+{
+    public long MaxSubarraySum(int[] nums, int k)
+    {
+        long INF = long.MaxValue;
+        long[] minPrefix = new long[k];
+
+        for (int i = 0; i < k; i++)
+            minPrefix[i] = INF;
+
+        minPrefix[0] = 0;
+
+        long prefix = 0;
+        long answer = long.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            prefix += nums[i];
+            int mod = (i + 1) % k;
+
+            if (minPrefix[mod] != INF)
+                answer = Math.Max(answer, prefix - minPrefix[mod]);
+
+            if (prefix < minPrefix[mod])
+                minPrefix[mod] = prefix;
+        }
+
+        return answer;
+    }
+}
