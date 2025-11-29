580fc8487ce7972144340dafd6e73c441a9a709c
Mateusz Kopij <sagasu@gmail.com>
2025-11-29 22:14:14 +0000

    Minimum_Operations_to_Make_Array_Sum_Divisible_by_K

---
+++ AlgoTest/LeetCode/Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace AlgoTest.LeetCode.Minimum_Operations_to_Make_Array_Sum_Divisible_by_K;
+
+public class Minimum_Operations_to_Make_Array_Sum_Divisible_by_K
+{
+    public int MinOperations(int[] nums, int k) => nums.Sum() % k;
+}
