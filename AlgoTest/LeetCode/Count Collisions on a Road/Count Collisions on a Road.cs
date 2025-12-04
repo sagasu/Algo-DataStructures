0653e5fcd01efbea5dd3e2f60ad411ae8a9d1806
Mateusz Kopij <sagasu@gmail.com>
2025-12-04 20:26:54 +0000

    Count_Collisions_on_a_Road

---
+++ AlgoTest/LeetCode/Count Collisions on a Road/Count Collisions on a Road.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgoTest.LeetCode.Count_Collisions_on_a_Road;
+
+public class Count_Collisions_on_a_Road
+{
+    public int CountCollisions(string directions)
+    {
+        int i = 0, j = directions.Length - 1;
+
+        while (i < directions.Length && directions[i] == 'L') i++;
+        while (j >= 0 && directions[j] == 'R') j--;
+
+        int count = 0;
+
+        for (int k = i; k <= j; k++)
+            if (directions[k] != 'S') count++;
+
+        return count;
+    }
+}
