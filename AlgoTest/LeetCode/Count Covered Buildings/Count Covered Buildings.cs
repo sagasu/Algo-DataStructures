56340532e412f11778cfc872c800b6b0f601ed4d
Mateusz Kopij <sagasu@gmail.com>
2025-12-11 19:36:51 +0000

    Count_Covered_Buildings

---
+++ AlgoTest/LeetCode/Count Covered Buildings/Count Covered Buildings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Count_Covered_Buildings;
+
+public class Count_Covered_Buildings
+{
+    public int CountCoveredBuildings(int n, int[][] buildings) 
+    {
+        var rowMin = new Dictionary<int, int>();
+        var rowMax = new Dictionary<int, int>();
+        
+        var colMin = new Dictionary<int, int>();
+        var colMax = new Dictionary<int, int>();
+
+        foreach (var b in buildings) 
+        {
+            int x = b[0], y = b[1];
+
+            if (!rowMin.ContainsKey(x)) 
+            {
+                rowMin[x] = rowMax[x] = y;
+            } 
+            else 
+            {
+                rowMin[x] = Math.Min(rowMin[x], y);
+                rowMax[x] = Math.Max(rowMax[x], y);
+            }
+
+            if (!colMin.ContainsKey(y)) 
+            {
+                colMin[y] = colMax[y] = x;
+            } 
+            else 
+            {
+                colMin[y] = Math.Min(colMin[y], x);
+                colMax[y] = Math.Max(colMax[y], x);
+            }
+        }
+
+        int covered = 0;
+        foreach (var b in buildings) 
+        {
+            int x = b[0], y = b[1];
+
+            bool hasLeft  = rowMin[x] < y;
+            bool hasRight = rowMax[x] > y;
+            bool hasUp    = colMin[y] < x;
+            bool hasDown  = colMax[y] > x;
+
+            if (hasLeft && hasRight && hasUp && hasDown)
+                covered++;
+        }
+
+        return covered;
+    }
+}
