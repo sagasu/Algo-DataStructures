d7b3c4611b7e024bf3c8ce942eea9a62f222557f
Mateusz Kopij <sagasu@gmail.com>
2025-12-13 12:08:59 +0000

    Coupon_Code_Validator

---
+++ AlgoTest/LeetCode/Coupon Code Validator/Coupon Code Validator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoTest.LeetCode.Coupon_Code_Validator;
+
+public class Coupon_Code_Validator
+{
+    public IList<string> ValidateCoupons(string[] code, string[] businessLine, bool[] isActive) => code.
+        Where(m => !string.IsNullOrEmpty(m)).
+        Select((m, i) => (m, i)).
+        Where(m => isActive[m.i]).
+        Where(m => ((string[])["electronics", "grocery", "pharmacy", "restaurant"]).Contains(businessLine[m.i])).
+        Where(m => !m.m.ToCharArray().Any(n => !char.IsLetterOrDigit(n) && n != '_')).
+        OrderBy(m => businessLine[m.i]).
+        ThenBy(m => m.m, StringComparer.Ordinal).
+        Select(m => m.m).
+        ToList();
+}
