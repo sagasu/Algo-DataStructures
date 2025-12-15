b69e65402f82575d4c35f61aa3bbb1507e9bcf1c
Mateusz Kopij <sagasu@gmail.com>
2025-12-15 16:08:59 +0000

    GetDescentPeriods

---
+++ AlgoTest/LeetCode/Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoTest.LeetCode.Number_of_Smooth_Descent_Periods_of_a_Stock;
+
+public class Number_of_Smooth_Descent_Periods_of_a_Stock
+{
+    public long GetDescentPeriods(int[] prices) {
+        long len = 1;
+        long totalLength = 1;
+        for(int right = 1; right < prices.Length; right++) {
+            if(prices[right] == prices[right - 1] - 1)
+            {
+                len++;
+            }
+            else
+            {
+                len = 1;
+            }
+            totalLength += len;
+        }
+        return totalLength;
+    }
+}
