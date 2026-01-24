cc4f14bb30968dc1dce4ce1c6bc175af438cd0e2
Mateusz Kopij <sagasu@gmail.com>
2026-01-24 21:13:31 +0000

    Minimize_Maximum_Pair_Sum_in_Array

---
+++ AlgoTest/LeetCode/Minimize Maximum Pair Sum in Array/Minimize Maximum Pair Sum in Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Minimize_Maximum_Pair_Sum_in_Array;
+
+public class Minimize_Maximum_Pair_Sum_in_Array
+{
+    public int MinPairSum(int[] nums) {
+        Array.Sort(nums);
+        int maxPairSum=0;
+        int i=0,j=nums.Length-1;
+
+        while(i<j){
+            int currentSum= nums[i]+nums[j];
+        
+            if(currentSum>maxPairSum) maxPairSum=currentSum;
+
+            i++;
+            j--;
+        }
+        return maxPairSum;
+    }
+}
