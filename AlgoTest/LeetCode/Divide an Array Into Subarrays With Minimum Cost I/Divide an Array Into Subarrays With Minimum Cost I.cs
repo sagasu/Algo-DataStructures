e6020cfd09a75e2b54067db5162f54feeb1958f8
Mateusz Kopij <sagasu@gmail.com>
2026-02-01 22:15:49 +0000

    Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I

---
+++ AlgoTest/LeetCode/Divide an Array Into Subarrays With Minimum Cost I/Divide an Array Into Subarrays With Minimum Cost I.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgoTest.LeetCode.Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I;
+
+public class Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I
+{
+    public int MinimumCost(int[] nums) {
+        int min1 = int.MaxValue;
+        int min2 = int.MaxValue;
+
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return nums[0] + min1 + min2;
+    }
+}
