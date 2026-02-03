b6e0e9e6405b5d602d22ba24519527bef6c42007
Mateusz Kopij <sagasu@gmail.com>
2026-02-03 21:31:18 +0000

    Trionic_Array_I

---
+++ AlgoTest/LeetCode/Trionic Array I/Trionic Array I.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgoTest.LeetCode.Trionic_Array_I;
+
+public class Trionic_Array_I
+{
+    public bool IsTrionic(int[] nums) {
+        int n = nums.Length, i = 0;
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == 0 || i == n - 1) return false;
+
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+        if (i == p || i == n - 1) return false;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        return i == n - 1;
+    }
+}
