57a2124b46e6dee9b178271fdb3be08955d2bc49
Mateusz Kopij <sagasu@gmail.com>
2026-02-06 18:56:08 +0000

    Minimum_Removals_to_Balance_Array

---
+++ AlgoTest/LeetCode/Minimum Removals to Balance Array/Minimum Removals to Balance Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgoTest.LeetCode.Minimum_Removals_to_Balance_Array;
+
+public class Minimum_Removals_to_Balance_Array
+{
+    public int MinRemoval(int[] nums, int k) {
+        Array.Sort(nums);
+        int left = 0;
+        int right = 0;
+        int min = int.MaxValue;
+
+        for(right = 0; right < nums.Length; right++) {
+            
+            while((long)nums[right] > (long)nums[left]*k) {
+                left++;
+            }
+
+            min = Math.Min(nums.Length-(right-left+1), min);
+        }
+
+        return min;
+    }
+}
