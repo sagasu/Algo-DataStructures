55c780078e670c1b9f05a1ea056cceed2a29907b
Mateusz Kopij <sagasu@gmail.com>
2026-02-18 23:18:42 +0000

    Binary_Number_with_Alternating_Bits

---
+++ AlgoTest/LeetCode/Binary Number with Alternating Bits/Binary Number with Alternating Bits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoTest.LeetCode.Binary_Number_with_Alternating_Bits;
+
+public class Binary_Number_with_Alternating_Bits
+{
+    public bool HasAlternatingBits(int n) {
+        var bits = new List<int>();
+        while (n != 0) {
+            bits.Add(n & 1);
+            n >>= 1;
+        }
+        for (int i = 0; i < bits.Count - 1; i++) {
+            if (bits[i] == bits[i + 1])
+                return false;
+        }
+        return true;
+    }
+}
