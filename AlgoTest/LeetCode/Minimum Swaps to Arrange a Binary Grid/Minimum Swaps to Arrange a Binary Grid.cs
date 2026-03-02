efc77433587cfec45b7f3a3462739d346c51bc68
Mateusz Kopij <sagasu@gmail.com>
2026-03-02 06:54:02 +0000

    Minimum_Swaps_to_Arrange_a_Binary_Grid

---
+++ AlgoTest/LeetCode/Minimum Swaps to Arrange a Binary Grid/Minimum Swaps to Arrange a Binary Grid.cs
@@ -0,0 +1,46 @@
+﻿namespace AlgoTest.LeetCode.Minimum_Swaps_to_Arrange_a_Binary_Grid;
+
+public class Minimum_Swaps_to_Arrange_a_Binary_Grid
+{
+    public int MinSwaps(int[][] grid)
+    {
+        int n = grid.Length;
+        int[] zeroCounts = new int[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+
+            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--)
+                count++;
+            
+            zeroCounts[i] = count;
+        }
+        
+        int swaps = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            int target = n - 1 - i;
+            int j = i;
+            
+            while (j < n && zeroCounts[j] < target)
+                j++;
+            
+            if (j == n) 
+                return -1;
+            
+            swaps += j - i;
+
+            int temp = zeroCounts[j];
+
+            for (int k = j; k > i; k--) {
+                zeroCounts[k] = zeroCounts[k - 1];
+            }
+            
+            zeroCounts[i] = temp;
+        }
+        
+        return swaps;
+    }
+}
